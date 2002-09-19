c94dea7d84a1bcd17586e30f1bb543b78a111e79
Benjamin Drieu <benj@users.sourceforge.net>
2002-09-19 15:42:27 +0000

    - mise à jour GRISBI_BIN_DIR

--- acconfig.h
+++ acconfig.h
@@ -1,5 +1,8 @@
 /* Definit le chemin vers le logo de Grisbi */
 #undef CHEMIN_LOGO
 
-/* Definit le chemin vers le binaire de Grisbi */
+/* Definit le chemin vers le binaire de l'applet de Grisbi */
 #undef APPLET_BIN_DIR
+
+/* Definit le chemin vers le binaire de Grisbi */
+#undef GRISBI_BIN_DIR
