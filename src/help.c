8be4fbb265393d6b8ebde735a7df3b945135ca6d
Gérald Niel <gegeweb@users.sourceforge.net>
2002-11-02 18:01:23 +0000

    Modification email Gérald

--- src/help.c
+++ src/help.c
@@ -37,7 +37,7 @@ void a_propos ( GtkWidget *bouton,
   const gchar *auteur[] = { "Auger Cédric ( cedric@grisbi.org ) : Programmation",
 			    "Cartron Daniel ( doc@grisbi.org ) : Manuel, Site internet, conseils en comptabilité",
 			    "Drieu Benjamin ( bdrieu@april.org ) : Programmation, packages deb",
-			    "Niel Gérald ( gerald.niel@gegeweb.net ) : Packages RPM, Site internet",
+			    "Niel Gérald ( gerald.niel@grisbi.org ) : Packages RPM, Site internet",
 			    "Pascual André ( andre@linuxgraphic.org ) : Logo",
 			    NULL };
 
