3ea763cb936e59984461ae182b8aa90a09095d33
Benjamin Drieu <benj@users.sourceforge.net>
2003-01-02 19:01:01 +0000

    Suppression, plus besoin.

--- acconfig.h
+++
@@ -1,8 +0,0 @@
-/* Definit le chemin vers le logo de Grisbi */
-#undef CHEMIN_LOGO
-
-/* Definit le chemin vers le binaire de l'applet de Grisbi */
-#undef APPLET_BIN_DIR
-
-/* Definit le chemin vers le binaire de Grisbi */
-#undef GRISBI_BIN_DIR
