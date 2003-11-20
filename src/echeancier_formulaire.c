ce200ab2d496ea87380630f0c3ad0038b14b4381
Benjamin Drieu <benj@users.sourceforge.net>
2003-11-20 15:26:26 +0000

    Fix bug #133, which prevented categories to be displayed for updated scheduled transactions

--- src/echeancier_formulaire.c
+++ src/echeancier_formulaire.c
@@ -1843,6 +1843,8 @@ void fin_edition_echeance ( void )
 
 		      echeance -> sous_categorie = sous_categ -> no_sous_categ;
 		    }
+		  
+		  echeance -> compte_virement = 0;
 		}
 	    }
 	  g_strfreev ( tableau_char );
