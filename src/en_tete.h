06f1d019e0f025fa7a3cb0da1b55441c509abfb3
Benjamin Drieu <benj@users.sourceforge.net>
2003-12-02 22:48:23 +0000

    Add a prototype to prevent gcc croaks

--- src/en_tete.h
+++ src/en_tete.h
@@ -82,6 +82,7 @@ gboolean charge_ib ( gchar *nom_ib );
 gboolean charge_ib_version_0_4_0 ( xmlDocPtr doc );
 void propose_changement_permissions ( void );
 double my_strtod ( const char *nptr, char **endptr );
+char * latin2utf8 (char * inchar);
 
 
 /************************/ 
