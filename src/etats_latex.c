f7b74cd3fb2a9081f24633c63d66af3aa74756a4
Benjamin Drieu <benj@users.sourceforge.net>
2004-01-15 13:27:57 +0000

    Improve LaTeX rendering a *lot*. LaTeX backend should now be able to render all reports without errors, though it is very ugly at the moment.

--- src/etats_latex.c
+++ src/etats_latex.c
@@ -27,6 +27,9 @@
 
 #include "etats_latex.h"
 
+int lastline = 0;
+int last_is_hsep = 0;
+FILE * out;
 
 
 struct struct_etat_affichage latex_affichage = {
@@ -45,10 +48,54 @@ struct struct_etat_affichage latex_affichage = {
 void latex_attach_label ( gchar * text, int x, int x2, int y, int y2, 
 			  enum alignement align, struct structure_operation * ope )
 {
+  gchar * safe_text;
+
   if ( !text )
     text = "";
 
-  printf ( "%s\n", text );
+  fprintf ( out, "%% %d,%d -> %d,%d : %s\n", x, x2, y, y2, text );
+
+  if ( y >= lastline )
+    {
+      lastline = y2;
+      if ( ! last_is_hsep )
+	{
+	  int pad;
+	  fprintf ( out, "\\\\\n" );
+	  for ( pad = 0 ; pad < x ; pad ++ )
+	    latex_attach_label ( NULL, pad, pad+1, y, y2, align, ope );
+	}
+    }
+  else
+    {
+      fprintf ( out, "&{\\vrule}&", text );
+    }
+
+
+  fprintf ( out, "\\begin{minipage}{\\hsize}\n" );
+
+  switch ( align )
+    {
+    case LEFT:
+      fprintf ( out, "\\raggedright\n" );
+      break;
+  
+    case RIGHT:
+      fprintf ( out, "\\raggedleft\n" );
+      break;
+  
+    case CENTER:
+      break;
+    }
+
+  safe_text = latex_safe(text);
+  fprintf ( out, "%s\n\\end{minipage}", safe_text );
+  free ( safe_text );
+
+/*   if ( x2 < nb_colonnes ) */
+/*     fprintf ( out, "&{\\vrule}&", text ); */
+
+  last_is_hsep = 0;
 }
 
 
@@ -59,7 +106,16 @@ void latex_attach_label ( gchar * text, int x, int x2, int y, int y2,
  */
 void latex_attach_vsep ( int x, int x2, int y, int y2)
 {
-  printf ( "|\n" );
+  if ( y >= lastline )
+    {
+      if ( ! last_is_hsep )
+	fprintf ( out, "\\\\\n" );
+      lastline = y2;
+    }
+
+  fprintf ( out, "{\\vrule}&" );
+
+  last_is_hsep = 0;
 }
 
 
@@ -70,14 +126,47 @@ void latex_attach_vsep ( int x, int x2, int y, int y2)
  */
 void latex_attach_hsep ( int x, int x2, int y, int y2)
 {
-  printf ( "-\n" );
+/*   if ( y > lastline ) */
+/*     {       */
+      if ( ! last_is_hsep )
+	fprintf ( out, "\\\\\n" );
+      lastline = y2;
+/*     } */
+  fprintf ( out, "\\hline\n" );
+
+  last_is_hsep = 1;
 }
 
 
 
 gint latex_initialise (GSList * opes_selectionnees)
 {
-  printf (">>> Initialisation\n");
+  int i;
+
+  out = fopen ("toto.tex", "w");
+  if (! out)
+    {
+      fprintf ( stderr, _("Cannot open file '%s': %s\n"), "toto.tex", 
+		strerror(errno) );
+      return FALSE;
+     }
+
+  fprintf (out, 
+	   "\\documentclass{article}\n\n"
+	   "\\usepackage{a4}\n"
+	   "\\usepackage[utf8]{inputenc}\n"
+	   "\\usepackage{eurosym}\n"
+	   "\\usepackage{landscape}\n"
+	   "\\usepackage{longtable}\n"
+	   "\\begin{document}\n\n"
+	   "\\begin{longtable}[l]{");
+
+  for (i = 1 ; i < nb_colonnes ; i++)
+    {
+      fprintf (out, "p{3cm}c");
+    }
+
+  fprintf (out, "p{3cm}}\n");
 
   return 1;
 }
@@ -86,8 +175,44 @@ gint latex_initialise (GSList * opes_selectionnees)
 
 gint latex_finish ()
 {
-  printf (">>> Fin\n");
+  fprintf (out, "\\end{longtable}\n"
+	   "\\end{document}\n");
 
+  fclose (out);
+
+  if ( system ( "latex -interaction=nonstopmode toto.tex" ) > 0 )
+    dialogue ( _("LaTeX run was unable to complete, see console output for details.") );
+  else 
+    system ( "xdvi toto.dvi &" );
+  
   return 1;
 }
 
+
+
+gchar * latex_safe ( gchar * text ) {
+  gchar * safe_text, *tmp;
+
+  if ( ! text || ! strlen(text))
+    return "";
+
+  safe_text = malloc ( strlen(text) * 2 );
+
+  for ( tmp = safe_text ; * text; text ++, tmp++ )
+    {
+      switch ( * text )
+	{
+	case '&':
+	case '\\':
+	  *tmp = '\\';
+	  *tmp++;
+
+	default:
+	  *tmp = *text;
+	  break;
+	}
+    }
+  *tmp = 0;
+
+  return safe_text;
+}
