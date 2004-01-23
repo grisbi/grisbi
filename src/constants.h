a34e5f0f4e9b71d6a6b21ce105f0eed6b203a25a
Alain Portal <dionysos-sf@users.sourceforge.net>
2004-01-23 12:30:34 +0000

    Constants definitions that are needed in several source files.

---
+++ src/constants.h
@@ -0,0 +1,29 @@
+/* Fichier de définition de constantes constant.h */
+
+/* Constantes utilisées par la fonction incremente_decremente_date */
+# define ONE_DAY 1
+# define ONE_WEEK 7
+# define ONE_MONTH 30
+# define ONE_YEAR 365
+
+/* Constantes qui définisent le numéro de widget dans le formulaire
+   de saisie des opérations */
+# define TRANSACTION_FORM_OP_NB 0
+# define TRANSACTION_FORM_DATE 1
+# define TRANSACTION_FORM_PARTY 2
+# define TRANSACTION_FORM_DEBIT 3
+# define TRANSACTION_FORM_CREDIT 4
+# define TRANSACTION_FORM_DEVISE 5
+# define TRANSACTION_FORM_CHANGE 6
+# define TRANSACTION_FORM_VALUE_DATE 7
+# define TRANSACTION_FORM_CATEGORY 8
+# define TRANSACTION_FORM_TYPE 9
+# define TRANSACTION_FORM_CHEQUE 10
+# define TRANSACTION_FORM_EXERCICE 11
+# define TRANSACTION_FORM_BUDGET 12
+# define TRANSACTION_FORM_CONTRA 13
+# define TRANSACTION_FORM_VOUCHER 14
+# define TRANSACTION_FORM_BREAKDOWN 15
+# define TRANSACTION_FORM_NOTES 16
+# define TRANSACTION_FORM_BANK 17
+# define TRANSACTION_FORM_MODE 18
