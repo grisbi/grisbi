5128a3191ade76031ced625ddb48a1b178288301
Benjamin Drieu <benj2@users.sourceforge.net>
2004-02-16 18:01:07 +0000

    Update prototypes

--- src/etats_latex.h
+++ src/etats_latex.h
@@ -4,7 +4,8 @@ gint latex_initialise (GSList * opes_selectionnees);
 gint latex_finish ();
 void latex_attach_hsep ( int x, int x2, int y, int y2);
 void latex_attach_vsep ( int x, int x2, int y, int y2);
-void latex_attach_label ( gchar * text, int x, int x2, int y, int y2, 
-			     enum alignement align, struct structure_operation * ope );
+void latex_attach_label ( gchar * text, gdouble properties, int x, int x2, int y, int y2, 
+			  enum alignement align, struct structure_operation * ope );
+void latex_safe ( gchar * text );
 
 struct struct_etat_affichage latex_affichage;
