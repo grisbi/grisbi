248eb1f5be1ca6db2dc2d396f84c4b81b05c9ce9
Benjamin Drieu <benj2@users.sourceforge.net>
2004-03-06 12:45:21 +0000

    Cosmetic changes

--- src/etats_latex.h
+++ src/etats_latex.h
@@ -1,11 +1,15 @@
 /* fichier d'en tête etats_latex.h */
 
+struct struct_etat_affichage latex_affichage;
+
+/* LaTeX backend functions */
 gint latex_initialise (GSList * opes_selectionnees);
 gint latex_finish ();
 void latex_attach_hsep ( int x, int x2, int y, int y2);
 void latex_attach_vsep ( int x, int x2, int y, int y2);
-void latex_attach_label ( gchar * text, gdouble properties, int x, int x2, int y, int y2, 
-			  enum alignement align, struct structure_operation * ope );
-void latex_safe ( gchar * text );
+void latex_attach_label ( gchar * text, gdouble properties, int x, int x2, 
+			  int y, int y2, enum alignement align, 
+			  struct structure_operation * ope );
 
-struct struct_etat_affichage latex_affichage;
+/* Utility functions */
+void latex_safe ( gchar * text );
