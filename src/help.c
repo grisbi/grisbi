7d3ee81c02f2372019d9d886b1e832a0dce3cd6f
Benjamin Drieu <benj2@users.sourceforge.net>
2004-03-12 19:01:48 +0000

    Add Axel as a contributor Minor visual glitch

--- src/help.c
+++ src/help.c
@@ -49,6 +49,7 @@ void a_propos ( GtkWidget *bouton,
 			     _("Francois Terrot (francois@terrot.net): Windows packaging, cygwin port"),
 			     _("Thierry Thomas (thierry@pompo.net): FreeBSD packaging"),
 			     _("Dominique Parisot (parisot@villey-le-sec.com): contributor"),
+			     _("Axel Rousseau (axel584@axel584.org): contributor"),
 			     _("Andre Pascual (andre@linuxgraphic.org) : Logo"),
 			    NULL };
   const gchar *traducteurs[] = { _("Daniel Cartron (cartron@grisbi.org) : English"),
@@ -75,7 +76,7 @@ void a_propos ( GtkWidget *bouton,
   const gchar *license = "This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA";
 
   dialogue = gnome_dialog_new ( _("About..."),
-				GNOME_STOCK_BUTTON_APPLY,
+				GNOME_STOCK_BUTTON_CLOSE,
 				NULL );
   gtk_window_set_transient_for ( GTK_WINDOW ( dialogue ),
 				 GTK_WINDOW ( window ) );
