d36a1beacdf22cb053410f92174f9b3831c14154
Benjamin Drieu <benj2@users.sourceforge.net>
2004-03-31 20:27:29 +0000

    Move enum preference_page where it belongs

--- src/parametres.h
+++ src/parametres.h
@@ -63,3 +63,23 @@ gboolean preference_selectable_func (GtkTreeSelection *selection,
 				     GtkTreeModel *model, GtkTreePath *path,
 				     gboolean path_currently_selected,
 				     gpointer data);
+
+
+/* Preference pages */
+enum preference_page  {
+    NOT_A_PAGE = -1,
+    FILES_PAGE,
+    IMPORT_PAGE,
+    FONTS_AND_LOGO_PAGE,
+    MESSAGES_AND_WARNINGS_PAGE,
+    ADDRESSES_AND_TITLES_PAGE,
+    TRANSACTION_FORM_PAGE,
+    TRANSACTIONS_LIST_PAGE,
+    TRANSACTIONS_PAGE,
+    RECONCILIATION_PAGE,
+    CURRENCIES_PAGE,
+    BANKS_PAGE,
+    FINANCIAL_YEARS_PAGE,
+    METHODS_OF_PAYMENT_PAGE,
+    NUM_PREFERENCES_PAGES
+};
