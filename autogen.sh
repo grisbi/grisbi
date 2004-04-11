cd92704639d370910cb5e7637c06618f31692620
Benjamin Drieu <benj2@users.sourceforge.net>
2004-04-11 16:17:02 +0000

    Initial import

---
+++ autogen.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# autogen.sh glue for CMU Cyrus IMAP
+# $Id: autogen.sh,v 1.1 2004/04/11 16:17:02 benj2 Exp $
+#
+# Requires: automake, autoconf, dpkg-dev
+set -e
+
+# Refresh GNU autotools toolchain.
+for i in config.guess config.sub missing install-sh mkinstalldirs ; do
+	test -r /usr/share/automake/${i} && {
+		rm -f ${i}
+		cp /usr/share/automake/${i} .
+	}
+	chmod 755 ${i}
+done
+
+aclocal -I macros
+autoheader
+#automake --verbose --foreign --add-missing
+autoconf
+
+exit 0
