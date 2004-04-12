7e0852b746b94d4a1339234823c551b28e4d98eb
Gérard Niel <gegeweb@users.sourceforge.net>
2004-04-12 16:38:01 +0000

    - Remove slackware specific file - Add test for slackware (path to automake)

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 #
 # autogen.sh glue for CMU Cyrus IMAP
-# $Id: autogen.sh,v 1.1.2.2 2004/04/11 16:25:56 benj2 Exp $
+# $Id: autogen.sh,v 1.1.2.3 2004/04/12 16:38:01 gegeweb Exp $
 #
 # Requires: automake, autoconf, dpkg-dev
 set -e
 
+PATH_AUTOMAKE=/usr/share/automake
+
+# test for slackware
+if test -x /usr/share/automake-1.7
+then
+	PATH_AUTOMAKE=/usr/share/automake-1.7
+fi
+
 # Refresh GNU autotools toolchain.
 for i in config.guess config.sub missing install-sh mkinstalldirs ; do
-	test -r /usr/share/automake/${i} && {
+	test -r $PATH_AUTOMAKE/${i} && {
 		rm -f ${i}
-		cp /usr/share/automake/${i} .
+		cp $PATH_AUTOMAKE/${i} .
 	}
 	chmod 755 ${i}
 done
