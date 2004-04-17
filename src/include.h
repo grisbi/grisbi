f3da1f2e42f6c016e4fbcdbb13039f50824bcf5b
Benjamin Drieu <benj2@users.sourceforge.net>
2004-04-17 15:25:32 +0000

    Fix comile time error in FreeBSD

--- src/include.h
+++ src/include.h
@@ -1,17 +1,18 @@
 /* fichier d'en tête include.h */
 
-#include "config.h"
 
-#include <gnome.h>
+#include "config.h"
+#include <libintl.h>
+#include <locale.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/timeb.h>
 #include <time.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <pwd.h>
-#include <sys/types.h>
 #include <signal.h>
 #include <ctype.h>
 #include <errno.h>
@@ -22,4 +23,7 @@
 #include <libxml/tree.h>
 
 #define GTK_ENABLE_BROKEN 1
+#define G_BROKEN_FILENAMES 1
 #include <gtk/gtktext.h>
+#include <gdk/gdkkeysyms.h>
+#include <popt.h>
