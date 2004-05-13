e84db991f46185ff8fddc2f074b1d7435e6a200b
François Terrot <teilginn@users.sourceforge.net>
2004-05-13 21:31:29 +0000

    #315 - dummy recuperation_donnees_ofx function for system with no libofx need to return a value NOOFX protection is moved to ofx.c which integrate grisbi_SOURCES in all cases

--- src/ofx.h
+++ src/ofx.h
@@ -1,10 +1,6 @@
 #ifndef OFX_H
 #define OFX_H
 
-#ifdef NOOFX
-#define recuperation_donnees_ofx(name) dialogue_error_hint(_("This build of Grisbi does not support OFX, please recompile Grisbi with OFX support enabled"), g_strdup_printf (_("Cannot process OFX file '%s'"), name))
-#else
 gboolean recuperation_donnees_ofx ( gchar *nom_fichier );
-#endif
 
 #endif
