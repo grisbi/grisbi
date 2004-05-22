7c38057a585b139a1a7fb167cd15960a54dc4349
Benjamin Drieu <benj2@users.sourceforge.net>
2004-05-22 13:47:11 +0000

    Mention Giorgio as a contributor to the italian translation even if he said he stopped the translation

--- src/help.c
+++ src/help.c
@@ -58,6 +58,7 @@ void a_propos ( GtkWidget *bouton,
 	const gchar *traducteurs[] = { _("Daniel Cartron (cartron@grisbi.org) : English"),
 	    _("Benjamin Drieu (bdrieu@april.org) : English"),
 	    _("Alain Portal (dionysos@grisbi.org) : English"),
+            _("Giorgo Mandolfo (giorgio@pollycoke.org) : Italian"),
 	    NULL };
 	    const gchar *liens[] = { COLON(_("Website")),
 		"http://www.grisbi.org/",
