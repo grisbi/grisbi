e366b8cfe9b638f4c9fcaba81f9a6fec6abbb0eb
Benjamin Drieu <benj2@users.sourceforge.net>
2004-12-23 20:09:59 +0000

    Change presentation

--- src/check.c
+++ src/check.c
@@ -43,114 +43,103 @@
 /******************************************************************************/
 void reconciliation_check ( void )
 {
-  gint affected_account = 0;
+  gint affected_accounts = 0;
   gint tested_account = 0;
   GSList *pUserAccountsList = NULL;
-  gchar *pHint = NULL, *pText;
+  gchar *pHint = NULL, *pText = "";
 
   /* s'il n'y a pas de compte, on quitte */
   if ( !nb_comptes )
-    return;
-
-  /* si l'utilisateur n'abandonne pas, faire */
-  pText = _("This will check that the last reconciliation amounts matches with "
-	    "total amounts of reconcilied transactions (and initial balance).");
-  if ( question_yes_no ( pText ))
-  {
-    /* On fera la vérification des comptes dans l'ordre préféré
-       de l'utilisateur. On fait une copie de la liste car
-       on va y écrire dedans pour sortir de la boucle de test des comptes. */
-
-    pUserAccountsList = g_slist_copy ( ordre_comptes );
-
-    /* Pour chacun des comptes, faire */
-    do
-    {
-      p_tab_nom_de_compte_variable = p_tab_nom_de_compte + GPOINTER_TO_INT ( pUserAccountsList -> data );
+      return;
     
+  pUserAccountsList = g_slist_copy ( ordre_comptes );
+  
+  /* Pour chacun des comptes, faire */
+  do
+  {
+      p_tab_nom_de_compte_variable = p_tab_nom_de_compte + GPOINTER_TO_INT ( pUserAccountsList -> data );
+      
       /* Si le compte a été rapproché au moins une fois.
          Seule la date permet de l'affirmer. */
       if ( DATE_DERNIER_RELEVE )
       {
-        GSList *pTransactionList;
-        gdouble reconcilied_amount = 0;
+	  GSList *pTransactionList;
+	  gdouble reconcilied_amount = 0;
 
-        /* On va recalculer le montant rapproché du compte (c-à-d le solde initial
-           plus le montant des opérations rapprochées) et le comparer à la valeur
-           stockée dans le fichier. Si les valeurs diffèrent, on affiche une boite
-	   d'avertissement */
+	  /* On va recalculer le montant rapproché du compte (c-à-d le solde initial
+	     plus le montant des opérations rapprochées) et le comparer à la valeur
+	     stockée dans le fichier. Si les valeurs diffèrent, on affiche une boite
+	     d'avertissement */
       
-        reconcilied_amount = SOLDE_INIT;
+	  reconcilied_amount = SOLDE_INIT;
 
-        /* On récupère la liste des opérations */
-        pTransactionList = LISTE_OPERATIONS;
+	  /* On récupère la liste des opérations */
+	  pTransactionList = LISTE_OPERATIONS;
 
-        while ( pTransactionList )
-        {
-	  struct structure_operation *pTransaction;
-
-	  pTransaction = pTransactionList -> data;
-
-	  /* On ne prend en compte que les opérations rapprochées.
-	     On ne prend pas en compte les opérations de ventilation. */
-	  if ( pTransaction -> pointe == OPERATION_RAPPROCHEE
-	       &&
-	       !pTransaction -> no_operation_ventilee_associee )
+	  while ( pTransactionList )
 	  {
-	      reconcilied_amount += calcule_montant_devise_renvoi ( pTransaction -> montant,
-								    DEVISE,
-								    pTransaction -> devise,
-								    pTransaction -> une_devise_compte_egale_x_devise_ope,
-								    pTransaction -> taux_change,
-								    pTransaction -> frais_change );
+	      struct structure_operation *pTransaction;
+
+	      pTransaction = pTransactionList -> data;
+
+	      /* On ne prend en compte que les opérations rapprochées.
+		 On ne prend pas en compte les opérations de ventilation. */
+	      if ( pTransaction -> pointe == OPERATION_RAPPROCHEE
+		   &&
+		   !pTransaction -> no_operation_ventilee_associee )
+	      {
+		  reconcilied_amount += calcule_montant_devise_renvoi ( pTransaction -> montant,
+									DEVISE,
+									pTransaction -> devise,
+									pTransaction -> une_devise_compte_egale_x_devise_ope,
+									pTransaction -> taux_change,
+									pTransaction -> frais_change );
+	      }
+	      pTransactionList = pTransactionList -> next;
 	  }
-	  pTransactionList = pTransactionList -> next;
-	}
 
-	if ( fabs ( reconcilied_amount - SOLDE_DERNIER_RELEVE ) >= 0.01 )
-	{
-	  affected_account++;
-
-	  pText = g_strdup_printf ( _("Last reconciliation amount : %f\n"
-				      "Computed reconciliation amount : %f\n\n"
-				      "Do you to continue test?"),
-				    reconcilied_amount,
-				    SOLDE_DERNIER_RELEVE );
-	  pHint = g_strdup_printf ( _("Account name : %s\n"),
-				    NOM_DU_COMPTE);
-	  if ( !question_yes_no_hint ( pHint, pText ) )
+	  if ( fabs ( reconcilied_amount - SOLDE_DERNIER_RELEVE ) >= 0.01 )
 	  {
-	    /* En utilisant une méthode de sortie de boucle plus propre,
-	       il est possible de se passer de pUserAccountsList et
-	       et de travailler directement avec ordre_comptes. */
-	    pUserAccountsList -> next = NULL;
+	      affected_accounts ++;
+
+	      pText = g_strconcat ( pText,
+				    g_strdup_printf ( _("<span weight=\"bold\">%s</span>\n"
+							"  Last reconciliation amount : %f\n"
+							"  Computed reconciliation amount : %f\n\n"),
+						      NOM_DU_COMPTE, reconcilied_amount,
+						      SOLDE_DERNIER_RELEVE ),
+				    NULL );
 	  }
-	  free ( pHint );
-	  free ( pText );
-	}
-	tested_account++;
+	  tested_account++;
       }
     }
     while ( (  pUserAccountsList = pUserAccountsList -> next ) );
 
-    if ( !affected_account )
+    if ( !affected_accounts )
     {
 	dialogue ( _("About reconciliation, your accounting is sane.") );
     }
     else
     {
-      pText = g_strdup_printf ( _("There are %d accounts in your file.  %d were tested.\n"
-				  "In %d account(s), stored and computed reconciliation "
-				  "don't match.  You should correct this quickly.\n"
-				  "Generally, in affected accounts, there are too many reconciled "
-				  "transactions. And these transactions are transfert between "
-				  "your Grisbi accounts."),
-				nb_comptes,
-				tested_account,
-				affected_account );
-      dialogue ( pText );
+
+	pText = g_strconcat ( 
+	    _("Grisbi found accounts where reconciliation totals are unconsistent "
+	      "with the sum of reconcilied transactions.  Generally, the cause is "
+	      "too many transfers to other accounts are reconciled.\n"
+	      "The following accounts are unconsistent:\n\n"), 
+	    pText, NULL );
+
+      if ( affected_accounts > 1 )
+	  pHint = g_strdup_printf ( _("%d accounts have unconsistencies."), 
+				    affected_accounts );
+      else
+	  pHint = _("An account has unconsistencies.");
+
+      dialogue_warning_hint ( pText, pHint );
+
       free ( pText );
+      free ( pHint );
     }
-  }
-  g_slist_free ( pUserAccountsList );
+    
+    g_slist_free ( pUserAccountsList );
 }
