2618a8a5a052159b0ba3f504c40ce0a19d4b9259
Alain Portal <dionysos-sf@users.sourceforge.net>
2005-01-11 13:52:04 +0000

    Print cheque number in reports for breakdown of transaction. Close #508.

--- src/etats_affiche.c
+++ src/etats_affiche.c
@@ -1,24 +1,28 @@
-/*  Fichier qui s'occupe d'afficher les états */
-
+/* ************************************************************************** */
+/* Fichier qui s'occupe d'afficher les états                                  */
+/*                                                                            */
+/*                            etats_affiche.c                                 */
+/*                                                                            */
 /*     Copyright (C)	2000-2003 Cédric Auger (cedric@grisbi.org)	      */
 /*			2004 Benjamin Drieu (bdrieu@april.org)		      */
-/* 			http://www.grisbi.org				      */
-
-/*     This program is free software; you can redistribute it and/or modify */
-/*     it under the terms of the GNU General Public License as published by */
-/*     the Free Software Foundation; either version 2 of the License, or */
-/*     (at your option) any later version. */
-
-/*     This program is distributed in the hope that it will be useful, */
-/*     but WITHOUT ANY WARRANTY; without even the implied warranty of */
-/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
-/*     GNU General Public License for more details. */
-
-/*     You should have received a copy of the GNU General Public License */
-/*     along with this program; if not, write to the Free Software */
-/*     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
-
-
+/*			2005 Alain Portal (dionysos@grisbi.org)		      */
+/*			http://www.grisbi.org				      */
+/*                                                                            */
+/*  This program is free software; you can redistribute it and/or modify      */
+/*  it under the terms of the GNU General Public License as published by      */
+/*  the Free Software Foundation; either version 2 of the License, or         */
+/*  (at your option) any later version.                                       */
+/*                                                                            */
+/*  This program is distributed in the hope that it will be useful,           */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*  GNU General Public License for more details.                              */
+/*                                                                            */
+/*  You should have received a copy of the GNU General Public License         */
+/*  along with this program; if not, write to the Free Software               */
+/*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "include.h"
 
@@ -1326,6 +1330,37 @@ gint etat_affiche_affichage_ligne_ope ( struct structure_operation *operation,
 
 	if ( etat_courant -> afficher_cheque_ope )
 	{
+	    /* Si l'opération est une opération de ventilation, il faut rechercher
+	       l'opération mère pour pouvoir récupérer le n° du chèque */
+	    if ( operation -> no_operation_ventilee_associee )
+	    {
+		gpointer **save_ptab;
+		GSList *pTransactionList;
+		gboolean found = FALSE;
+
+		/* On récupère donc la liste des opérations du compte et on en fait
+		   le tour jusqu'à ce qu'on trouve l'opération mère */
+		save_ptab = p_tab_nom_de_compte_variable;
+		p_tab_nom_de_compte_variable = p_tab_nom_de_compte + operation -> no_compte;
+		pTransactionList = LISTE_OPERATIONS;
+		while ( pTransactionList && !found )
+		{
+		    struct structure_operation *pTransaction;
+
+		    pTransaction = pTransactionList -> data;
+		    
+		    if ( pTransaction -> operation_ventilee &&
+			 pTransaction -> no_operation == operation -> no_operation_ventilee_associee &&
+			 pTransaction -> contenu_type )
+		    {
+			operation -> contenu_type = pTransaction -> contenu_type;
+			found = TRUE;
+		    }
+		    pTransactionList = pTransactionList -> next;
+		}
+		p_tab_nom_de_compte_variable = save_ptab;
+	    }
+	    
 	    if ( operation -> contenu_type )
 	    {
 		text = operation -> contenu_type;
