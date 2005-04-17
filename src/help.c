df9c99d044d86eb2305a732437cff6e38f62a3c2
Benjamin Drieu <benj2@users.sourceforge.net>
2005-04-17 15:33:57 +0000

    Update authors

--- src/help.c
+++ src/help.c
@@ -44,7 +44,7 @@ void a_propos ( GtkWidget *bouton,
     const gchar *generalites = _("Grisbi is a personnal accounting application for Linux\nSuch a program allows you to sort your financial transactions, whatever they are, in a convenient and intuitive way.  Thus, you will be able to manage them as to fit your needs the more you can.\nGrisbi aims both at simplicty and efficiency for common use, and at a powerful use for power users.  We will always try to respect this as features will come.");
 
     const gchar *auteurs[] = { 
-      _("Alain Pichon (alain.pichon[at]grisbi.org): RHEL and Fedora packaging"),
+      _("Alain Pichon (aph[at]grisbi.org): RHEL and Fedora packaging"),
       _("Alain Portal <aportal AT univ-monpt2 DOT fr>: Programming, manual"),
       _("Andre Pascual (andre[at]linuxgraphic.org): Logo"),
       _("Axel Rousseau (axel584[at]axel584.org): contributor"),
