741a8d46fab84ec986a57f3643a2b3ced2feb23d
Benjamin Drieu <benj2@users.sourceforge.net>
2005-06-09 11:31:54 +0000

    Move structure to private, since it should not be required anymore from header files

--- src/gsb_transaction_data.c
+++ src/gsb_transaction_data.c
@@ -42,6 +42,54 @@
 #include "structures.h"
 /*END_INCLUDE*/
 
+
+/**
+ * \struct 
+ * Describe a transaction
+ */
+typedef struct
+{
+    /** @name general stuff */
+    gint transaction_number;
+    gchar *transaction_id;				/**< filled by ofx */
+    gint account_number;
+    gdouble transaction_amount;
+    gint party_number;
+    gchar *notes;
+    gint marked_transaction;				/**<  0=nothing, 1=P, 2=T, 3=R */
+    gshort automatic_transaction;			/**< 0=manual, 1=automatic (scheduled transaction) */
+    gint reconcile_number;
+    guint financial_year_number;
+    gchar *voucher;
+    gchar *bank_references;
+
+    /** @name dates of the transaction */
+    GDate *date;
+    GDate *value_date;
+
+    /** @name currency stuff */
+    gint currency_number;
+    gint change_between_account_and_transaction;	/**< if 1 : 1 account_currency = (exchange_rate * amount) transaction_currency */
+    gdouble exchange_rate;
+    gdouble exchange_fees;
+
+    /** @name category stuff */
+    gint category_number;
+    gint sub_category_number;
+    gint budgetary_number;
+    gint sub_budgetary_number;
+    gint transaction_number_transfer;
+    gint account_number_transfer;			/**< -1 for a deleted account */
+    gint breakdown_of_transaction;			/**< 1 if it's a breakdown of transaction */
+    gint mother_transaction_number;			/**< for a breakdown, the mother's transaction number */
+
+    /** @name method of payment */
+    gint method_of_payment_number;
+    gchar *method_of_payment_content;
+
+} struct_transaction;
+
+
 /*START_STATIC*/
 static gint gsb_transaction_data_get_last_white_number (void);
 static struct_transaction *gsb_transaction_data_get_transaction_by_no ( gint no_transaction );
@@ -50,6 +98,7 @@ static gboolean gsb_transaction_data_save_transaction_pointer ( gpointer transac
 
 /*START_EXTERN*/
 /*END_EXTERN*/
+
 
 /** the g_slist which contains all the transactions structures */
 static GSList *transactions_list;
--- src/gsb_transaction_data.h
+++ src/gsb_transaction_data.h
@@ -2,54 +2,6 @@
 #define _GSB_TRANSACTION_DATA_H (1)
 
 
-/** \struct
- * describe a transaction 
- * */
-
-typedef struct
-{
-    /** @name general stuff */
-    gint transaction_number;
-    gchar *transaction_id;				/**< filled by ofx */
-    gint account_number;
-    gdouble transaction_amount;
-    gint party_number;
-    gchar *notes;
-    gint marked_transaction;				/**<  0=nothing, 1=P, 2=T, 3=R */
-    gshort automatic_transaction;			/**< 0=manual, 1=automatic (scheduled transaction) */
-    gint reconcile_number;
-    guint financial_year_number;
-    gchar *voucher;
-    gchar *bank_references;
-
-    /** @name dates of the transaction */
-
-    GDate *date;
-    GDate *value_date;
-
-    /** @name currency stuff */
-    gint currency_number;
-    gint change_between_account_and_transaction;	/**< if 1 : 1 account_currency = (exchange_rate * amount) transaction_currency */
-    gdouble exchange_rate;
-    gdouble exchange_fees;
-
-    /** @name category stuff */
-    gint category_number;
-    gint sub_category_number;
-    gint budgetary_number;
-    gint sub_budgetary_number;
-    gint transaction_number_transfer;
-    gint account_number_transfer;			/**< -1 for a deleted account */
-    gint breakdown_of_transaction;			/**< 1 if it's a breakdown of transaction */
-    gint mother_transaction_number;			/**< for a breakdown, the mother's transaction number */
-
-    /** @name method of payment */
-    gint method_of_payment_number;
-    gchar *method_of_payment_content;
-
-} struct_transaction;
-
-
 /* START_INCLUDE_H */
 #include "gsb_transaction_data.h"
 /* END_INCLUDE_H */
