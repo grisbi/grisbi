6b4e1b00f2768b84b9f01e1ce986069dd395066c
Benjamin Drieu <benj2@users.sourceforge.net>
2005-07-02 13:28:01 +0000

    Let compilation work even if libofx is not instaled or activated

--- src/include.h
+++ src/include.h
@@ -35,8 +35,26 @@
 
 
 /* to avoid an error because of mk_include and g2banking */
-
 #ifndef HAVE_G2BANKING
 #define AB_BANKING void
 #endif
 
+
+/* Define gettext functions */
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  define _(String) gettext(String)
+#  ifdef gettext_noop
+#    define N_(String) gettext_noop(String)
+#  else
+#    define N_(String) (String)
+#  endif /* gettext_noop */
+#else
+#  define _(String) (String)
+#  define N_(String) (String)
+#  define textdomain(String) (String)
+#  define gettext(String) (String)
+#  define dgettext(Domain,String) (String)
+#  define dcgettext(Domain,String,Type) (String)
+#  define bindtextdomain(Domain,Directory) (Domain) 
+#endif /* ENABLE_NLS */
--- src/structures.h
+++ src/structures.h
@@ -53,25 +53,6 @@
 #define PRESPACIFY(s) (g_strconcat ( " ", s, NULL ))
 #define POSTSPACIFY(s) (g_strconcat ( s, " ", NULL ))
 
-#ifdef ENABLE_NLS
-#  include <libintl.h>
-#  define _(String) gettext(String)
-#  ifdef gettext_noop
-#    define N_(String) gettext_noop(String)
-#  else
-#    define N_(String) (String)
-#  endif /* gettext_noop */
-#else
-#  define _(String) (String)
-#  define N_(String) (String)
-#  define textdomain(String) (String)
-#  define gettext(String) (String)
-#  define dgettext(Domain,String) (String)
-#  define dcgettext(Domain,String,Type) (String)
-#  define bindtextdomain(Domain,Directory) (Domain) 
-#endif /* ENABLE_NLS */
-
-
 
 /* variables contenant juste 0 ou 1 */
 
