1216e6d27b484af9a6716e3d4a8a252ea482eac4
Benjamin Drieu <benj2@users.sourceforge.net>
2005-07-02 13:28:34 +0000

    Let compilation work even if libofx is not instaled or activated

--- src/include.h
+++ src/include.h
@@ -32,3 +32,22 @@
 #include <gtk/gtktext.h>
 #include <gdk/gdkkeysyms.h>
 
+
+
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  define _(String) gettext(String)
+#  ifdef gettext_noop
+#    define N_(String) gettext_noop(String)
+#  else
+#    define N_(String) (String)
+#  endif /* gettext_noop */
+#else
+#  define _(String) (String)
+#  define N_(String) (String)
+#  define textdomain(String) (String)
+#  define gettext(String) (String)
+#  define dgettext(Domain,String) (String)
+#  define dcgettext(Domain,String,Type) (String)
+#  define bindtextdomain(Domain,Directory) (Domain) 
+#endif /* ENABLE_NLS */
