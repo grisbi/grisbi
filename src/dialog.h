e9fd718268e2cda2609ec1d29e38604b85a16d1b
Benjamin Drieu <benj2@users.sourceforge.net>
2005-07-04 17:51:34 +0000

    Add conditional_message

--- src/dialog.h
+++ src/dialog.h
@@ -9,9 +9,9 @@
 gboolean blocage_boites_dialogues ( GtkWidget *dialog,
 				    gpointer null );
 void dialogue ( gchar *texte_dialogue );
-void dialogue_conditional ( gchar *text, int * var );
-void dialogue_conditional_hint ( gchar *hint, gchar * text, int * var );
-void dialogue_conditional_info_hint ( gchar *hint, gchar * text, int * var );
+void dialogue_conditional ( gchar *text, gchar * var );
+void dialogue_conditional_hint ( gchar *hint, gchar * text, gchar * var );
+void dialogue_conditional_info_hint ( gchar *hint, gchar * text, gchar * var );
 void dialogue_error ( gchar *text );
 void dialogue_error_brain_damage ();
 void dialogue_error_hint ( gchar *text, gchar * hint );
@@ -25,5 +25,16 @@ void dialogue_warning_hint ( gchar *text, gchar * hint );
 gchar * make_hint ( gchar * hint, gchar * text );
 gboolean question ( gchar *texte );
 gboolean question_yes_no_hint ( gchar * hint, gchar *texte );
+void dialog_message ( gchar * label );
 /* END_DECLARATION */
+
+struct conditional_message
+{
+    gchar * name;
+    gchar * hint;
+    gchar * message;
+    gboolean default_state;
+    gboolean hidden;
+};
+
 #endif
