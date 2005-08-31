571b9246b3970a718f7e38d596242a0bddf3727e
Benjamin Drieu <benj2@users.sourceforge.net>
2005-08-31 14:37:41 +0000

    Remove useless tab

--- src/parametres.c
+++ src/parametres.c
@@ -315,14 +315,6 @@ void preferences ( gint page )
     gtk_tree_store_append (GTK_TREE_STORE (preference_tree_model), &iter2, &iter);
     gtk_tree_store_set (GTK_TREE_STORE (preference_tree_model),
 			&iter2,
-			0, _("Informations shown"),
-			1, TRANSACTIONS_LIST_PAGE,
-			-1);
-    gtk_notebook_append_page (preference_frame, onglet_affichage_liste(), NULL);
-
-    gtk_tree_store_append (GTK_TREE_STORE (preference_tree_model), &iter2, &iter);
-    gtk_tree_store_set (GTK_TREE_STORE (preference_tree_model),
-			&iter2,
 			0, _("List behavior"),
 			1, TRANSACTIONS_PAGE,
 			-1);
