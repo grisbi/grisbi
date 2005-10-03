1808ae326c1bab48b3f8070ccbf63bc086e276bb
Benjamin Drieu <benj2@users.sourceforge.net>
2005-10-03 22:56:20 +0000

    New file

---
+++ src/csv_parse.c
@@ -0,0 +1,445 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*     Copyright (C)	     2005 Benjamin Drieu (bdrieu@april.org)	      */
+/* 			http://www.grisbi.org				      */
+/*                                                                            */
+/*  This program is free software; you can redistribute it and/or modify      */
+/*  it under the terms of the GNU General Public License as published by      */
+/*  the Free Software Foundation; either version 2 of the License, or         */
+/*  (at your option) any later version.                                       */
+/*                                                                            */
+/*  This program is distributed in the hope that it will be useful,           */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*  GNU General Public License for more details.                              */
+/*                                                                            */
+/*  You should have received a copy of the GNU General Public License         */
+/*  along with this program; if not, write to the Free Software               */
+/*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include "include.h"
+
+/*START_INCLUDE*/
+#include "csv_parse.h"
+#include "utils_dates.h"
+#include "utils_str.h"
+#include "csv.h"
+#include "structures.h"
+/*END_INCLUDE*/
+
+/*START_EXTERN*/
+extern FILE * out;
+/*END_EXTERN*/
+
+/*START_STATIC*/
+static gboolean csv_import_valid_date ( gchar * string );
+static gchar * sanitize_field ( gchar * begin, gchar * end  );
+/*END_STATIC*/
+
+
+
+/**
+ * TODO
+ *
+ *
+ */
+GSList * csv_parse_line ( gchar * contents[], gchar * separator )
+{
+    gchar * tmp = (* contents), * begin = tmp;
+    gint is_unquoted = FALSE, len = strlen ( separator );
+    GSList * list = NULL;
+
+    if ( *tmp == '\n' )
+    {
+	*contents = tmp+1;
+	return GINT_TO_POINTER(-1);
+    }
+    
+    if ( *tmp == '!' || *tmp == '#' || *tmp == ';' )
+    {
+	*contents = strchr ( tmp, '\n' ) + 1;
+	return GINT_TO_POINTER(-1);
+    }
+
+    while ( *tmp )
+    {
+	switch ( *tmp )
+	{
+	    case '\n':
+		list = g_slist_append ( list, sanitize_field ( begin, tmp ) );
+		*contents = tmp+1;
+		return list;
+
+	    case '"':
+		if ( ! is_unquoted )
+		{
+		    tmp++;
+		    while ( *tmp )
+		    {
+			/* Thsi is lame escaping but we need to
+			 * support it. */
+			if ( *tmp == '\\' && *(tmp+1) == '"' )
+			{
+			    tmp += 2;
+			}
+
+			/* End of quoted string. */
+			if ( *tmp == '"' && *(tmp+1) != '"' )
+			{
+			    tmp += 1;
+			    break;
+			}
+			
+			tmp++;
+		    }
+		}
+
+	    default:
+		is_unquoted = TRUE;
+		if ( !strncmp ( tmp, separator, len ) )
+		{
+		    list = g_slist_append ( list, sanitize_field ( begin, tmp ) );
+		    begin = tmp + len;
+		    is_unquoted = FALSE;
+		}
+		break;
+	}
+
+	tmp++;
+    }
+
+    return NULL;
+}
+
+
+
+/**
+ * TODO
+ *
+ */
+gchar * sanitize_field ( gchar * begin, gchar * end  )
+{
+    gchar * field, * iter;
+
+    g_return_val_if_fail ( begin <= end, NULL );
+
+    if ( begin == end )
+	return "";
+
+    iter = field = g_malloc ( end - begin );
+
+    /* Strip out intial white spaces. */
+    while ( *begin == ' ' )
+	begin++;
+
+    if ( *begin == '"' )
+    {
+	begin++;
+	while ( *end != '"' && end >= begin )
+	    end--;
+    }
+
+    while ( begin < end )
+    {
+	if ( *begin == '"' && *(begin+1) == '"' )
+	    begin++;
+
+	if ( *begin == '\\' && *(begin+1) == '"' )
+	    begin++;
+	
+	*iter++ = *begin++;
+    }
+
+    /* Strip out remaining white spaces. */
+    while ( *(iter-1) == ' ' || *(iter-1) == '\n' )
+	iter--;
+
+    *iter = '\0';
+
+    return field;
+}
+
+
+
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_validate_string ( gchar * string )
+{
+    g_return_if_fail ( string );
+
+    return string != NULL;
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_validate_date ( gchar * string )
+{
+    GDate * date;
+    g_return_if_fail ( string );
+    
+    date = gsb_parse_date_string ( string );
+
+    return date &&  g_date_valid ( date );
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_validate_number ( gchar * string )
+{
+    g_return_if_fail ( string );
+
+    while ( *string )
+    {
+	if ( ! g_ascii_isdigit ( * string ) ||
+	     ! g_ascii_isspace ( * string ) )
+	{
+	    return FALSE;
+	}
+    }
+
+    return TRUE;
+}
+
+
+
+/**
+ *
+ * We don't check punctuation signs because mileage may vary with
+ * locales.
+ */
+gboolean csv_import_validate_amount ( gchar * string )
+{
+    g_return_if_fail ( string );
+
+    while ( *string )
+    {
+	if ( g_ascii_isalpha ( * string ) )
+	{
+	    return FALSE;
+	}
+
+	string++;
+    }
+
+    return TRUE;
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_valid_date ( gchar * string )
+{
+    g_return_if_fail ( string );
+    return g_date_valid ( gsb_parse_date_string ( string ) );
+}
+
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_currency ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    ope -> date_de_valeur = gsb_parse_date_string ( string );
+    return g_date_valid ( ope -> date_de_valeur );
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_date ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    ope -> date = gsb_parse_date_string ( string );
+    return g_date_valid ( ope -> date );
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_value_date ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    ope -> date_de_valeur = gsb_parse_date_string ( string );
+    return g_date_valid ( ope -> date_de_valeur );
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_payee ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    ope -> tiers = g_strdup ( string );
+    return TRUE;
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_notes ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    ope -> notes = g_strdup ( string );
+    return TRUE;
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_voucher ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    ope -> cheque = my_strtod ( string, NULL );
+    return TRUE;
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_category ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    ope -> categ = g_strdup ( string );
+    return TRUE;
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_sub_category ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    if ( ! ope -> categ )
+	return FALSE;
+    ope -> categ = g_strconcat ( ope -> categ, " : ", string, NULL );
+    return TRUE;
+}
+
+
+
+/**
+ * TODO: Use  a real parsing
+ *
+ */
+gboolean csv_import_parse_amount ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    if ( strlen ( string ) > 0 )
+    {
+	ope -> montant = my_strtod ( string, NULL );
+    }
+    return TRUE;
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_credit ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+    if ( strlen ( string ) > 0 )
+    {
+	ope -> montant = my_strtod ( string, NULL );
+    }
+    return TRUE;
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_debit ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+
+    if ( strlen ( string ) > 0 )
+    {
+	ope -> montant = - my_strtod ( string, NULL );
+    }
+    return TRUE;
+}
+
+
+
+/**
+ *
+ *
+ */
+gboolean csv_import_parse_p_r ( struct struct_ope_importation * ope, gchar * string )
+{
+    g_return_if_fail ( string );
+
+    if ( ! strcmp ( string, "P" ) )
+    {
+	ope -> p_r = OPERATION_POINTEE;
+    }
+    else if ( ! strcmp ( string, "T" ) )
+    {
+	ope -> p_r = OPERATION_TELERAPPROCHEE;
+    }
+    else if ( ! strcmp ( string, "R" ) )
+    {
+	ope -> p_r = OPERATION_RAPPROCHEE;
+    }
+    else
+    {
+	ope -> p_r = OPERATION_NORMALE;
+    }
+    return TRUE;
+}
+
+
+
+/* Local Variables: */
+/* c-basic-offset: 4 */
+/* End: */
---
+++ src/csv_parse.h
@@ -0,0 +1,29 @@
+#ifndef CSV_PARSE_H
+#define CSV_PARSE_H
+
+/* START_INCLUDE_H */
+#include "csv_parse.h"
+#include "structures.h"
+/* END_INCLUDE_H */
+
+/* START_DECLARATION */
+gboolean csv_import_parse_amount ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_category ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_credit ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_currency ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_date ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_debit ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_notes ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_p_r ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_payee ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_sub_category ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_value_date ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_parse_voucher ( struct struct_ope_importation * ope, gchar * string );
+gboolean csv_import_validate_amount ( gchar * string );
+gboolean csv_import_validate_date ( gchar * string );
+gboolean csv_import_validate_number ( gchar * string );
+gboolean csv_import_validate_string ( gchar * string );
+GSList * csv_parse_line ( gchar * contents[], gchar * separator );
+/* END_DECLARATION */
+
+#endif
