a9d3c6e2862c71213430f8cca6460a0939559fbc
Benjamin Drieu <benj2@users.sourceforge.net>
2005-10-03 22:59:13 +0000

    Add a new structure

--- src/import_csv.h
+++ src/import_csv.h
@@ -1,7 +1,9 @@
 #ifndef _IMPORT_CSV_H
 #define _IMPORT_CSV_H (1)
+
 /* START_INCLUDE_H */
 #include "import_csv.h"
+#include "structures.h"
 /* END_INCLUDE_H */
 
 
@@ -9,4 +11,12 @@
 gboolean importer_csv ();
 gboolean recuperation_donnees_csv ( FILE *fichier );
 /* END_DECLARATION */
+
+struct csv_field {
+    gboolean * name;
+    gfloat alignment;
+    gboolean (* validate ) ( gchar * );
+    gboolean (* parse ) ( struct struct_ope_importation *, gchar * );
+};
+
 #endif
