8c34055ef6fcfbff34859423dd079bb9b50f46be
Benjamin Drieu <benj2@users.sourceforge.net>
2005-10-03 22:56:50 +0000

    Fix copyright

--- src/dialog.c
+++ src/dialog.c
@@ -1,11 +1,7 @@
 /* ************************************************************************** */
-/* Fichier qui s'occupe de la page d'accueil ( de démarrage lors de           */
-/* l'ouverture d'un fichier de comptes                                        */
-/*                                                                            */
-/*                                  accueil.c                                 */
 /*                                                                            */
 /*     Copyright (C)	2000-2003 Cédric Auger (cedric@grisbi.org)	      */
-/*			2003-2004 Benjamin Drieu (bdrieu@april.org)	      */
+/*			2003-2005 Benjamin Drieu (bdrieu@april.org)	      */
 /* 			http://www.grisbi.org				      */
 /*                                                                            */
 /*  This program is free software; you can redistribute it and/or modify      */
