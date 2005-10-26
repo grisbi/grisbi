54a59c74a1feab81aeda3dab3658990b78143c5e
Benjamin Drieu <benj2@users.sourceforge.net>
2005-10-26 22:04:52 +0000

    New function

--- src/etats_onglet.h
+++ src/etats_onglet.h
@@ -11,6 +11,7 @@ void changement_etat ( gint report_number );
 GtkWidget *creation_onglet_etats ( void );
 void dupliquer_etat ( void );
 void efface_etat ( void );
+void export_etat_courant_vers_csv ( );
 void export_etat_courant_vers_html ( );
 void exporter_etat ( void );
 void importer_etat ( void );
