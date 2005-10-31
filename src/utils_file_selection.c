99548490c43880b96028adba23f93b4daa0030b4
Benjamin Drieu <benj2@users.sourceforge.net>
2005-10-31 15:46:17 +0000

    Use correct button for load and save dialogs

--- src/utils_file_selection.c
+++ src/utils_file_selection.c
@@ -184,7 +184,9 @@ GtkWidget* file_selection_new ( const gchar *title, const gint properties )
 					      GTK_FILE_CHOOSER_ACTION_SAVE :
 					      GTK_FILE_CHOOSER_ACTION_OPEN ),
 					    GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-					    GTK_STOCK_OPEN, GTK_RESPONSE_OK,
+					    ( properties & FILE_SELECTION_IS_SAVE_DIALOG ?
+					      GTK_STOCK_SAVE : GTK_STOCK_OPEN ),
+					    GTK_RESPONSE_OK,
 					    NULL );
 
     return GTK_WIDGET (filesel);
