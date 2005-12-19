bebde90fa621408b73e08e2a687acf91010cdb03
Benjamin Drieu <benj2@users.sourceforge.net>
2005-12-19 10:11:54 +0000

    Do not use translated strings for paper sizes

--- src/print_config.c
+++ src/print_config.c
@@ -37,13 +37,13 @@ gboolean print_config_radio_toggled ( GtkToggleButton * togglebutton, gpointer u
 /** This array contains paper sizes to be used to construct print
   copies of reports. */
 struct paper_config paper_sizes[8] = {
-    {N_("A3"), 297, 420},
-    {N_("A4"), 210, 297},
-    {N_("A5"), 148, 210},
-    {N_("A6"), 105, 148},
-    {N_("USletter"), 215, 279.4},
-    {N_("USlegal"), 215, 355.6},
-    {N_("USexecutive"), 184.15, 266.7},
+    {"A3", 297, 420},
+    {"A4", 210, 297},
+    {"A5", 148, 210},
+    {"A6", 105, 148},
+    {"USletter", 215, 279.4},
+    {"USlegal", 215, 355.6},
+    {"USexecutive", 184.15, 266.7},
     {NULL, 0, 0},
 };
 
