8614c3a31c4d9d3a8d22e03c6ab72de85fedbc4c
Benjamin Drieu <benj2@users.sourceforge.net>
2006-01-04 17:46:28 +0000

    Add comment

--- src/qif.c
+++ src/qif.c
@@ -1,25 +1,24 @@
-/* ce fichier de la gestion du format qif */
-
-
-/*     Copyright (C) 2000-2003  Cédric Auger */
-/* 			cedric@grisbi.org */
-/* 			http://www.grisbi.org */
-
-/*     This program is free software; you can redistribute it and/or modify */
-/*     it under the terms of the GNU General Public License as published by */
-/*     the Free Software Foundation; either version 2 of the License, or */
-/*     (at your option) any later version. */
-
-/*     This program is distributed in the hope that it will be useful, */
-/*     but WITHOUT ANY WARRANTY; without even the implied warranty of */
-/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
-/*     GNU General Public License for more details. */
-
-/*     You should have received a copy of the GNU General Public License */
-/*     along with this program; if not, write to the Free Software */
-/*     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*     Copyright (C)	2000-2003 Cédric Auger	(cedric@grisbi.org)	      */
+/*			2005-2006 Benjamin Drieu (bdrieu@april.org)	      */
+/* 			http://www.grisbi.org				      */
+/*                                                                            */
+/*  This program is free software; you can redistribute it and/or modify      */
+/*  it under the terms of the GNU General Public License as published by      */
+/*  the Free Software Foundation; either version 2 of the License, or         */
+/*  (at your option) any later version.                                       */
+/*                                                                            */
+/*  This program is distributed in the hope that it will be useful,           */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*  GNU General Public License for more details.                              */
+/*                                                                            */
+/*  You should have received a copy of the GNU General Public License         */
+/*  along with this program; if not, write to the Free Software               */
+/*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "include.h"
 
@@ -43,14 +42,26 @@
 /*START_STATIC*/
 /*END_STATIC*/
 
-
 /*START_EXTERN*/
 extern GSList *liste_comptes_importes;
 extern GSList *liste_comptes_importes_error;
 /*END_EXTERN*/
 
 
-/* *******************************************************************************/
+
+/**
+ * \brief Import QIF data.
+ *
+ * Open a QIF file and fills in data in a struct_compte_importation
+ * data structure.
+ *
+ * \param fichier	File handle of imported file, ready for
+ *			reading.
+ * \param imported	A pointer to structure containing name and
+ *			format of imported file.
+ *
+ * \return		TRUE on success.
+ */
 gboolean recuperation_donnees_qif ( FILE * fichier, struct imported_file * imported )
 {
     gchar *pointeur_char;
