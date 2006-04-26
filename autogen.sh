d4d101ad0b45c249e61823a3aabc51c10c6c2bb3
Benjamin Drieu <benj2@users.sourceforge.net>
2006-04-26 16:42:29 +0000

    Add support for libtool

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # autogen.sh glue for Grisbi
-# $Id: autogen.sh,v 1.9 2005/05/31 10:33:55 gouri48 Exp $
+# $Id: autogen.sh,v 1.10 2006/04/26 16:42:29 benj2 Exp $
 #
 # Requires: automake, autoconf, dpkg-dev
 
@@ -45,6 +45,43 @@ for i in config.guess config.sub missing install-sh mkinstalldirs ; do
 	    chmod 755 ${i}
 	fi
 done
+
+
+
+#
+# Apple's Developer Tools have a "libtool" that has nothing to do with
+# the GNU libtool; they call the latter "glibtool".  They also call
+# libtoolize "glibtoolize".
+#
+# Check for "glibtool" first.
+# Borrowed from ethereal
+#
+LTVER=`glibtool --version 2>/dev/null | grep ' libtool)' | \
+    sed 's/.*libtool) \([0-9][0-9.]*\)[^ ]* .*/\1/'`
+if test -z "$LTVER"
+then
+	LTVER=`libtool --version | grep ' libtool)' | \
+	    sed 's/.*) \([0-9][0-9.]*\)[^ ]* .*/\1/' `
+	LIBTOOLIZE=libtoolize
+else
+	LIBTOOLIZE=glibtoolize
+fi
+case "$LTVER" in
+'' | 0.* | 1.[0-3]* )
+
+  cat >&2 <<_EOF_
+
+	You must have libtool 1.4 or later installed to compile $PROJECT.
+	Download the appropriate package for your distribution/OS,
+	or get the source tarball at ftp://ftp.gnu.org/pub/gnu/libtool/
+_EOF_
+  exit 1
+  ;;
+esac
+
+LTARGS=" --copy --force"
+$LIBTOOLIZE $LTARGS
+
 
 aclocal -I macros
 autoheader
