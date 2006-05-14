ef24959f1e1f224d11d3f5f9d5d1bf96349332d2
Benjamin Drieu <benj2@users.sourceforge.net>
2006-05-14 15:02:23 +0000

    Actually code the g2bankig plugin

--- src/plugins/g2banking/g2banking.c
+++ src/plugins/g2banking/g2banking.c
@@ -21,16 +21,31 @@
 
 #include "include.h"
 
+
 /*START_INCLUDE*/
+#include "gbanking.h"
+#include "import.h"
+#include "gsb_real.h"
+#include "utils_str.h"
+#include "dialog.h"
+#include "include.h"
 /*END_INCLUDE*/
 
 /*START_EXTERN*/
+extern void gsb_import_register_account ( GSList * tmp );
 /*END_EXTERN*/
 
 /*START_STATIC*/
 /*END_STATIC*/
 
+#include <g2banking/gbanking.h>
+#include <aqbanking/imexporter.h>
+#include <gwenhywfar/debug.h>
+/* FIXME : je sais pas pourquoi c'est pas pris par mk_include... donc le met ici [cedric] */
+int GrisbiBanking_ImportContext (AB_BANKING *ab, 
+				 AB_IMEXPORTER_CONTEXT *ctx);
 
+AB_BANKING *gbanking=0;
 
 /** Module name. */
 G_MODULE_EXPORT const gchar plugin_name[] = "g2banking";
@@ -39,16 +54,208 @@ G_MODULE_EXPORT const gchar plugin_name[] = "g2banking";
 
 /** Initialization function. */
 G_MODULE_EXPORT void plugin_register () {
+    int rv;
+
     printf ("Initializating g2banking plugin\n");
+
+    gbanking=GBanking_new("grisbi", 0);
+    GBanking_SetImportContextFn(gbanking, GrisbiBanking_ImportContext);
+    rv=AB_Banking_Init(gbanking);
+    if (rv) {
+	printf (_("Could not initialize AqBanking, "
+		  "online banking will not be available\n"));
+	AB_Banking_free(gbanking);
+	gbanking=0;
+    }
+    else
+    {
+	printf ("g2banking successfully registered.");
+    }
 }
 
 
 
 /** Main function of module. */
-G_MODULE_EXPORT gint plugin_run ( gchar * file_name, gchar **file_content,
-				  gboolean crypt, gulong length )
+G_MODULE_EXPORT GtkWidget * plugin_run ( gchar * file_name, gchar **file_content,
+					 gboolean crypt, gulong length )
 {
-    return gsb_file_util_crypt_file ( file_name, file_content, crypt, length );    
+    return GBanking_JobView_new(gbanking, 0);
+}
+
+
+
+/** Release plugin  */
+G_MODULE_EXPORT gboolean plugin_release ( )
+{
+    if (gbanking) {
+	int rv;
+
+	rv=AB_Banking_Fini(gbanking);
+	if (rv) {
+	    printf (_("Could not deinitialize AqBanking (%d)\n"), rv);
+	    AB_Banking_free(gbanking);
+	    gbanking=0;
+	}
+    }
+}
+
+
+
+/**
+ *
+ *
+ *
+ */
+int GrisbiBanking_ImportContext (AB_BANKING *ab, 
+				 AB_IMEXPORTER_CONTEXT *ctx)
+{
+    AB_IMEXPORTER_ACCOUNTINFO *ai;
+    GSList *liste_comptes_importes_gbanking;
+    int errors;
+
+    errors=0;
+    liste_comptes_importes_gbanking=NULL;
+    ai=AB_ImExporterContext_GetFirstAccountInfo(ctx);
+    while(ai)
+    {
+	struct struct_compte_importation *compte_nouveau;
+	const char *s;
+	AB_TRANSACTION *t;
+
+	fprintf(stderr, "Importing account.\n");
+	compte_nouveau=calloc(1, sizeof(struct struct_compte_importation));
+	s=AB_ImExporterAccountInfo_GetAccountNumber(ai);
+	if (s)
+	    compte_nouveau->id_compte=my_strdup(s);
+	s=AB_ImExporterAccountInfo_GetAccountName(ai);
+	if (s)
+	    compte_nouveau->nom_de_compte=my_strdup(s);
+	compte_nouveau->origine=TYPE_GBANKING;
+
+	compte_nouveau -> action = 0 ; /** Means create account every time.  */
+
+	t=AB_ImExporterAccountInfo_GetFirstTransaction(ai);
+	while(t) {
+	    struct struct_ope_importation *ope_import;
+	    const GWEN_TIME *ti;
+	    const AB_VALUE *va;
+	    const GWEN_STRINGLIST *sl;
+
+	    ope_import=calloc(1, sizeof(struct struct_ope_importation));
+
+	    /* date */
+	    ope_import->date_de_valeur=NULL;
+	    ti=AB_Transaction_GetValutaDate(t);
+	    if (ti) {
+		int year, month, day;
+
+		if (!GWEN_Time_GetBrokenDownDate(ti, &day, &month, &year)) {
+		    GDate *date;
+
+		    date=g_date_new_dmy(day, month+1, year);
+		    ope_import->date_de_valeur=date;
+		}
+	    }
+
+	    ope_import->date=NULL;
+	    ti=AB_Transaction_GetDate(t);
+	    if (ti) {
+		int year, month, day;
+
+		if (!GWEN_Time_GetBrokenDownDate(ti, &day, &month, &year)) {
+		    GDate *date;
+
+		    date=g_date_new_dmy(day, month+1, year);
+		    ope_import->date=date;
+		}
+	    }
+
+	    va=AB_Transaction_GetValue(t);
+	    if (va) {
+		ope_import->montant=gsb_real_double_to_real (AB_Value_GetValue(va));
+	    }
+
+	    sl=AB_Transaction_GetRemoteName(t);
+	    if (sl) {
+		GWEN_BUFFER *nbuf;
+		GWEN_STRINGLISTENTRY *se;
+
+		nbuf=GWEN_Buffer_new(0, 256, 0, 1);
+		se=GWEN_StringList_FirstEntry(sl);
+		while(se) {
+		    const char *s;
+
+		    s=GWEN_StringListEntry_Data(se);
+		    assert(s);
+		    if (GWEN_Buffer_GetUsedBytes(nbuf))
+			GWEN_Buffer_AppendByte(nbuf, ' ');
+		    GWEN_Buffer_AppendString(nbuf, s);
+		    se=GWEN_StringListEntry_Next(se);
+		} /* while */
+		if (GWEN_Buffer_GetUsedBytes(nbuf))
+		    ope_import->tiers=my_strdup(GWEN_Buffer_GetStart(nbuf));
+		GWEN_Buffer_free(nbuf);
+	    } /* if remote name */
+
+	    sl=AB_Transaction_GetPurpose(t);
+	    if (sl) {
+		GWEN_BUFFER *nbuf;
+		GWEN_STRINGLISTENTRY *se;
+
+		nbuf=GWEN_Buffer_new(0, 256, 0, 1);
+		se=GWEN_StringList_FirstEntry(sl);
+		while(se) {
+		    const char *s;
+
+		    s=GWEN_StringListEntry_Data(se);
+		    assert(s);
+		    if (GWEN_Buffer_GetUsedBytes(nbuf))
+			GWEN_Buffer_AppendByte(nbuf, ' ');
+		    GWEN_Buffer_AppendString(nbuf, s);
+		    se=GWEN_StringListEntry_Next(se);
+		} /* while */
+		if (GWEN_Buffer_GetUsedBytes(nbuf))
+		    ope_import->notes=my_strdup(GWEN_Buffer_GetStart(nbuf));
+		GWEN_Buffer_free(nbuf);
+	    } /* if purpose */
+
+	    /* append transaction */
+	    DBG_NOTICE(0, "Adding transaction");
+	    compte_nouveau->operations_importees=
+		g_slist_append(compte_nouveau->operations_importees,
+			       ope_import);
+
+	    t=AB_ImExporterAccountInfo_GetNextTransaction(ai);
+	} /* while */
+
+	/* append account */
+	liste_comptes_importes_gbanking=
+	    g_slist_append(liste_comptes_importes_gbanking,
+			   compte_nouveau);
+	ai=AB_ImExporterContext_GetNextAccountInfo(ctx);
+    } /* while */
+
+    if (errors)
+	errors=!question_yes_no_hint(_("Warning" ),
+				     _("An error or warning has occured. "
+				       "Do you still want to import the data ?"));
+    if (!errors)
+    {
+	GSList *liste_tmp;
+
+	liste_tmp = liste_comptes_importes_gbanking;
+
+	/* ajoute le ou les compte aux autres comptes importés */
+
+	while (liste_tmp){
+	    gsb_import_register_account ( liste_tmp->data );
+	    liste_tmp=liste_tmp->next;
+	}
+
+	traitement_operations_importees ();
+    }
+
+    return 0;
 }
 
 
