d33ca96f6ea9092ef7e1149d40d467050394d8eb
Benjamin Drieu <benj2@users.sourceforge.net>
2006-05-15 16:32:28 +0000

    Implement OFX import on plugin

--- src/plugins/ofx/ofx.h
+++ src/plugins/ofx/ofx.h
@@ -7,4 +7,35 @@
 /* START_DECLARATION */
 /* END_DECLARATION */
 
+
+
+#ifndef _WIN32
+/* On Windows, the Ofx Severity enumerate values are already used in wingdi.h, DELETE is used in winnt.h
+ * This is a work around to this issues :
+ *  INFO, WARN, ERROR, DELETE and REPLACE are defined in standard libofx.h;
+ *  on windows there should be prefixed by OFX_ 
+ */
+
+#ifndef OFX_INFO
+#define OFX_INFO    INFO
 #endif
+
+#ifndef OFX_WARN
+#define OFX_WARN    WARN
+#endif
+
+#ifndef OFX_ERROR
+#define OFX_ERROR   ERROR
+#endif
+
+#ifndef OFX_DELETE
+#define OFX_DELETE  DELETE
+#endif
+
+#ifndef OFX_REPLACE
+#define OFX_REPLACE REPLACE
+#endif
+
+#endif /* _WIN32 */
+
+#endif /* GSB_OFX_H */
