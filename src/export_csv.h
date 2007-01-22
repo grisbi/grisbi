2b117332867efa0e8d4fa043f7655cc3b7c5108f
Benjamin Drieu <benj2@users.sourceforge.net>
2007-01-22 15:15:45 +0000

    Declare protoype

--- src/export_csv.h
+++ src/export_csv.h
@@ -1,2 +1,15 @@
+#ifndef EXPORT_CSV_H
+#define EXPORT_CSV_H
+
+/* START_INCLUDE_H */
+#include "print_config.h"
+/* END_INCLUDE_H */
+
+
+/*START_DECLARATION*/
+void csv_export ( gchar * filename, gint account_nb );
 void export_accounts_to_csv ( GSList*  );
+/*END_DECLARATION*/
+
+#endif
 
