00f11a9c3af051c74a5fe821ace5fc1521e1d96d
Gérard Niel <gegeweb@users.sourceforge.net>
2009-03-31 19:23:55 +0000

    fix compilation error under Mac OS X 10.5.*

--- src/metatree.h
+++ src/metatree.h
@@ -113,7 +113,7 @@ void metatree_remove_transaction ( GtkTreeView * tree_view,
 gboolean metatree_selection_changed ( GtkTreeSelection * selection, GtkTreeModel * model );
 void metatree_set_linked_widgets_sensitive ( GtkTreeModel * model, gboolean sensitive,
 					     gchar * link_type );
-inline gboolean metatree_sort_column  ( GtkTreeModel * model, 
+gboolean metatree_sort_column  ( GtkTreeModel * model, 
 					GtkTreeIter * a, GtkTreeIter * b, 
 					gpointer user_data );
 gboolean supprimer_division ( GtkTreeView * tree_view );
