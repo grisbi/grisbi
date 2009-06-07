6d403de7a1978c33810ca022da259521f6f4fbb6
Mickaël Remars <mykeul@users.sourceforge.net>
2009-06-07 21:55:11 +0000

    Removed an unneeded directory separator that windows doesn't like

--- src/gsb_file_load.c
+++ src/gsb_file_load.c
@@ -7917,7 +7917,7 @@ void gsb_file_load_copy_old_file ( gchar *filename, gchar *file_content)
         copy_old_filename = g_path_get_basename ( filename );
         copy_old_filename = gsb_string_remplace_string ( copy_old_filename, ".gsb",
                         "-old-version.gsb" );
-        copy_old_filename = g_build_filename ( G_DIR_SEPARATOR_S, 
+        copy_old_filename = g_build_filename (
                         my_get_XDG_grisbi_data_dir (),
                         copy_old_filename, NULL );
 
