9c54bba7c7ec89b530af8ba9f851a9cb48302aea
Mickaël Remars <mykeul@users.sourceforge.net>
2009-08-25 09:49:25 +0000

    add gtk's dlls to the zip

--- win32-msvc/package.cmd
+++ win32-msvc/package.cmd
@@ -2,7 +2,8 @@
 set DIR=target\zip
 mkdir %DIR%
 
-xcopy /Y Release\*.exe %DIR%
+xcopy /Y target\Release\*.dll %DIR%
+xcopy /Y target\Release\*.exe %DIR%
 
 xcopy /S /Y ..\help %DIR%\help\
 call tools\dos2unix.cmd ..\help\tips\tips.txt %DIR%\help\tips\tips.txt
