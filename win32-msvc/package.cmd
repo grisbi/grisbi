960919770bf72ddf795d082d73f762a09c988931
Mickaël Remars <mykeul@users.sourceforge.net>
2009-09-21 08:56:10 +0000

    Generates grisbi.mo files and includes them in the zip

--- win32-msvc/package.cmd
+++ win32-msvc/package.cmd
@@ -12,6 +12,11 @@ call tools\dos2unix.cmd ..\help\tips\tips.txt %DIR%\help\tips\tips.txt
 
 xcopy /S /Y ..\pixmaps\*.png %DIR%\pixmaps\
 
+for %%f in (CS DA DE EL EO ES FA FR HE IT NL PL PT_BR RO RU ZH_CN) do (
+	mkdir target\zip\lib\locale\%%f\LC_MESSAGES
+	target\gtk-dev\bin\msgfmt.exe ..\po\%%f.po --output-file=target\zip\lib\locale\%%f\LC_MESSAGES\grisbi.mo
+)
+
 cd %DIR%
 zip -r ..\grisbi *
 pause
