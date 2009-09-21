136511ad5a1606f4de22d17158e6dba3b6ca0846
Mickaël Remars <mykeul@users.sourceforge.net>
2009-09-21 07:58:15 +0000

    Fixed windows packaging script: multiple gtk files were missing

--- win32-msvc/package.cmd
+++ win32-msvc/package.cmd
@@ -4,8 +4,8 @@ mkdir %DIR%
 
 xcopy /Y target\Release\*.dll %DIR%
 xcopy /Y target\Release\*.exe %DIR%
-xcopy /Y target\Release\etc %DIR%\etc\
-xcopy /Y target\Release\lib %DIR%\lib\
+xcopy /S /Y target\Release\etc %DIR%\etc\
+xcopy /S /Y target\Release\lib %DIR%\lib\
 
 xcopy /S /Y ..\help %DIR%\help\
 call tools\dos2unix.cmd ..\help\tips\tips.txt %DIR%\help\tips\tips.txt
