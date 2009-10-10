ecebf0c0ecb6a5c27ea3704630a4cad6ab891d89
William Ollivier <guneemwelloeux@users.sourceforge.net>
2009-10-10 22:43:59 +0000

    VERSION prend une nouvelle valeur

--- win32-msvc/config.h
+++ win32-msvc/config.h
@@ -1,7 +1,7 @@
 #ifndef _WIN32_MSVC_CONFIG_H
 #define _WIN32_MSVC_CONFIG_H
 
-#define VERSION	"0.6.0-grisbi for Windows (GTK 2.12.9)"
+#define VERSION	"0.6.0-grisbi for Windows"
 #define PACKAGE	"grisbi"
 
 #include "win32gtkutils.h"
