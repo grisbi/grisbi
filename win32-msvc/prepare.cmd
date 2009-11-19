b9d6480901b0345320d4844a9250e78858010acf
William Ollivier <guneemwelloeux@users.sourceforge.net>
2009-11-19 23:04:52 +0000

    Several fixes to the Makefile Modified prepare script for lazy compilation

--- win32-msvc/prepare.cmd
+++ win32-msvc/prepare.cmd
@@ -11,10 +11,31 @@ SET GTK_BIN_FILE=gtk2-runtime-2.16.5-2009-08-06-ash.exe
 SET CURRENT_DIR=%CD%
 cd ..
 wget -nc -c "http://sourceforge.net/projects/gtk-win/files/GTK+ Runtime Environment/GTK+ 2.16/%GTK_BIN_FILE%/download"
-./%GTK_BIN_FILE%
+.\%GTK_BIN_FILE%
 cd %CURRENT_DIR%
 mkdir target
 cd target
+
+:: Download and unzip libxml2 dev and bin files
+wget -nc -c ftp://ftp.zlatkovic.com/libxml/libxml2-2.7.6.win32.zip
+unzip -uo libxml2-2.7.6.win32.zip -d plugins-dev
+
+:: Download and install openssl, copy the required files in the right place
+wget -nc -c http://www.slproweb.com/download/Win32OpenSSL-0_9_8l.exe
+ECHO ***** ATTENTION: Do not modify the default installation path for OpenSSL otherwise you will have trouble compiling *****
+.\Win32OpenSSL-0_9_8l.exe
+mkdir plugins-dev\openssl
+mkdir plugins-dev\openssl\lib
+xcopy /Y C:\OpenSSL\lib\libeay32.lib plugins-dev\openssl\lib\libeay32.lib
+xcopy /Y C:\OpenSSL\lib\ssleay32.lib plugins-dev\openssl\lib\ssleay32.lib
+mkdir plugins-dev\openssl\include
+xcopy /YICD C:\OpenSSL\include\openssl\* plugins-dev\openssl\include\openssl
+xcopy /YICD C:\OpenSSL\*.dll plugins-dev\openssl\bin
+
+:: Download and unzip libofx dev and bin files
+wget -nc -c https://sourceforge.net/projects/grisbi/files/dependancies/0.6/libofx.zip/download
+unzip -uo libofx.zip -d plugins-dev
+
 wget -nc -c http://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.16/%GTK_DEV_FILE%.zip
 :: Checking if already unzipped
 :: If the readme file exists for this version, update the unzip
