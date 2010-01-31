6e50786a2e147b6ef65d488bf0ba969003d399d4
Pierre Biava <pbiava@users.sourceforge.net>
2010-01-31 07:50:15 +0000

    create balance_estimate_data.h file

---
+++ src/balance_estimate_data.h
@@ -0,0 +1,19 @@
+#ifndef _BALANCE_ESTIMATE_DATA_H
+#define _BALANCE_ESTIMATE_DATA_H
+
+#include <gtk/gtk.h>
+
+/* START_INCLUDE_H */
+/* END_INCLUDE_H */
+
+
+typedef struct _bet_range           SBR;
+typedef struct _historical          SH;
+
+
+/* START_DECLARATION */
+
+/* END_DECLARATION */
+
+
+#endif
