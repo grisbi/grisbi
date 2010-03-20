1efdd7af17384637c598633ae4bddc023c711c43
Pierre Biava <pbiava@users.sourceforge.net>
2010-03-20 21:09:35 +0000

    Fixes a compilation error

--- src/balance_estimate_future.c
+++ src/balance_estimate_future.c
@@ -126,7 +126,7 @@ GtkWidget *bet_future_create_page ( void )
  * */
 gboolean bet_future_configure_form_to_future ( void )
 {
-    gtk_widget_show (form_scheduled_part);
+    //~ gtk_widget_show (form_scheduled_part);
 
     return TRUE;
 }
@@ -138,7 +138,7 @@ gboolean bet_future_configure_form_to_future ( void )
  * */
 gboolean bet_future_configure_form_to_transaction ( void )
 {
-    gtk_widget_hide (form_scheduled_part);
+    //~ gtk_widget_hide (form_scheduled_part);
 
     return TRUE;
 }
