8e1165a50fdf2b4ef997ffe6bb1fe1d08e3b3513
Rémi Cardona <remi@gentoo.org>
2010-05-17 00:37:44 +0200

    tips: copy tips content to a static C array

--- src/tip.c
+++ src/tip.c
@@ -39,6 +39,72 @@ static gchar * get_next_tip ();
 #define END_STATIC
 
 static gint max;
+static const gchar* tips[] =
+{
+    N_("You can reconcile transactions to match your real financial state as "
+    "seen on your bank receipts.  If you just spend 15mn each month reconciling "
+    "Grisbi transactions with your bank receipts, you are guaranteed that your "
+    "accounts are in perfect shape."),
+    N_("Grisbi automatically saves a copy of your work if it crashes.\n\nThus "
+    "it is unlikely that you loose any data in case Grisbi encounters a "
+    "programming error."),
+    N_("Grisbi can import Gnucash files natively, then you don't loose your work "
+    "when you switch from Gnucash to Grisbi."),
+    N_("You can organize transactions into budgetary lines, which are budgetary "
+    "items (why money is spent) that can help you classify your spendings.\n\n"
+    "Combined with categories (how money is spend), budgetary lines are a "
+    "powerful way to analyse budget and to make reports."),
+    N_("You can create categories or budgetary lines directly by typing them in "
+    "transaction form.\n\nIf you write a category or a budgetary line which name "
+    "contains a ':', a sub-category or sub-budgetary line will be created as "
+    "well."),
+    N_("You can encrypt your Grisbi files to improve your privacy.  Just select "
+    "the 'Encrypt Grisbi file' in preferences window and enter a password next "
+    "time you save your file.\n\nEncryption is irreversible, if you loose your "
+    "password, there is NO WAY to restore your Grisbi accounts.  Use with "
+    "caution !"),
+    N_("You can efficiently manage your professional expenses with Grisbi.\n\n"
+    "Create a <i>Professional expenses</i> budgetary line and put all "
+    "professional expenses there.  When you are paid, split your salary as a "
+    "split of transaction, with both a <i>Salary</i> part and a <i>Profesionnal "
+    "expenses</i> part.\n\nThen, you can create a new report that sums up "
+    "transactions from the <i>Profesionnal expenses</i> budgetary line and see "
+    "the balance."),
+    N_("You can use the import facility to automatically reconcile transactions "
+    "against a QIF or OFX file from your online bank."),
+    N_("You can configure the contents of the transaction list.\n\nRight-click "
+    "on a cell of the transaction list and select the <i>Change cell content</i> "
+    "option.  Then select any content you want and configure transaction list to "
+    "your needs."),
+    N_("You can configure fields displayed in the transaction form.\n\nGo to the "
+    "<i>Preferences</i> dialog and select the <i>Form content</i> option.  Then "
+    "you can click on fields to be displayed and drag them through the form to "
+    "adjust their position."),
+    N_("You can hide annoying dialogs like this one, by clicking the <i>\'Do not "
+    "show this message again\'</i> checkbox in this dialog.\n\nYou can then "
+    "activate them again by going to the <i>Preferences</i> window and then to "
+    "the <i>Dialog and messages</i> option."),
+    N_("By double cliking on a date field, you can pop up a calendar to select "
+    "a date."),
+    N_("There are shortcuts in date fields, to avoid typing date or selecting "
+    "it with mouse.\n\n<b>+</b> and <b>-</b> increment and decrement date of one "
+    "day, <i>Page Up</i> and <i>Page Down</i> increment and decrement date of "
+    "one month ... and there are more, check out the manual."),
+    N_("You can archive previous financial years to speed up Grisbi and "
+    "possibily put old transactions into separate files for archival.  Use "
+    "'File/Archive transaction' menu to achieve this."),
+    N_("If unused payees have accumulated in the course of time due to bank "
+    "imports, you can remove all of them in one click!  Just go to the "
+    "<i>Payees</i> page and click on the 'Remove unused payees' button."),
+    N_("You can sort the transaction list according to any criteria.  Click on "
+    "the column titles to sort transactions differently.  For example, you can "
+    "sort transactions by payee or category name."),
+    N_("You can reorder accounts in the navigation list using drag &amp; drop."),
+    N_("You can have future scheduled transactions 'executed' before their date, "),
+    N_("No more tip available !  Please send any tips you know to "
+    "grisbi-devel@lists.sourceforge.net so that we can include them in next "
+    "version for all users.")
+};
 
 /** 
  * Display a tip forcefully, even if show_tip option has been disabled.
