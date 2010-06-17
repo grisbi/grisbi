63d7ae4cb6fd1d9f69a1454c6ba617375d8d6179
William OLLIVIER <guneemwelloeux@users.sourceforge.net>
2010-06-17 19:46:58 +0200

    Fix for compilation error on Windows, due to redefined symbol

--- src/plugins/ofx/ofx.h
+++ src/plugins/ofx/ofx.h
@@ -30,6 +30,9 @@ G_MODULE_EXPORT extern GSList * ofx_plugin_run ( GtkWidget * assistant,
 #endif
 
 #ifndef OFX_ERROR
+#ifdef _MSC_VER
+#undef ERROR
+#endif // _MSC_VER
 #define OFX_ERROR   ERROR
 #endif
 
