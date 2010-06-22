9d84a2b1fa9a1a43100324853355f0dbbc2d7939
William OLLIVIER <guneemwelloeux@users.sourceforge.net>
2010-06-22 12:25:43 +0200

    Windows: Better resources (Icon, Native version number, etc...)

--- win32-msvc/src/resource.h
+++ win32-msvc/src/resource.h
@@ -1,31 +1,16 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by testui.rc
+// Used by grisbi.rc
 //
+#define IDI_ICON1                       104
 
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_TESTUI_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_TESTUI			107
-#define IDI_SMALL				108
-#define IDC_TESTUI			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Valeurs par défaut suivantes des nouveaux objets
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
