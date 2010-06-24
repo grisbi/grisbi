grisbi/grisbi

commit 399d5adc2036d6a43e43a9d19947f2225a3636b3

replace deprecated GLib function g_date_day() with g_date_get_day()

src/gsb_file_load.c
@@ -8613,7 +8613,7 @@ gboolean gsb_file_load_update_previous_version ( void )
                     reconcile_number = gsb_data_reconcile_get_account_last_number (
                                             reconcile -> account_number );
                     final_date = gsb_data_reconcile_get_final_date ( reconcile_number );
-                    ecart_date = g_date_days_between ( final_date, reconcile -> final_date );
+                    ecart_date = g_date_get_days_between ( final_date, reconcile -> final_date );
 
                     if ( abs (ecart_date) < 10 )
                     {

src/etats_affiche.c
@@ -856,14 +856,14 @@ gint etat_affiche_affiche_total_periode ( gint transaction_number,
 		{
 		    case 0:
 			/* split by day, set the day of the transaction */
-			date_debut_periode = g_date_new_dmy ( g_date_day ( date),
+			date_debut_periode = g_date_new_dmy ( g_date_get_day ( date),
 							      g_date_get_month ( date),
 							      g_date_get_year ( date));
 			break;
 
 		    case 1:
 			/* séparation par semaine : on recherche le début de la semaine qui contient l'opé */
-			date_debut_periode = g_date_new_dmy ( g_date_day ( date),
+			date_debut_periode = g_date_new_dmy ( g_date_get_day ( date),
 							      g_date_get_month ( date),
 							      g_date_get_year ( date));
 
@@ -948,7 +948,7 @@ gint etat_affiche_affiche_total_periode ( gint transaction_number,
 
 	    /* 	  si on est en début de semaine, on met un sous total */
 
-	    date_tmp = g_date_new_dmy ( g_date_day ( date_debut_periode ),
+	    date_tmp = g_date_new_dmy ( g_date_get_day ( date_debut_periode ),
 					g_date_get_month ( date_debut_periode ),
 					g_date_get_year ( date_debut_periode ));
 	    g_date_add_days ( date_tmp,
@@ -976,7 +976,7 @@ gint etat_affiche_affiche_total_periode ( gint transaction_number,
 
 	    g_date_free ( date_tmp );
 
-	    date_tmp = g_date_new_dmy ( g_date_day ( date_debut_periode ),
+	    date_tmp = g_date_new_dmy ( g_date_get_day ( date_debut_periode ),
 					g_date_get_month ( date_debut_periode ),
 					g_date_get_year ( date_debut_periode ));
 	    g_date_add_days ( date_tmp,
@@ -1094,14 +1094,14 @@ gint etat_affiche_affiche_total_periode ( gint transaction_number,
 	    {
 		case 0:
 		    /* split by day, set on the transactions day */
-		    date_debut_periode = g_date_new_dmy ( g_date_day ( date),
+		    date_debut_periode = g_date_new_dmy ( g_date_get_day ( date),
 							  g_date_get_month ( date),
 							  g_date_get_year ( date));
 		    break;
 		case 1:
 		    /* séparation par semaine : on recherche le début de la semaine qui contient l'opé */
 
-		    date_debut_periode = g_date_new_dmy ( g_date_day ( date),
+		    date_debut_periode = g_date_new_dmy ( g_date_get_day ( date),
 							  g_date_get_month ( date),
 							  g_date_get_year ( date));
 

src/etats_calculs.c
@@ -819,7 +819,7 @@ GSList *recupere_opes_etat ( gint report_number )
 			    case 9:
 				/* 30 derniers jours */
 
-				date_tmp = g_date_new_dmy ( g_date_day ( date_jour ),
+				date_tmp = g_date_new_dmy ( g_date_get_day ( date_jour ),
 							    g_date_get_month ( date_jour ),
 							    g_date_get_year ( date_jour ) );
 				g_date_subtract_days ( date_tmp,
@@ -835,7 +835,7 @@ GSList *recupere_opes_etat ( gint report_number )
 			    case 10:
 				/* 3 derniers mois */
 
-				date_tmp = g_date_new_dmy ( g_date_day ( date_jour ),
+				date_tmp = g_date_new_dmy ( g_date_get_day ( date_jour ),
 							    g_date_get_month ( date_jour ),
 							    g_date_get_year ( date_jour ) );
 				g_date_subtract_months ( date_tmp,
@@ -851,7 +851,7 @@ GSList *recupere_opes_etat ( gint report_number )
 			    case 11:
 				/* 6 derniers mois */
 
-				date_tmp = g_date_new_dmy ( g_date_day ( date_jour ),
+				date_tmp = g_date_new_dmy ( g_date_get_day ( date_jour ),
 							    g_date_get_month ( date_jour ),
 							    g_date_get_year ( date_jour ) );
 				g_date_subtract_months ( date_tmp,
@@ -867,7 +867,7 @@ GSList *recupere_opes_etat ( gint report_number )
 			    case 12:
 				/* 12 derniers mois */
 
-				date_tmp = g_date_new_dmy ( g_date_day ( date_jour ),
+				date_tmp = g_date_new_dmy ( g_date_get_day ( date_jour ),
 							    g_date_get_month ( date_jour ),
 							    g_date_get_year ( date_jour ) );
 				g_date_subtract_months ( date_tmp,

src/export_csv.c
@@ -442,7 +442,7 @@ static gboolean gsb_csv_export_transaction ( gint transaction_number,
 	{
 	    CSV_CLEAR_FIELD (csv_field_date);
 	    csv_field_date = g_strdup_printf ("%d/%d/%d", 
-					      g_date_day ( date ), 
+					      g_date_get_day ( date ), 
 					      g_date_get_month ( date ),
 					      g_date_get_year ( date ) );
 	}
@@ -452,7 +452,7 @@ static gboolean gsb_csv_export_transaction ( gint transaction_number,
 	{
 	    CSV_CLEAR_FIELD (csv_field_date_val);
 	    csv_field_date_val = g_strdup_printf ("%d/%d/%d", 
-						  g_date_day ( value_date ), 
+						  g_date_get_day ( value_date ), 
 						  g_date_get_month ( value_date ),
 						  g_date_get_year ( value_date ) );
 	}

src/gsb_calendar_entry.c
@@ -675,7 +675,7 @@ gboolean gsb_calendar_entry_calendar_key_press ( GtkCalendar *pCalendar,
 	    return TRUE;
     }
 
-    day = g_date_day (date);
+    day = g_date_get_day (date);
     month = g_date_get_month (date);
     year = g_date_get_year (date);
     g_date_free (date);

src/qif.c
@@ -1012,7 +1012,7 @@ gboolean qif_export ( const gchar *filename,
 		/* this is the beginning of the qif file, we set some beginnings things */
 		fprintf ( fichier_qif,
 			  "D%d/%d/%d\n",
-			  g_date_day (gsb_data_transaction_get_date (transaction_number_tmp)),
+			  g_date_get_day (gsb_data_transaction_get_date (transaction_number_tmp)),
 			  g_date_get_month (gsb_data_transaction_get_date (transaction_number_tmp)),
 			  g_date_get_year (gsb_data_transaction_get_date (transaction_number_tmp)));
 
@@ -1046,7 +1046,7 @@ gboolean qif_export ( const gchar *filename,
 
 		fprintf ( fichier_qif,
 			  "D%d/%d/%d\n",
-			  g_date_day (gsb_data_transaction_get_date (transaction_number_tmp)),
+			  g_date_get_day (gsb_data_transaction_get_date (transaction_number_tmp)),
 			  g_date_get_month (gsb_data_transaction_get_date (transaction_number_tmp)),
 			  g_date_get_year (gsb_data_transaction_get_date (transaction_number_tmp)));
 

commit 6945aea0b9e0877c30bd6e5590eab15a48879d44

fix overzealous replacement of g_date_get_days
Broken sed expression in commit 399d5adc2036d6a43e43a9d19947f2225a3636b3

src/gsb_file_load.c
@@ -8613,7 +8613,7 @@ gboolean gsb_file_load_update_previous_version ( void )
                     reconcile_number = gsb_data_reconcile_get_account_last_number (
                                             reconcile -> account_number );
                     final_date = gsb_data_reconcile_get_final_date ( reconcile_number );
-                    ecart_date = g_date_get_days_between ( final_date, reconcile -> final_date );
+                    ecart_date = g_date_days_between ( final_date, reconcile -> final_date );
 
                     if ( abs (ecart_date) < 10 )
                     {