149745d2f34fbf393c11e458a2a7f6dbe4cf1276
pbiava <pierre.biava@nerim.net>
2010-08-19 20:49:54 +0200

    add print_tree_view_list

---
+++ src/print_tree_view_list.h
@@ -0,0 +1,16 @@
+#ifndef _PRINT_TREE_VIEW_LIST_H
+#define _PRINT_TREE_VIEW_LIST_H
+
+#include <gtk/gtk.h>
+
+/* START_INCLUDE_H */
+/* END_INCLUDE_H */
+
+
+
+/* START_DECLARATION */
+/* END_DECLARATION */
+
+
+#endif /*_PRINT_TREE_VIEW_LIST_H */
+
