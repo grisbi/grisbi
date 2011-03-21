00880037c8078bcc1751d523d62c21dd60d0271d
Rémi Cardona <remi@gentoo.org>
2011-03-21 08:24:42 +0100

    Fix memory leaks in gsb_category_assistant_scan_directory()

--- src/gsb_category.c
+++ src/gsb_category.c
@@ -312,6 +312,7 @@ GSList * gsb_category_assistant_scan_directory ( gchar * basename, GtkTreeModel 
 	gchar * pos = strchr ( basename, '.' );
 	if ( pos ) 
 	    *pos = '\0';
+	g_free ( dirname );
 	dirname = g_strconcat ( DATA_PATH, G_DIR_SEPARATOR_S, "categories", G_DIR_SEPARATOR_S, basename, NULL );
     }
     if ( ! g_file_test ( dirname, G_FILE_TEST_IS_DIR ) )
@@ -319,6 +320,7 @@ GSList * gsb_category_assistant_scan_directory ( gchar * basename, GtkTreeModel 
 	gchar * pos = strchr ( basename, '_' );
 	if ( pos ) 
 	    *pos = '\0';
+	g_free ( dirname );
 	dirname = g_strconcat ( DATA_PATH, G_DIR_SEPARATOR_S, "categories", G_DIR_SEPARATOR_S, basename, NULL );
     }
 
@@ -336,6 +338,8 @@ GSList * gsb_category_assistant_scan_directory ( gchar * basename, GtkTreeModel 
 	    }
 	}
     }
+
+    g_free ( dirname );
 
     return g_slist_sort ( list, (GCompareFunc) strcmp );
 }
