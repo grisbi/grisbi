3fb5a5798ad58a5505d4c45d84482fe7bc6bafa1
Rémi Cardona <remi@gentoo.org>
2011-04-03 13:36:38 +0200

    Remove useless strlen() call
    gtk_ui_manager_add_ui_from_string() accepts -1 as the string length and
    figures it out on its own as 'ui_manager_buffer' is NULL-terminated.

--- src/menu.c
+++ src/menu.c
@@ -317,9 +317,7 @@ GtkWidget *init_menus ( GtkWidget *vbox )
 
     gtk_ui_manager_insert_action_group ( ui_manager, actions, 0 );
     merge_id = gtk_ui_manager_add_ui_from_string ( ui_manager,
-                        ui_manager_buffer,
-                        strlen ( ui_manager_buffer ),
-                        NULL );
+                        ui_manager_buffer, -1, NULL );
 
 #ifndef GTKOSXAPPLICATION
     gtk_window_add_accel_group ( GTK_WINDOW ( run.window ),
