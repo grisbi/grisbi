e1adb1127f9409fe2989eb83da1ecbf6a11693a3
Rémi Cardona <remi@gentoo.org>
2011-05-19 18:58:55 +0200

    gsb_real: simplify gsb_real_set_decimal_point() and gsb_real_set_thousands_sep()
    Both g_free() and g_strdup() are NULL-safe. Further more, this fixes a
    potential memory leak if either function was called with a zero-length
    string.

--- src/gsb_real.c
+++ src/gsb_real.c
@@ -1012,13 +1012,8 @@ gchar *gsb_real_get_decimal_point ( void )
  * */
 void gsb_real_set_decimal_point ( const gchar *decimal_point )
 {
-    if ( gsb_decimal_point && strlen ( gsb_decimal_point ) )
-        g_free ( gsb_decimal_point );
-
-    if ( decimal_point == NULL )
-        gsb_decimal_point = NULL;
-    else
-		gsb_decimal_point = g_strdup ( decimal_point );
+    g_free ( gsb_decimal_point );
+    gsb_decimal_point = g_strdup ( decimal_point );
 }
 
 
@@ -1042,13 +1037,8 @@ gchar *gsb_real_get_thousands_sep ( void )
  * */
 void gsb_real_set_thousands_sep ( const gchar *thousands_sep )
 {
-    if ( gsb_thousands_sep && strlen ( gsb_thousands_sep ) )
-        g_free ( gsb_thousands_sep );
-
-    if ( thousands_sep == NULL )
-        gsb_thousands_sep = NULL;
-    else
-        gsb_thousands_sep = g_strdup ( thousands_sep );
+    g_free ( gsb_thousands_sep );
+    gsb_thousands_sep = g_strdup ( thousands_sep );
 }
 
 
