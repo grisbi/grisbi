64b605417e46b483a750ff408d372251c51fab4a
pbiava <pierre.biava@nerim.net>
2011-09-19 21:08:16 +0200

    utils_gtkbuilder: added a function to initialize a GtkBuilder

--- src/utils_gtkbuilder.h
+++ src/utils_gtkbuilder.h
@@ -12,6 +12,8 @@ gchar *utils_gtkbuilder_get_full_path ( const gchar *name );
 GtkWidget *utils_gtkbuilder_get_widget_by_name ( GtkBuilder *builder,
                         const gchar *gtk_builder_name,
                         const gchar *optional_name );
+gint utils_gtkbuilder_merge_ui_data_in_builder ( GtkBuilder *builder,
+                    const gchar *ui_name );
 /* END_DECLARATION */
 
 #endif /* _H_UTILS_GTKBUILDER */
