16a785297798b01a8588feb3d5eb49804cf2f3df
Chtibzh <didier.chevalier35@gmail.com>
2011-09-24 17:49:36 +0200

    grisbi-0.8.8

--- win32-msvc/config.h
+++ win32-msvc/config.h
@@ -1,7 +1,7 @@
 #ifndef _WIN32_MSVC_CONFIG_H
 #define _WIN32_MSVC_CONFIG_H
 
-#define VERSION	"0.6.0-grisbi for Windows"
+#define VERSION	"0.8.8-grisbi for Windows"
 #define PACKAGE	"grisbi"
 
 #include "win32gtkutils.h"
