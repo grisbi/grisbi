3942eedb9da53f8243355a98ccc97667348b9253
pbiava <pierre.biava@nerim.net>
2011-10-02 11:33:37 +0200

    Add etats_config_ui files

---
+++ src/etats_config_ui.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*     Copyright (C)    2000-2008 Cédric Auger (cedric@grisbi.org)            */
+/*                      2003-2008 Benjamin Drieu (bdrieu@april.org)           */
+/*          2008-2011 Pierre Biava (grisbi@pierre.biava.name)                 */
+/*          http://www.grisbi.org                                             */
+/*                                                                            */
+/*  This program is free software; you can redistribute it and/or modify      */
+/*  it under the terms of the GNU General Public License as published by      */
+/*  the Free Software Foundation; either version 2 of the License, or         */
+/*  (at your option) any later version.                                       */
+/*                                                                            */
+/*  This program is distributed in the hope that it will be useful,           */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*  GNU General Public License for more details.                              */
+/*                                                                            */
+/*  You should have received a copy of the GNU General Public License         */
+/*  along with this program; if not, write to the Free Software               */
+/*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+
+
+/*START_INCLUDE*/
+#include "gsb_etats_config.h"
+#include "dialog.h"
+#include "etats_calculs.h"
+#include "etats_config.h"
+#include "fenetre_principale.h"
+#include "gsb_calendar_entry.h"
+#include "gsb_data_account.h"
+#include "gsb_data_budget.h"
+#include "gsb_data_category.h"
+#include "gsb_data_fyear.h"
+#include "gsb_data_payee.h"
+#include "gsb_data_payment.h"
+#include "navigation.h"
+#include "structures.h"
+#include "utils.h"
+#include "utils_gtkbuilder.h"
+#include "utils_str.h"
+
+#include "erreur.h"
+/*END_INCLUDE*/
+
+
---
+++ src/etats_config_ui.h
@@ -0,0 +1,12 @@
+#ifndef GSB_ETATS_CONFIG_UI_H
+#define GSB_ETATS_CONFIG_UI_H (1)
+
+/* START_INCLUDE_H */
+/* END_INCLUDE_H */
+
+/* START_DECLARATION */
+void gsb_etats_config_personnalisation_etat ( void );
+/* END_DECLARATION */
+
+
+#endif /* GSB_ETATS_CONFIG_UI_H */
