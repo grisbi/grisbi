d5a1fa5505a5012f101034a236fabcaa1f7cf589
pbiava <pierre.biava@nerim.net>
2011-10-08 19:20:29 +0200

    utils_button.c .h: new functions for togglebuttons

--- src/utils_buttons.h
+++ src/utils_buttons.h
@@ -17,10 +17,16 @@ typedef enum GSB_BUTTON_STYLE {
 
 /*START_DECLARATION*/
 gboolean gsb_button_sensitive_by_checkbutton ( GtkWidget *check_button,
-					       GtkWidget *widget );
+                        GtkWidget *widget );
 GtkWidget * new_image_label ( GsbButtonStyle style, const gchar * image_name, const gchar * name );
 GtkWidget * new_stock_image_label ( GsbButtonStyle style, const gchar * stock_id, const gchar * name );
 void set_popup_position (GtkMenu *menu, gint *x, gint *y, gboolean *push_in, gpointer user_data);
+void utils_togglebutton_collapse_expand_all_rows ( GtkToggleButton *togglebutton,
+                        GtkWidget *tree_view );
+void utils_togglebutton_change_label_select_unselect ( GtkToggleButton *togglebutton,
+                        gint toggle );
+void utils_togglebutton_select_unselect_all_rows ( GtkToggleButton *togglebutton,
+                        GtkWidget *tree_view );
 /*END_DECLARATION*/
 
 #endif
