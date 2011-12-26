a54ec6617b7539ca590f4597d3a7a92195d01413
pbiava <pierre.biava@nerim.net>
2011-12-26 22:00:53 +0100

    fixes a segmentation fault when closing Grisbi

--- src/main.c
+++ src/main.c
@@ -135,8 +135,6 @@ int main ( int argc, char **argv )
     g_mem_set_vtable(glib_mem_profiler_table);
 #endif
 
-    gsb_dirs_init ( );
-
 #ifdef _WIN32
     main_win_32 (  argc, argv );
 #else
@@ -146,15 +144,6 @@ int main ( int argc, char **argv )
         main_linux ( argc, argv );
     #endif /* GTKOSXAPPLICATION || linux */
 #endif /* _WIN32 */
-    gsb_locale_shutdown ( );
-    gsb_dirs_shutdown ( );
-
-    /* liberation libgoffice */
-    libgoffice_shutdown ( );
-
-#if GSB_GMEMPROFILE
-    g_mem_profile();
-#endif
 
     exit ( 0 );
 }
@@ -179,7 +168,6 @@ void main_linux ( int argc, char **argv )
 
     /* Setup locale/gettext */
     setlocale (LC_ALL, "");
-
     gsb_locale_init ( );
 
 #if IS_DEVELOPMENT_VERSION == 1
@@ -192,6 +180,9 @@ void main_linux ( int argc, char **argv )
     libgoffice_init ( );
     /* Initialize plugins manager */
     go_plugins_init (NULL, NULL, NULL, NULL, TRUE, GO_TYPE_PLUGIN_LOADER_MODULE);
+
+    /* initialisation des différents répertoires */
+    gsb_dirs_init ( );
 
     /* on commence par détourner le signal SIGSEGV */
     gsb_grisbi_trappe_signal_sigsegv ( );
@@ -225,6 +216,19 @@ void main_linux ( int argc, char **argv )
 
     /* sauvegarde les raccourcis claviers */
     gtk_accel_map_save ( C_PATH_CONFIG_ACCELS ( ) );
+
+    /* libération de mémoire */
+    gsb_locale_shutdown ( );
+    gsb_dirs_shutdown ( );
+
+    /* liberation libgoffice */
+    libgoffice_shutdown ( );
+
+#if GSB_GMEMPROFILE
+    g_mem_profile();
+#endif
+
+    exit ( 0 );
 }
 
 
@@ -267,8 +271,10 @@ void main_mac_osx ( int argc, char **argv )
 
     /* Setup locale/gettext */
     setlocale (LC_ALL, "");
-
     gsb_locale_init ( );
+
+    /* initialisation des différents répertoires */
+    gsb_dirs_init ( );
 
     /* on commence par détourner le signal SIGSEGV */
     gsb_grisbi_trappe_signal_sigsegv ( );
@@ -338,6 +344,18 @@ void main_mac_osx ( int argc, char **argv )
 
     g_object_unref ( theApp );
 
+    gsb_locale_shutdown ( );
+    gsb_dirs_shutdown ( );
+
+    /* liberation libgoffice */
+    libgoffice_shutdown ( );
+
+#if GSB_GMEMPROFILE
+    g_mem_profile();
+#endif
+
+    exit ( 0 );
+
 #endif /* GTKOSXAPPLICATION */
 }
 
@@ -384,6 +402,9 @@ void main_win_32 (  int argc, char **argv )
 
     win32_parse_gtkrc ( "gtkrc" );
 
+    /* initialisation des différents répertoires */
+    gsb_dirs_init ( );
+
     /* parse command line parameter, exit with correct error code when needed */
     if ( !parse_options (argc, argv, &opt, &status ) )
         exit ( status );
@@ -413,6 +434,18 @@ void main_win_32 (  int argc, char **argv )
 
     /* sauvegarde les raccourcis claviers */
     gtk_accel_map_save ( C_PATH_CONFIG_ACCELS ( ) );
+
+    gsb_locale_shutdown ( );
+    gsb_dirs_shutdown ( );
+
+    /* liberation libgoffice */
+    libgoffice_shutdown ( );
+
+#if GSB_GMEMPROFILE
+    g_mem_profile();
+#endif
+
+    exit ( 0 );
 
 #endif /* WIN_32 */
 }
