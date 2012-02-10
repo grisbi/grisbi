885e5f37688d18e0f05dbf7a2f16fde842fd3608
pbiava <pierre.biava@nerim.net>
2012-02-10 22:29:10 +0100

    gsb_file.c: minor change

--- src/gsb_file.c
+++ src/gsb_file.c
@@ -1002,7 +1002,6 @@ gboolean gsb_file_close ( void )
          &&
          nom_fichier_comptes )
             gsb_file_util_modify_lock ( FALSE );
-        gsb_gui_init_general_vbox ( );
 
         /* free all the variables */
         init_variables ();
