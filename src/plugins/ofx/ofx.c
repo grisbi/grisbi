af14f10706f343174f73785f4dd52f4c5a48a110
Rémi Cardona <remi@gentoo.org>
2012-12-31 14:15:10 +0100

    Move ofx related defines to where they are used

--- src/plugins/ofx/ofx.c
+++ src/plugins/ofx/ofx.c
@@ -48,6 +48,46 @@ static GSList * recuperation_donnees_ofx ( GtkWidget * assistant, struct importe
 /*END_STATIC*/
 
 
+
+/* It seems the following applies to everyone, otherwise OFX_* are not defined */
+/* #ifdef _WIN32 */
+/* On Windows, the Ofx Severity enumerate values are already used in wingdi.h, DELETE is used in winnt.h
+ * This is a work around to this issues :
+ *  INFO, WARN, ERROR, DELETE and REPLACE are used in standard libofx.h;
+ *  on windows they should be prefixed by OFX_
+ */
+
+#ifndef _MINGW
+
+#ifndef OFX_INFO
+#define OFX_INFO    INFO
+#endif
+
+#ifndef OFX_WARN
+#define OFX_WARN    WARN
+#endif
+
+#ifndef OFX_ERROR
+#if defined(_MSC_VER) || defined(_MINGW)
+#undef ERROR
+#endif /* _MSC_VER */
+#define OFX_ERROR   ERROR
+#endif
+
+#ifndef OFX_DELETE
+#define OFX_DELETE  DELETE
+#endif
+
+#ifndef OFX_REPLACE
+#define OFX_REPLACE REPLACE
+#endif
+
+#endif /* _MIN_GW */
+
+/* #endif _WIN32 */
+
+
+
 static struct import_format ofx_format =
 {
     "OFX",
