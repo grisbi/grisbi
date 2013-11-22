4371db6ae0b3c303102c66eb9a80e0efefb21dfe
Ludovic Rousseau <ludovic.rousseau+github@gmail.com>
2013-11-22 15:43:58 +0100

    struct conditional_message: use const
    Fix compiler warnings:
    dialog.c:56:7: warning: initializing 'gchar *' (aka 'char *') with an expression of type 'const char [29]' discards qualifiers [-Wincompatible-pointer-types-discards-qualifiers]
        { "ofx-security-not-implemented", N_("Security feature not implemented"),
          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    dialog.c:56:39: warning: initializing 'gchar *' (aka 'char *') with an expression of type 'const char [33]' discards qualifiers [-Wincompatible-pointer-types-discards-qualifiers]
        { "ofx-security-not-implemented", N_("Security feature not implemented"),
                                          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /usr/local/Cellar/glib/2.38.2/include/glib-2.0/glib/gi18n.h:30:20: note: expanded from macro 'N_'
                       ^~~~~~~~
    etc.

--- src/dialog.h
+++ src/dialog.h
@@ -9,9 +9,9 @@
 
 struct conditional_message
 {
-    gchar * name;
-    gchar * hint;
-    gchar * message;
+    const gchar * name;
+    const gchar * hint;
+    const gchar * message;
     gboolean hidden;
     gboolean default_answer;	/** Useful for yes/no questions. */
 };
