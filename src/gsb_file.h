ee55852bd49fc4fec018f6467af8ac4bd9b64dd3
pbiava <pierre.biava@orange.fr>
2016-09-06 19:46:59 +0200

    cosmetic changes in gsb_file.h and gsb_file_load.h

--- src/gsb_file.h
+++ src/gsb_file.h
@@ -9,25 +9,25 @@
 
 /* START_DECLARATION */
 gboolean        gsb_file_automatic_backup_change_time   (GtkWidget *spinbutton,
-                                                         gpointer null );
+                                                         gpointer null);
 gboolean        gsb_file_automatic_backup_start         (GtkWidget *checkbutton,
-                                                         gpointer null );
-gboolean        gsb_file_close                          ( void );
-void            gsb_file_free_last_path                 ( void );
-void            gsb_file_free_backup_path               ( void );
-const gchar *   gsb_file_get_backup_path                ( void );
-const gchar *   gsb_file_get_last_path                  ( void );
-void            gsb_file_init_last_path                 ( const gchar *last_path );
-gboolean        gsb_file_new_finish                     ( void );
-void            gsb_file_new_gui                        ( void );
-gboolean        gsb_file_open_file                      ( gchar *filename );
-gboolean        gsb_file_open_menu                      ( void );
-void            gsb_file_remove_name_from_opened_list   ( gchar *filename );
-gboolean        gsb_file_save                           ( void );
-gboolean        gsb_file_save_as                        ( void );
-void            gsb_file_set_backup_path                ( const gchar *path );
-void            gsb_file_set_modified                   ( gboolean modified );
-gboolean        gsb_file_get_modified                   ( void );
-void            gsb_file_update_last_path               ( const gchar *last_path );
+                                                         gpointer null);
+gboolean        gsb_file_close                          (void);
+void            gsb_file_free_last_path                 (void);
+void            gsb_file_free_backup_path               (void);
+const gchar *   gsb_file_get_backup_path                (void);
+const gchar *   gsb_file_get_last_path                  (void);
+void            gsb_file_init_last_path                 (const gchar *last_path);
+gboolean        gsb_file_new_finish                     (void);
+void            gsb_file_new_gui                        (void);
+gboolean        gsb_file_open_file                      (gchar *filename);
+gboolean        gsb_file_open_menu                      (void);
+void            gsb_file_remove_name_from_opened_list   (gchar *filename);
+gboolean        gsb_file_save                           (void);
+gboolean        gsb_file_save_as                        (void);
+void            gsb_file_set_backup_path                (const gchar *path);
+void            gsb_file_set_modified                   (gboolean modified);
+gboolean        gsb_file_get_modified                   (void);
+void            gsb_file_update_last_path               (const gchar *last_path);
 /* END_DECLARATION */
 #endif
