c15ae3fe243b01e558935be1a18ba8ebade2dace
pbiava <pierre.biava@orange.fr>
2016-09-19 13:40:34 +0200

    fixes limit of spinbutton

--- src/gsb_automem.c
+++ src/gsb_automem.c
@@ -571,7 +571,7 @@ GtkWidget *gsb_automem_radiobutton3_new ( const gchar *choice1,
  * this is a gint spin button
  * this function is the same as gsb_automem_spin_button_new_full, but
  * set defaults parameters
- * by default : between 0 and G_MAXDOUBLE ; value is integer, step increment is 1 and page is 5
+ * by default : between 0 and GSB_MAX_SPIN_BUTTON ; value is integer, step increment is 1 and page is 5
  *
  * \param value a pointer to an integer wich will change with the value of the spin button
  * \param hook An optional hook to run at each change
@@ -587,7 +587,7 @@ GtkWidget *gsb_automem_spin_button_new ( gint *value,
     gdouble page_size, climb_rate;
 
     lower = 0.0;
-    upper = G_MAXDOUBLE;
+    upper = GSB_MAX_SPIN_BUTTON;
     step_increment = 1.0;
     page_increment = 5.0;
     page_size = 0.0;
