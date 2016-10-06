d05bd8833945a7217d165babedbfb61f52e971e3
Ludovic Rousseau <ludovic.rousseau@free.fr>
2016-10-06 21:50:16 +0200

    Travis-CI: build with the lowest level of warnings
    CFLAGS="-Wno-unused-parameter -Wno-deprecated-declarations"
    
    -Wno-unused-parameter
      because some function parameters are unused and I don't want to fix
      that for now.
    
    -Wno-deprecated-declarations
      because the code uses deprecated GTK+ functions and I don't want to
      fix that for now.

--- travis-ci/travis-autogen.sh
+++ travis-ci/travis-autogen.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 
 # Warnings enabled
-CFLAGS="-Wall -Wextra"
+CFLAGS=""
+#CFLAGS="-Wall"
+#CFLAGS+=" -Wextra"
 
-CFLAGS+=" -Wbad-function-cast"
+#CFLAGS+=" -Wbad-function-cast"
 #CFLAGS+=" -Wcast-align"
-CFLAGS+=" -Wchar-subscripts"
-CFLAGS+=" -Wempty-body"
-CFLAGS+=" -Wformat"
-CFLAGS+=" -Wformat-security"
-CFLAGS+=" -Winit-self"
-CFLAGS+=" -Winline"
-CFLAGS+=" -Wmissing-declarations"
-CFLAGS+=" -Wmissing-include-dirs"
-CFLAGS+=" -Wmissing-prototypes"
-CFLAGS+=" -Wnested-externs"
-CFLAGS+=" -Wold-style-definition"
-CFLAGS+=" -Wpointer-arith"
-CFLAGS+=" -Wredundant-decls"
-CFLAGS+=" -Wshadow"
-CFLAGS+=" -Wstrict-prototypes"
-CFLAGS+=" -Wswitch-enum"
-CFLAGS+=" -Wundef"
-CFLAGS+=" -Wuninitialized"
-CFLAGS+=" -Wunused"
-CFLAGS+=" -Wwrite-strings"
-CFLAGS+=" -fdiagnostics-color=auto"
+#CFLAGS+=" -Wchar-subscripts"
+#CFLAGS+=" -Wempty-body"
+#CFLAGS+=" -Wformat"
+#CFLAGS+=" -Wformat-security"
+#CFLAGS+=" -Winit-self"
+#CFLAGS+=" -Winline"
+#CFLAGS+=" -Wmissing-declarations"
+#CFLAGS+=" -Wmissing-include-dirs"
+#CFLAGS+=" -Wmissing-prototypes"
+#CFLAGS+=" -Wnested-externs"
+#CFLAGS+=" -Wold-style-definition"
+#CFLAGS+=" -Wpointer-arith"
+#CFLAGS+=" -Wredundant-decls"
+#CFLAGS+=" -Wshadow"
+#CFLAGS+=" -Wstrict-prototypes"
+#CFLAGS+=" -Wswitch-enum"
+#CFLAGS+=" -Wundef"
+#CFLAGS+=" -Wuninitialized"
+#CFLAGS+=" -Wunused"
+#CFLAGS+=" -Wwrite-strings"
 
 # warnings disabled on purpose
 CFLAGS+=" -Wno-unused-parameter"
 #CFLAGS+=" -Wno-unused-function"
-#CFLAGS+=" -Wno-deprecated-declarations"
+CFLAGS+=" -Wno-deprecated-declarations"
 
 # fails on warning
 #CFLAGS+=" -Werror"
 
 export CFLAGS
+echo "CFLAGS: $CFLAGS"
 
 sh -x ./autogen.sh
-./configure
+./configure "$@"
