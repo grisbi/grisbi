863d26fb5de848c5a59e7cf06a9381e2854d4a4b
pbiava <pierre.biava@orange.fr>
2016-10-07 21:16:41 +0200

    fixes bug after cherry picked commit 130df30

--- src/affichage.c
+++ src/affichage.c
@@ -109,7 +109,7 @@ static GtkWidget *preview = NULL;
  *
  * \return
  * */
-static void prefs_sort_column_treeview_clicked (GtkWidget *toggle_button,
+static void prefs_sort_order_column_treeview_clicked (GtkWidget *toggle_button,
                                                 gpointer null)
 {
     gsb_archive_config_set_sort_order (GINT_TO_POINTER (conf.prefs_sort_order));
