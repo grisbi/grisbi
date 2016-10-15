dbc55f2460ebd0436ed7719a1bf4e6c99e952619
Pierre Biava <pierre@pierre.biava.name>
2016-10-15 22:15:45 +0200

    remove warning of compilation

--- src/affichage.c
+++ src/affichage.c
@@ -34,6 +34,7 @@
 #include "custom_list.h"
 #include "fenetre_principale.h"
 #include "grisbi_settings.h"
+#include "grisbi_win.h"
 #include "gsb_archive_config.h"
 #include "gsb_automem.h"
 #include "gsb_data_account.h"
