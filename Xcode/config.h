71f1070d39fdc664d4611233842341c0bc8f4c9c
Ludovic Rousseau <ludovic.rousseau@free.fr>
2016-12-14 11:23:31 +0100

    Xcode: push initial project
    Build works using Xcode 8.1 on Sierra.
    You need to install the dependencies using Homebrew.

---
+++ Xcode/config.h
@@ -0,0 +1,157 @@
+/* always defined to indicate that i18n is enabled */
+/* #undef ENABLE_NLS */
+
+/* Using GDK Quartz (not X11) */
+//#define GDK_QUARTZ 1
+
+/* Gettext package. */
+#define GETTEXT_PACKAGE "grisbi"
+
+/* grisbi major version */
+//#define GRISBI_MAJOR_VERSION 1
+
+/* grisbi micro version */
+//#define GRISBI_MICRO_VERSION 1
+
+/* grisbi minor version */
+//#define GRISBI_MINOR_VERSION 1
+
+/* define to 1 if g_no_optim support is enabled */
+//#define GSB_NOOPTIM
+
+/* Defined if app is GtkOSXApplication */
+/* #undef GTKOSXAPPLICATION */
+
+/* Define to 1 if you have the `backtrace' function. */
+//#define HAVE_BACKTRACE 1
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+/* #undef HAVE_BIND_TEXTDOMAIN_CODESET */
+
+/* Define to 1 if you have the `dcgettext' function. */
+/* #undef HAVE_DCGETTEXT */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+//#define HAVE_DLFCN_H 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+/* #undef HAVE_GETTEXT */
+
+/* Define if goffice support is enabled */
+//#define HAVE_GOFFICE 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+//#define HAVE_INTTYPES_H 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+//#define HAVE_LC_MESSAGES 1
+
+/* Define to 1 if you have the `z' library (-lz). */
+/* #undef HAVE_LIBZ */
+
+/* Define to 1 if you have the <locale.h> header file. */
+//#define HAVE_LOCALE_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+//#define HAVE_MEMORY_H 1
+
+/* Build with ofx support */
+//#define HAVE_OFX 1
+
+/* Define if OpenSSL support is enabled */
+/* #undef HAVE_SSL */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+//#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+//#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+//#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+//#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+//#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+//#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+//#define HAVE_UNISTD_H 1
+
+/* Define if libxml2 support is enabled */
+#define HAVE_XML2 1
+
+/* Defined if Grisbi is a developmeent version */
+#define IS_DEVELOPMENT_VERSION 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+//#define LT_OBJDIR ".libs/"
+
+/* Defined if system is Mac OS X */
+#define OS_OSX 1
+
+/* Name of package */
+#define PACKAGE "grisbi"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://www.grisbi.org/mantis/login_select_proj_page.php?ref=bug_report_page.php"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "grisbi"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "grisbi 1.1.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "grisbi"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://www.grisbi.org/index.php"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.1.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Enable extensions on AIX 3, Interix.  */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+/* Enable threading extensions on Solaris.  */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+/* Enable extensions on HP NonStop.  */
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+/* Enable general extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+
+
+/* Version number of package */
+#define VERSION "1.1.1"
+
+/* Define to 1 if on MINIX. */
+/* #undef _MINIX */
+
+/* Define to 2 if the system does not provide POSIX.1 features except with
+   this defined. */
+/* #undef _POSIX_1_SOURCE */
+
+/* Define to 1 if you need to in order for `stat' and other things to work. */
+/* #undef _POSIX_SOURCE */
+
+#define HELP_PATH ""
+#define LOGO_PATH ""
