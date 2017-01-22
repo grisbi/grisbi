045a7ac3fddcd57d5665d946012ed74250e05565
Egor Zindy <ezindy@gmail.com>
2017-01-22 16:46:01 +0000

    Fix to expand the navigation pane to the bottom

--- src/navigation.c
+++ src/navigation.c
@@ -211,6 +211,7 @@ GtkWidget *gsb_gui_navigation_create_navigation_pane ( void )
     grid = gtk_grid_new ();
 
     sw = gtk_scrolled_window_new (NULL, NULL);
+    gtk_widget_set_vexpand (sw, TRUE);
     gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (sw), GTK_SHADOW_ETCHED_IN);
     gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (sw),
                                     GTK_POLICY_AUTOMATIC,
