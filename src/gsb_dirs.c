e777ebd29ed001cfba31407c6a63c3d9e8e3b9a8
Pierre Biava <pierre@pierre.biava.name>
2017-05-06 22:35:27 +0200

    Cosmetics changes

--- src/gsb_dirs.c
+++ src/gsb_dirs.c
@@ -39,176 +39,237 @@ static gchar *ui_dir;
 static gchar *user_config_dir;
 static gchar *user_data_dir;
 static gchar *user_default_dir;
-
 static gchar *grisbirc_filename;
 static gchar *accelerator_filename;
-/*static gchar *grisbi_dir;
-*/
 
-void gsb_dirs_init ( char* gsb_bin_path )
+/******************************************************************************/
+/* Public functions                                                           */
+/******************************************************************************/
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+void gsb_dirs_init (char* gsb_bin_path)
 {
 	gchar *grisbi_dir;
     gchar *local_ui_dir;
     GDir *local_ui_handle;
 
     /* Get the grisbi executable directory as it may be useful when running dev instances */
-    grisbi_dir = g_path_get_dirname ( gsb_bin_path );
-    local_ui_dir = g_strconcat ( grisbi_dir, "/ui", NULL );
+    grisbi_dir = g_path_get_dirname (gsb_bin_path);
+    local_ui_dir = g_strconcat (grisbi_dir, "/ui", NULL);
 
-	g_free ( grisbi_dir );
+	g_free (grisbi_dir);
 
 #ifdef _WIN32
 {
     gchar *dir;
 
-    dir = g_win32_get_package_installation_directory_of_module ( NULL );
+    dir = g_win32_get_package_installation_directory_of_module (NULL);
 	printf ("PACKAGE dir = %s\n", dir);
-    categories_dir = g_build_filename ( dir, "\\share\\grisbi\\categories", NULL );
-    help_dir = g_build_filename ( dir, "\\share\\doc\\grisbi", NULL );
-    locale_dir = g_strconcat ( dir, "\\share\\locale", NULL );
-    pixmaps_dir = g_strconcat ( dir, "\\share\\pixmaps\\grisbi", NULL );
-    ui_dir = g_strconcat ( dir, "\\share\\grisbi\\ui", NULL );
+    categories_dir = g_build_filename (dir, "\\share\\grisbi\\categories", NULL);
+    help_dir = g_build_filename (dir, "\\share\\doc\\grisbi", NULL);
+    locale_dir = g_strconcat (dir, "\\share\\locale", NULL);
+    pixmaps_dir = g_strconcat (dir, "\\share\\pixmaps\\grisbi", NULL);
+    ui_dir = g_strconcat (dir, "\\share\\grisbi\\ui", NULL);
 
-    g_free ( dir );
+    g_free (dir);
 
-    user_config_dir = g_build_filename ( g_get_user_config_dir (), "grisbi", NULL);
-    user_data_dir = g_build_filename ( g_get_user_data_dir (), "grisbi", NULL);
-    user_default_dir = g_strdup ( g_get_user_special_dir (G_USER_DIRECTORY_DOCUMENTS) );
+    user_config_dir = g_build_filename (g_get_user_config_dir (), "grisbi", NULL);
+    user_data_dir = g_build_filename (g_get_user_data_dir (), "grisbi", NULL);
+    user_default_dir = g_strdup (g_get_user_special_dir (G_USER_DIRECTORY_DOCUMENTS));
 		printf ("user_directory = %s\n", user_default_dir);
 }
 #else
 {
     #ifdef OS_OSX
     {
-        user_config_dir = g_build_filename ( g_get_home_dir (),
-                        "Library/Application Support/Grisbi/config", NULL );
-        user_data_dir = g_build_filename ( g_get_home_dir (),
-                        "Library/Application Support/Grisbi/data", NULL );
-        user_default_dir = g_strdup ( g_get_home_dir() );
+        user_config_dir = g_build_filename (g_get_home_dir (),
+                        "Library/Application Support/Grisbi/config", NULL);
+        user_data_dir = g_build_filename (g_get_home_dir (),
+                        "Library/Application Support/Grisbi/data", NULL);
+        user_default_dir = g_strdup (g_get_home_dir());
 
         #ifdef GTKOSXAPPLICATION
         {
-            if ( gtkosx_application_get_bundle_id ( ) )
+            if (gtkosx_application_get_bundle_id ())
             {
                 gchar *res_path = gtkosx_application_get_resource_path ();
 
-                categories_dir = g_build_filename ( res_path, "share/grisbi/categories", NULL );
-                help_dir = g_build_filename ( res_path, "share/doc/grisbi", NULL );
-                locale_dir = g_build_filename (res_path, "/share/locale", NULL );
-                pixmaps_dir = g_build_filename (res_path, "/share/pixmaps/grisbi", NULL );
-                ui_dir = g_build_filename (res_path, "/share/grisbi/ui", NULL );
+                categories_dir = g_build_filename (res_path, "share/grisbi/categories", NULL);
+                help_dir = g_build_filename (res_path, "share/doc/grisbi", NULL);
+                locale_dir = g_build_filename (res_path, "/share/locale", NULL);
+                pixmaps_dir = g_build_filename (res_path, "/share/pixmaps/grisbi", NULL);
+                ui_dir = g_build_filename (res_path, "/share/grisbi/ui", NULL);
 
-                g_free ( res_path );
+                g_free (res_path);
             }
             else
             {
-                categories_dir = g_build_filename ( DATA_PATH, "categories", NULL );
-                help_dir = g_strdup ( HELP_PATH );
-                locale_dir = g_strdup ( LOCALEDIR );
-                pixmaps_dir = g_strdup ( PIXMAPS_DIR );
-                ui_dir = g_strdup ( UI_DIR );
+                categories_dir = g_build_filename (DATA_PATH, "categories", NULL);
+                help_dir = g_strdup (HELP_PATH);
+                locale_dir = g_strdup (LOCALEDIR);
+                pixmaps_dir = g_strdup (PIXMAPS_DIR);
+                ui_dir = g_strdup (UI_DIR);
             }
         }
         #endif /* GTKOSXAPPLICATION */
     }
     #else
     {
-        categories_dir = g_build_filename ( DATA_PATH, "categories", NULL );
-        help_dir = g_strdup ( HELP_PATH );
-        locale_dir = g_strdup ( LOCALEDIR );
-        pixmaps_dir = g_strdup ( PIXMAPS_DIR );
-        ui_dir = g_strdup ( UI_DIR );
+        categories_dir = g_build_filename (DATA_PATH, "categories", NULL);
+        help_dir = g_strdup (HELP_PATH);
+        locale_dir = g_strdup (LOCALEDIR);
+        pixmaps_dir = g_strdup (PIXMAPS_DIR);
+        ui_dir = g_strdup (UI_DIR);
 
-        user_config_dir = g_build_filename ( g_get_user_config_dir (), "grisbi", NULL);
-        user_data_dir = g_build_filename ( g_get_user_data_dir (), "grisbi", NULL);
-        user_default_dir = g_strdup ( g_get_home_dir() );
+        user_config_dir = g_build_filename (g_get_user_config_dir (), "grisbi", NULL);
+        user_data_dir = g_build_filename (g_get_user_data_dir (), "grisbi", NULL);
+        user_default_dir = g_strdup (g_get_home_dir());
     }
     #endif /* OS_OSX */
 }
 #endif /* _WIN32 */
 
-    local_ui_handle = g_dir_open ( local_ui_dir, 0, NULL );
-    if ( NULL != local_ui_handle )
+    local_ui_handle = g_dir_open (local_ui_dir, 0, NULL);
+    if (NULL != local_ui_handle)
     {
-        g_free ( ui_dir );
+        g_free (ui_dir);
         ui_dir = local_ui_dir;
     }
 	else
-		g_free ( local_ui_dir );
+		g_free (local_ui_dir);
 
-    accelerator_filename = g_build_filename ( user_config_dir, "grisbi-accels", NULL );
+    accelerator_filename = g_build_filename (user_config_dir, "grisbi-accels", NULL);
 }
 
-
-void gsb_dirs_shutdown ( void )
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+void gsb_dirs_shutdown (void)
 {
-    g_free ( categories_dir );
-    g_free ( help_dir );
-    g_free ( locale_dir );
-    g_free ( pixmaps_dir );
-    g_free ( ui_dir );
-    g_free ( user_config_dir );
-    g_free ( user_data_dir );
-    g_free ( grisbirc_filename );
-    g_free ( accelerator_filename );
-    g_free ( user_default_dir );
+    g_free (categories_dir);
+    g_free (help_dir);
+    g_free (locale_dir);
+    g_free (pixmaps_dir);
+    g_free (ui_dir);
+    g_free (user_config_dir);
+    g_free (user_data_dir);
+    g_free (grisbirc_filename);
+    g_free (accelerator_filename);
+    g_free (user_default_dir);
 }
 
-
-const gchar *gsb_dirs_get_categories_dir ( void )
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+const gchar *gsb_dirs_get_categories_dir (void)
 {
     return categories_dir;
 }
 
-
-const gchar *gsb_dirs_get_help_dir ( void )
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+const gchar *gsb_dirs_get_help_dir (void)
 {
     return help_dir;
 }
 
-
-const gchar *gsb_dirs_get_locale_dir ( void )
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+const gchar *gsb_dirs_get_locale_dir (void)
 {
     return locale_dir;
 }
 
-
-const gchar *gsb_dirs_get_pixmaps_dir ( void )
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+const gchar *gsb_dirs_get_pixmaps_dir (void)
 {
     return pixmaps_dir;
 }
 
-
-const gchar *gsb_dirs_get_ui_dir ( void )
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+const gchar *gsb_dirs_get_ui_dir (void)
 {
     return ui_dir;
 }
 
-
-const gchar *gsb_dirs_get_grisbirc_filename ( void )
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+const gchar *gsb_dirs_get_grisbirc_filename (void)
 {
     gchar *filename;
 
 #if IS_DEVELOPMENT_VERSION == 1
-    filename = g_strconcat ( PACKAGE, "dev.conf", NULL);
+    filename = g_strconcat (PACKAGE, "dev.conf", NULL);
 #else
-    filename = g_strconcat ( PACKAGE, ".conf", NULL);
+    filename = g_strconcat (PACKAGE, ".conf", NULL);
 #endif
 
-    grisbirc_filename = g_build_filename ( user_config_dir, filename, NULL );
-    g_free ( filename );
+    grisbirc_filename = g_build_filename (user_config_dir, filename, NULL);
+    g_free (filename);
 
     return grisbirc_filename;
 }
 
-
-const gchar *gsb_dirs_get_user_config_dir ( void )
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+const gchar *gsb_dirs_get_user_config_dir (void)
 {
     return user_config_dir;
 }
 
-
-const gchar *gsb_dirs_get_user_data_dir ( void )
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+const gchar *gsb_dirs_get_user_data_dir (void)
 {
     return user_data_dir;
 }
@@ -220,7 +281,7 @@ const gchar *gsb_dirs_get_user_data_dir ( void )
  *
  * \return the accelerator filename with an absolute path
  */
-const gchar *gsb_dirs_get_accelerator_filename ( void )
+const gchar *gsb_dirs_get_accelerator_filename (void)
 {
     return accelerator_filename;
 }
@@ -233,7 +294,7 @@ const gchar *gsb_dirs_get_accelerator_filename ( void )
  *
  * \return the absolute path of the configuration file directory
  */
-const gchar *gsb_dirs_get_default_dir ( void )
+const gchar *gsb_dirs_get_default_dir (void)
 {
     return user_default_dir;
 }
@@ -245,11 +306,11 @@ const gchar *gsb_dirs_get_default_dir ( void )
  *
  *  \return string  must be freed
  */
-gchar *gsb_dirs_get_print_dir_var ( void )
+gchar *gsb_dirs_get_print_dir_var (void)
 {
     gchar *path_str = NULL;
 
-    path_str = g_strdup_printf ( "Paths\n"
+    path_str = g_strdup_printf ("Paths\n"
                         "\tg_get_user_data_dir ()               = %s\n"
                         "\tgsb_dirs_get_user_data_dir ()        = %s\n\n"
                         "\tg_get_user_config_dir ()             = %s\n"
@@ -269,7 +330,7 @@ gchar *gsb_dirs_get_print_dir_var ( void )
                         gsb_dirs_get_categories_dir (),
                         gsb_dirs_get_locale_dir (),
                         gsb_dirs_get_pixmaps_dir (),
-                        gsb_dirs_get_ui_dir () );
+                        gsb_dirs_get_ui_dir ());
 
     return path_str;
 }
--- src/gsb_dirs.h
+++ src/gsb_dirs.h
@@ -3,19 +3,19 @@
 
 #include <glib.h>
 
-void gsb_dirs_init ( char* gsb_bin_path );
-void gsb_dirs_shutdown ( void );
-const gchar *gsb_dirs_get_categories_dir ( void );
-const gchar *gsb_dirs_get_help_dir ( void );
-const gchar *gsb_dirs_get_default_dir ( void );
-const gchar *gsb_dirs_get_locale_dir ( void );
-const gchar *gsb_dirs_get_pixmaps_dir ( void );
-const gchar *gsb_dirs_get_ui_dir ( void );
-const gchar *gsb_dirs_get_user_config_dir ( void );
-const gchar *gsb_dirs_get_user_data_dir ( void );
+void 			gsb_dirs_init 						(char* gsb_bin_path);
+void 			gsb_dirs_shutdown (void);
+const gchar *	gsb_dirs_get_categories_dir 		(void);
+const gchar *	gsb_dirs_get_help_dir 				(void);
+const gchar *	gsb_dirs_get_default_dir 			(void);
+const gchar *	gsb_dirs_get_locale_dir 			(void);
+const gchar *	gsb_dirs_get_pixmaps_dir 			(void);
+const gchar *	gsb_dirs_get_ui_dir 				(void);
+const gchar *	gsb_dirs_get_user_config_dir 		(void);
+const gchar *	gsb_dirs_get_user_data_dir 			(void);
 
-const gchar *gsb_dirs_get_accelerator_filename ( void );
-const gchar *gsb_dirs_get_grisbirc_filename ( void );
+const gchar *	gsb_dirs_get_accelerator_filename 	(void);
+const gchar *	gsb_dirs_get_grisbirc_filename 		(void);
 
-gchar *gsb_dirs_get_print_dir_var ( void );
+gchar *			gsb_dirs_get_print_dir_var 			(void);
 #endif
