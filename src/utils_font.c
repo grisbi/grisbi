4a49bc1bc15fee478c4de39caf94b5256df5639e
Pierre Biava <pierre@pierre.biava.name>
2017-07-04 14:03:45 +0200

    removes deprecated widget GtkFontSelectionDialog

--- src/utils_font.c
+++ src/utils_font.c
@@ -1,7 +1,8 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*     Copyright (C)	2000-2008 Cédric Auger (cedric@grisbi.org)	      */
-/* 			http://www.grisbi.org				      */
+/*     Copyright (C)	2000-2008 Cédric Auger (cedric@grisbi.org)	          */
+/*          2008-2017 Pierre Biava (grisbi@pierre.biava.name)                 */
+/* 			http://www.grisbi.org				                              */
 /*                                                                            */
 /*  This program is free software; you can redistribute it and/or modify      */
 /*  it under the terms of the GNU General Public License as published by      */
@@ -73,6 +74,7 @@ GtkWidget *utils_font_create_button ( gchar **fontname,
     GtkWidget *font_name_label;
     GtkWidget *font_size_label;
 
+
     font_button = gtk_button_new ();
     g_signal_connect ( G_OBJECT ( font_button ), "clicked",
 		       G_CALLBACK ( utils_font_choose ), fontname );
@@ -81,16 +83,13 @@ GtkWidget *utils_font_create_button ( gchar **fontname,
     gtk_container_add (GTK_CONTAINER(font_button), hbox_font);
 
     font_name_label = gtk_label_new (NULL);
+	gtk_widget_set_name (font_name_label, "font_label");
     gtk_box_pack_start ( GTK_BOX ( hbox_font ), font_name_label,
 			 TRUE, TRUE, 5 );
-
-    gtk_box_pack_start ( GTK_BOX ( hbox_font ), gtk_separator_new ( GTK_ORIENTATION_VERTICAL ),
-			 FALSE, FALSE, 0 );
 
     font_size_label = gtk_label_new (NULL);
     gtk_box_pack_start ( GTK_BOX ( hbox_font ), font_size_label,
 			 FALSE, FALSE, 5 );
-
     g_object_set_data ( G_OBJECT (font_button),
 			"hook", hook);
     g_object_set_data ( G_OBJECT (font_button),
@@ -103,9 +102,9 @@ GtkWidget *utils_font_create_button ( gchar **fontname,
 
     utils_font_update_labels(font_button, *fontname);
 
+
     return font_button;
 }
-
 
 /**
  * callback called when click on the font button
@@ -123,24 +122,22 @@ static gboolean utils_font_choose ( GtkWidget *button,
     GtkWidget *dialog;
     GCallback (*hook) (gchar *, gpointer);
 
-    dialog = gtk_font_selection_dialog_new (_("Choosing font"));
+    dialog = gtk_font_chooser_dialog_new (_("Choosing font"), NULL);
 
     if (*fontname)
-	gtk_font_selection_dialog_set_font_name (GTK_FONT_SELECTION_DIALOG(dialog),
-						 *fontname );
-    gtk_window_set_modal ( GTK_WINDOW ( dialog ),
-			   TRUE );
+		gtk_font_chooser_set_font (GTK_FONT_CHOOSER (dialog), *fontname);
+    gtk_window_set_modal ( GTK_WINDOW ( dialog ), TRUE );
 
     switch ( gtk_dialog_run ( GTK_DIALOG ( dialog ) ) )
     {
-	case GTK_RESPONSE_OK:
-	    new_fontname = gtk_font_selection_dialog_get_font_name (GTK_FONT_SELECTION_DIALOG(dialog));
-	    gtk_widget_destroy (dialog);
-	    utils_font_update_labels (button, new_fontname);
-	    break;
-	default:
-	    gtk_widget_destroy (dialog);
-	    return FALSE;
+		case GTK_RESPONSE_OK:
+			new_fontname = gtk_font_chooser_get_font (GTK_FONT_CHOOSER (dialog));
+			gtk_widget_destroy (dialog);
+			utils_font_update_labels (button, new_fontname);
+			break;
+		default:
+			gtk_widget_destroy (dialog);
+			return FALSE;
     }
 
     if (*fontname)
@@ -169,10 +166,11 @@ static gboolean utils_font_choose ( GtkWidget *button,
 static void utils_font_update_labels ( GtkWidget *button,
 				       const gchar *fontname)
 {
-    gchar * font_name, *font_size;
-    PangoFontDescription *font_description;
     GtkWidget *font_name_label;
     GtkWidget *font_size_label;
+    gchar *font_name;
+    gchar *font_size;
+	gchar *data;
 
     font_name_label = g_object_get_data (G_OBJECT (button),
 					 "name_label" );
@@ -180,32 +178,33 @@ static void utils_font_update_labels ( GtkWidget *button,
 					 "size_label" );
     if (fontname)
     {
-	gchar *tmp;
+		GtkCssProvider *css_provider = NULL;
+		gchar *tmp;
 
-	font_description = pango_font_description_from_string (fontname);
+		css_provider = gtk_css_provider_get_default ();
 
-	font_name = my_strdup ( fontname );
-	tmp = font_name + strlen(font_name) - 1;
-	while (g_ascii_isdigit(*tmp) ||
-	       (*tmp) == '.')
-	    tmp --;
-	font_size = tmp+1;
+		data = g_strconcat ("GtkLabel#font_label {font: ", fontname, "}", NULL);
+		gtk_css_provider_load_from_data (css_provider, data, -1, NULL);
 
-	while (*tmp == ' ' ||
-	       *tmp == ',')
-	{
-	    *tmp=0;
-	    tmp--;
-	}
+		font_name = my_strdup ( fontname );
+		tmp = font_name + strlen(font_name) - 1;
+		while (g_ascii_isdigit(*tmp) ||
+			   (*tmp) == '.')
+			tmp --;
+		font_size = tmp+1;
+
+		while (*tmp == ' ' ||
+			   *tmp == ',')
+		{
+			*tmp=0;
+			tmp--;
+		}
     }
     else
     {
-	font_description = NULL;
-	font_name = my_strdup (_("No font defined"));
-	font_size = NULL;
+		font_name = my_strdup (_("No font defined"));
+		font_size = NULL;
     }
-
-    gtk_widget_modify_font (font_name_label, font_description);
 
     gtk_label_set_text (GTK_LABEL(font_name_label), font_name);
     gtk_label_set_text (GTK_LABEL(font_size_label), font_size);
