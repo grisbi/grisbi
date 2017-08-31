87a40fbeafd5e2627f7ee2184b4a1536aa307c15
Pierre Biava <pierre@pierre.biava.name>
2017-08-31 12:38:56 +0200

    Cosmetics changes

--- src/gsb_data_partial_balance.h
+++ src/gsb_data_partial_balance.h
@@ -8,54 +8,58 @@
 /* END_INCLUDE_H */
 
 /* START_DECLARATION */
-GPtrArray *gsb_data_partial_balance_calculate_balances_at_date ( gint partial_balance_number,
-                        GDate *date );
-gboolean gsb_data_partial_balance_drag_data_get ( GtkTreeDragSource * drag_source,
-                        GtkTreePath * path,
-                        GtkSelectionData * selection_data );
-gboolean gsb_data_partial_balance_drag_data_received ( GtkTreeDragDest * drag_dest,
-                        GtkTreePath * dest_path,
-                        GtkSelectionData * selection_data );
-gsb_real gsb_data_partial_balance_get_balance_at_date ( gint partial_balance_number,
-                        GDate *date );
-gboolean gsb_data_partial_balance_get_colorise ( gint partial_balance_number );
-gint gsb_data_partial_balance_get_currency ( gint partial_balance_number );
-gsb_real gsb_data_partial_balance_get_current_amount ( gint partial_balance_number );
-gchar *gsb_data_partial_balance_get_current_balance ( gint partial_balance_number );
-KindAccount gsb_data_partial_balance_get_kind ( gint partial_balance_number );
-GSList *gsb_data_partial_balance_get_list ( void );
-const gchar *gsb_data_partial_balance_get_liste_cptes ( gint partial_balance_number );
-gchar *gsb_data_partial_balance_get_marked_balance ( gint partial_balance_number );
-const gchar *gsb_data_partial_balance_get_name ( gint partial_balance_number );
-gint gsb_data_partial_balance_get_number ( gpointer balance_ptr );
-gboolean gsb_data_partial_balance_init_variables ( void );
-gboolean gsb_data_partial_balance_set_colorise ( gint partial_balance_number,
-                        gboolean colorise );
-gboolean gsb_data_partial_balance_set_currency ( gint partial_balance_number,
-                        gint currency );
-gboolean gsb_data_partial_balance_set_kind ( gint partial_balance_number,
-                        KindAccount kind );
-gboolean gsb_data_partial_balance_set_liste_cptes ( gint partial_balance_number,
-                        const gchar *liste_cptes );
-gboolean gsb_data_partial_balance_set_name ( gint partial_balance_number,
-                        const gchar *name );
-void gsb_partial_balance_add ( GtkWidget *button, GtkWidget *main_widget );
-gboolean gsb_partial_balance_button_press ( GtkWidget *tree_view,
-                        GdkEventButton *ev,
-                        gpointer null );
-void gsb_partial_balance_colorise_toggled ( GtkCellRendererToggle *cell,
-                        gchar *path_str,
-                        GtkWidget *tree_view );
-GtkListStore *gsb_partial_balance_create_model ( void );
-void gsb_partial_balance_delete ( GtkWidget *button, GtkWidget *main_widget );
-void gsb_partial_balance_edit ( GtkWidget *button, GtkWidget *main_widget );
-void gsb_partial_balance_fill_model ( GtkListStore *list_store );
-gboolean gsb_partial_balance_key_press ( GtkWidget *tree_view, GdkEventKey *ev );
-gint gsb_partial_balance_new_at_position ( const gchar *name, gint pos );
-gboolean gsb_partial_balance_select_func ( GtkTreeSelection *selection,
-                        GtkTreeModel *model,
-                        GtkTreePath *path,
-                        gboolean path_currently_selected,
-                        GObject *main_widget );
+GPtrArray *		gsb_data_partial_balance_calculate_balances_at_date	(gint partial_balance_number,
+																	 GDate *date);
+gboolean 		gsb_data_partial_balance_drag_data_get 				(GtkTreeDragSource *drag_source,
+																	 GtkTreePath * path,
+																	 GtkSelectionData *selection_data);
+gboolean 		gsb_data_partial_balance_drag_data_received 		(GtkTreeDragDest *drag_dest,
+																	 GtkTreePath * path,
+																	 GtkSelectionData *selection_data);
+gsb_real 		gsb_data_partial_balance_get_balance_at_date 		(gint partial_balance_number,
+																	 GDate *date);
+gboolean 		gsb_data_partial_balance_get_colorise 				(gint partial_balance_number);
+gint 			gsb_data_partial_balance_get_currency 				(gint partial_balance_number);
+gsb_real 		gsb_data_partial_balance_get_current_amount 		(gint partial_balance_number);
+gchar *			gsb_data_partial_balance_get_current_balance 		(gint partial_balance_number);
+KindAccount 	gsb_data_partial_balance_get_kind 					(gint partial_balance_number);
+GSList *		gsb_data_partial_balance_get_list 					(void);
+const gchar *	gsb_data_partial_balance_get_liste_cptes 			(gint partial_balance_number);
+gchar *			gsb_data_partial_balance_get_marked_balance 		(gint partial_balance_number);
+const gchar *	gsb_data_partial_balance_get_name 					(gint partial_balance_number);
+gint 			gsb_data_partial_balance_get_number 				(gpointer balance_ptr);
+gboolean 		gsb_data_partial_balance_init_variables 			(void);
+gboolean 		gsb_data_partial_balance_set_colorise 				(gint partial_balance_number,
+																	 gboolean colorise);
+gboolean 		gsb_data_partial_balance_set_currency 				(gint partial_balance_number,
+																	 gint currency);
+gboolean 		gsb_data_partial_balance_set_kind 					(gint partial_balance_number,
+																	 KindAccount kind);
+gboolean 		gsb_data_partial_balance_set_liste_cptes 			(gint partial_balance_number,
+																	 const gchar *liste_cptes);
+gboolean 		gsb_data_partial_balance_set_name 					(gint partial_balance_number,
+																	 const gchar *name);
+void 			gsb_partial_balance_add 							(GtkWidget *button,
+																	 GtkWidget *main_widget);
+gboolean 		gsb_partial_balance_button_press 					(GtkWidget *tree_view,
+																	 GdkEventButton *ev,
+																	 gpointer null);
+void 			gsb_partial_balance_colorise_toggled 				(GtkCellRendererToggle *cell,
+																	 gchar *path_str,
+																	 GtkWidget *tree_view);
+GtkListStore *	gsb_partial_balance_create_model 					(void);
+void 			gsb_partial_balance_delete 							(GtkWidget *button,
+																	 GtkWidget *main_widget);
+void 			gsb_partial_balance_edit 							(GtkWidget *button,
+																	 GtkWidget *main_widget);
+void 			gsb_partial_balance_fill_model 						(GtkListStore *list_store);
+gboolean 		gsb_partial_balance_key_press 						(GtkWidget *tree_view,
+																	 GdkEventKey *ev);
+gint 			gsb_partial_balance_new_at_position 				(const gchar *name, gint pos);
+gboolean 		gsb_partial_balance_select_func 					(GtkTreeSelection *selection,
+																	 GtkTreeModel *model,
+																	 GtkTreePath *path,
+																	 gboolean path_currently_selected,
+																	 GObject *main_widget);
 /* END_DECLARATION */
 #endif
--- src/gsb_data_payment.h
+++ src/gsb_data_payment.h
@@ -12,44 +12,44 @@
 
 
 /* START_DECLARATION */
-gint gsb_data_payment_create_default  ( gint account_number );
-gint gsb_data_payment_get_account_number ( gint payment_number );
-gint gsb_data_payment_get_automatic_numbering ( gint payment_number );
-const gchar *gsb_data_payment_get_last_number ( gint payment_number );
-gint gsb_data_payment_get_last_number_to_int ( gint payment_number );
-GSList *gsb_data_payment_get_list_for_account ( gint account_number );
-const gchar *gsb_data_payment_get_name ( gint payment_number );
-gint gsb_data_payment_get_number ( gpointer payment_ptr );
-gint gsb_data_payment_get_number_by_name ( const gchar *name,
-						    gint account_number );
-GSList *gsb_data_payment_get_payments_list ( void );
-gint gsb_data_payment_get_show_entry ( gint payment_number );
-gint gsb_data_payment_get_sign ( gint payment_number );
-gint gsb_data_payment_get_similar ( gint origin_payment,
-				    gint target_account_number );
-gint gsb_data_payment_get_transfer_payment_number ( gint account_number );
-gchar *gsb_data_payment_incremente_last_number ( gint payment_number,
-                    gint increment );
-gboolean gsb_data_payment_init_variables ( void );
-gint gsb_data_payment_new ( const gchar *name );
-gboolean gsb_data_payment_remove ( gint payment_number );
-gint gsb_data_payment_search_number_other_account_by_name ( gint payment_number,
-                                gint account_number );
-gboolean gsb_data_payment_set_account_number ( gint payment_number,
-					       gint account_number );
-gboolean gsb_data_payment_set_automatic_numbering ( gint payment_number,
-						    gint automatic_numbering );
-gboolean gsb_data_payment_set_last_number ( gint payment_number,
-					    const gchar *last_number );
-gboolean gsb_data_payment_set_last_number_from_int ( gint payment_number,
-					    gint last_number );
-gboolean gsb_data_payment_set_name ( gint payment_number,
-				     const gchar *name );
-gint gsb_data_payment_set_new_number ( gint payment_number,
-				       gint new_no_payment );
-gboolean gsb_data_payment_set_show_entry ( gint payment_number,
-					   gint show_entry );
-gboolean gsb_data_payment_set_sign ( gint payment_number,
-				     gint sign );
+gint 			gsb_data_payment_create_default  						(gint account_number);
+gint 			gsb_data_payment_get_account_number 					(gint payment_number);
+gint 			gsb_data_payment_get_automatic_numbering 				(gint payment_number);
+const gchar *	gsb_data_payment_get_last_number 						(gint payment_number);
+gint 			gsb_data_payment_get_last_number_to_int 				(gint payment_number);
+GSList *		gsb_data_payment_get_list_for_account 					(gint account_number);
+const gchar *	gsb_data_payment_get_name 								(gint payment_number);
+gint 			gsb_data_payment_get_number 							(gpointer payment_ptr);
+gint 			gsb_data_payment_get_number_by_name 					(const gchar *name,
+																		 gint account_number);
+GSList *		gsb_data_payment_get_payments_list 						(void);
+gint 			gsb_data_payment_get_show_entry 						(gint payment_number);
+gint 			gsb_data_payment_get_sign 								(gint payment_number);
+gint 			gsb_data_payment_get_similar 							(gint origin_payment,
+																		 gint target_account_number);
+gint 			gsb_data_payment_get_transfer_payment_number 			(gint account_number);
+gchar *			gsb_data_payment_incremente_last_number 				(gint payment_number,
+																		 gint increment);
+gboolean 		gsb_data_payment_init_variables 						(void);
+gint 			gsb_data_payment_new 									(const gchar *name);
+gboolean 		gsb_data_payment_remove 								(gint payment_number);
+gint 			gsb_data_payment_search_number_other_account_by_name 	(gint payment_number,
+																		 gint account_number);
+gboolean 		gsb_data_payment_set_account_number 					(gint payment_number,
+																		 gint account_number);
+gboolean 		gsb_data_payment_set_automatic_numbering 				(gint payment_number,
+																		 gint automatic_numbering);
+gboolean 		gsb_data_payment_set_last_number 						(gint payment_number,
+																		 const gchar *last_number);
+gboolean 		gsb_data_payment_set_last_number_from_int 				(gint payment_number,
+																		 gint last_number);
+gboolean 		gsb_data_payment_set_name 								(gint payment_number,
+																		 const gchar *name);
+gint 			gsb_data_payment_set_new_number 						(gint payment_number,
+																		 gint new_no_payment);
+gboolean 		gsb_data_payment_set_show_entry 						(gint payment_number,
+																		 gint show_entry);
+gboolean 		gsb_data_payment_set_sign 								(gint payment_number,
+																		 gint sign);
 /* END_DECLARATION */
 #endif
