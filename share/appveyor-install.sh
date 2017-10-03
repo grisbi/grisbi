5ca50f00a79ca71cc18d451e640083a64969dda8
xfred81 <32123746+xfred81@users.noreply.github.com>
2017-10-03 22:36:12 +0200

    AppVeyor integration, 32 & 64bit
    * MSYSTEM used in AppVeyor's environment (ease integration)
    * Update of MSYS2 core system disabled (may block AppVeyor as it may require to start a new console)
    * First import for install/builder script
    * Support of Windows 32bit & 64bit targets
    * Explicit version & architecture in artifacts
    
    In appveyor.yml:
    Delivery keys have to be adjusted; also, note that only autotools version (currently 1.1.1) is kept in AppVeyor: no build number is used for now, so that AppVeyor may complain about duplicate version. As a workaround, remove any duplicate version(s) in AppVeyor and in github's packages' section.

--- share/appveyor-install.sh
+++ share/appveyor-install.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-if test "$MSYSTEM"x == "MINGW64"; then
-	i_pkg_postfix = "w64-x86_64"
+source /appveyor.environment
+export MSYSTEM
+
+if test "$MSYSTEM"x == "MINGW64"x; then
+	i_pkg_postfix="w64-x86_64"
+else
+	i_pkg_postfix="w64-i686"
 fi
 
-pacman -Syu --noconfirm
+#if test "$MSYSTEM"x == "MINGW64"; then
+# pacman -Syu --noconfirm
+#fi
+
+pacman -S --noconfirm mingw-glib2-devel
 pacman -S --noconfirm mingw-$i_pkg_postfix-cairo
 pacman -S --noconfirm mingw-$i_pkg_postfix-gtk3
 pacman -S --noconfirm mingw-$i_pkg_postfix-freetype
-pacman -S --noconfirm mingw-$i_pkg_postfix-libgsf  
-pacman -S unzip --noconfirm
+pacman -S --noconfirm mingw-$i_pkg_postfix-libgsf 
