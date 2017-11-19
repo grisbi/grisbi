183790946ca5f1d335ebf92fa5f6c1c3383fba0b
Pierre Biava <pierre@pierre.biava.name>
2017-11-19 19:27:03 +0100

    Cosmetics changes

--- src/gsb_form_scheduler.h
+++ src/gsb_form_scheduler.h
@@ -26,24 +26,24 @@ enum scheduled_form_widget {
 
 
 /* START_DECLARATION */
-gboolean gsb_form_scheduler_button_press_event ( GtkWidget *entry,
-                        GdkEventButton *ev,
-                        gint *ptr_origin );
-gboolean gsb_form_scheduler_change_account ( GtkWidget *button,
-                        gpointer null );
-gboolean gsb_form_scheduler_clean ( void );
-gboolean gsb_form_scheduler_create ( GtkWidget *table );
-gboolean gsb_form_scheduler_entry_lose_focus ( GtkWidget *entry,
-                        GdkEventFocus *ev,
-                        gint *ptr_origin );
-gboolean gsb_form_scheduler_free_list ( void );
-gint gsb_form_scheduler_get_account ( void );
-GtkWidget *gsb_form_scheduler_get_element_widget ( gint element_number );
-gboolean gsb_form_scheduler_get_scheduler_part ( gint scheduled_number );
-gboolean gsb_form_scheduler_recover_splits_of_transaction ( gint scheduled_transaction,
-							    gint transaction_number );
-gboolean gsb_form_scheduler_sensitive_buttons ( gboolean sensitive );
-gboolean gsb_form_scheduler_set ( gint scheduled_number );
-gboolean gsb_form_scheduler_set_frequency ( gint frequency );
+gboolean 	gsb_form_scheduler_button_press_event 				(GtkWidget *entry,
+																 GdkEventButton *ev,
+																 gint *ptr_origin);
+gboolean 	gsb_form_scheduler_change_account 					(GtkWidget *button,
+																 gpointer null);
+gboolean 	gsb_form_scheduler_clean 							(void);
+gboolean 	gsb_form_scheduler_create 							(GtkWidget *table);
+gboolean 	gsb_form_scheduler_entry_lose_focus 				(GtkWidget *entry,
+																 GdkEventFocus *ev,
+																 gint *ptr_origin);
+gboolean 	gsb_form_scheduler_free_list 						(void);
+gint 		gsb_form_scheduler_get_account 						(void);
+GtkWidget *	gsb_form_scheduler_get_element_widget 				(gint element_number);
+gboolean 	gsb_form_scheduler_get_scheduler_part 				(gint scheduled_number);
+gboolean 	gsb_form_scheduler_recover_splits_of_transaction 	(gint scheduled_transaction,
+																 gint transaction_number);
+gboolean 	gsb_form_scheduler_sensitive_buttons 				(gboolean sensitive);
+gboolean 	gsb_form_scheduler_set								(gint scheduled_number);
+gboolean 	gsb_form_scheduler_set_frequency 					(gint frequency);
 /* END_DECLARATION */
 #endif
