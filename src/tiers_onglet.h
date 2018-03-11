5bc4af68dcbcf1442581a0cd89b551f625ea6d07
Pierre Biava <pierre@pierre.biava.name>
2018-03-11 17:08:48 +0100

    Cosmetics changes

--- src/tiers_onglet.h
+++ src/tiers_onglet.h
@@ -8,21 +8,18 @@
 
 
 /* START_DECLARATION */
-void gsb_gui_payees_toolbar_set_style ( gint toolbar_style );
-GtkWidget *payees_create_list ( void );
-void payees_fill_list ( void );
-void payees_init_variables_list ( void );
-GtkWidget *payees_get_tree_view ( void );
-GtkTreeStore *payees_get_tree_store ( void );
-
-void payees_new_payee ( void );
-void payees_delete_payee ( void );
-void payees_edit_payee ( void );
-
-void payees_manage_payees ( void );
-void payees_remove_unused_payees ( void );
-
-gboolean payees_hold_position_set_expand ( gboolean expand );
-gboolean payees_hold_position_set_path ( GtkTreePath *path );
+void 			gsb_gui_payees_toolbar_set_style 	(gint toolbar_style);
+GtkWidget *		payees_create_list 					(void);
+void 			payees_delete_payee 				(void);
+void 			payees_edit_payee 					(void);
+void 			payees_fill_list 					(void);
+GtkTreeStore *	payees_get_tree_store 				(void);
+GtkWidget *		payees_get_tree_view 				(void);
+gboolean 		payees_hold_position_set_expand 	(gboolean expand);
+gboolean 		payees_hold_position_set_path 		(GtkTreePath *path);
+void 			payees_init_variables_list 			(void);
+void 			payees_manage_payees 				(void);
+void 			payees_new_payee 					(void);
+void 			payees_remove_unused_payees 		(void);
 /* END_DECLARATION */
 #endif
