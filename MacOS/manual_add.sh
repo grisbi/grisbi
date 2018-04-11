dd4c91a49793575333dc7e7e13d60f8c8b2011ff
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-04-11 16:49:04 +0200

    macOS: Fix OFX support
    Closes issue 1836
    http://www.grisbi.org/bugsreports/view.php?id=1836

--- MacOS/manual_add.sh
+++ MacOS/manual_add.sh
@@ -6,7 +6,13 @@ set -e
 
 DEST=MacOS/dist/Grisbi.app/Contents/Resources
 
-EXTRA_FILES="share/icons/Adwaita/scalable/actions/pan-down-symbolic.svg
+EXTRA_FILES="
+lib/goffice/
+share/libofx/dtd/
+share/icons/Adwaita/scalable/actions/list-add-symbolic.svg
+share/icons/Adwaita/scalable/actions/list-remove-all-symbolic.svg
+share/icons/Adwaita/scalable/actions/list-remove-symbolic.svg
+share/icons/Adwaita/scalable/actions/pan-down-symbolic.svg
 share/icons/Adwaita/scalable/actions/pan-end-symbolic-rtl.svg
 share/icons/Adwaita/scalable/actions/pan-end-symbolic.svg
 share/icons/Adwaita/scalable/actions/pan-start-symbolic-rtl.svg
