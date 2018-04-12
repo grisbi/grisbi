4dcab0266ae829639b27e974cd41631950c6e67f
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-04-12 17:54:27 +0200

    Fix Negative array index write (NEGATIVE_RETURNS)
    find_element_col_for_archive() may return -1 in case of error.
    
    In that case visible_col[element_date] would return a strange value with
    element_date = -1

--- src/transaction_list.c
+++ src/transaction_list.c
@@ -323,7 +323,11 @@ void transaction_list_append_archive ( gint archive_store_number )
     archive_number = gsb_data_archive_store_get_archive_number ( archive_store_number );
 
     if ( find_element_col ( ELEMENT_DATE ) == 0 )
+    {
         element_date = find_element_col_for_archive ( );
+        if (element_date < 0)
+            return;
+    }
 
     newrecord -> visible_col[element_date] = gsb_format_gdate (
                         gsb_data_archive_get_beginning_date ( archive_number ) );
