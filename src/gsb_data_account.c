3bd9e91669640d550cf3143361979868afa872eb
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-04-12 17:08:00 +0200

    Fix Out-of-bounds read (OVERRUN)
    The array column_element_sort[] has CUSTOM_MODEL_VISIBLE_COLUMNS
    elements.
    The array index must be _stricly_ smaller than
    CUSTOM_MODEL_VISIBLE_COLUMNS since the last index is
    CUSTOM_MODEL_VISIBLE_COLUMNS-1.

--- src/gsb_data_account.c
+++ src/gsb_data_account.c
@@ -1241,7 +1241,7 @@ gint gsb_data_account_get_element_sort ( gint account_number,
 
     if ( no_column < 0
 	 ||
-	 no_column > CUSTOM_MODEL_VISIBLE_COLUMNS )
+	 no_column >= CUSTOM_MODEL_VISIBLE_COLUMNS )
     {
         gchar* tmpstr;
 
