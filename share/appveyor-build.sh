390aec89e99271aa35aef00054e23a6b76959c23
Pierre Biava <pierre@pierre.biava.name>
2018-05-16 20:13:33 +0200

    Changes secure string for PBi, remove warnings and add hour in version number
    Changes secure string for PBi
    remove warnings in appveyor log
    add hour in version number

--- share/appveyor-build.sh
+++ share/appveyor-build.sh
@@ -18,9 +18,10 @@ cd /c/projects/grisbi-src
 ./configure --prefix /c/projects/grisbi-inst/ --with-ofx PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/inst/lib/pkgconfig
 
 v=$(grep PACKAGE_VERSION config.h | cut -f2 -d '"')
-v="$v-$(date +'%Y.%m.%d')"
+v="$v-$(date +'%Y.%m.%d-%H')"
 powershell.exe -command "Update-AppveyorBuild -Version \"$v\""
 # -B%APPVEYOR_BUILD_NUMBER%\""
+echo "Version = $v"
 
 make -j 2
 
@@ -28,3 +29,12 @@ make install
 
 cd /nsis-3.03
 ./makensis.exe /c/projects/grisbi-src/share/grisbi.nsi
+
+cd /c/projects/grisbi-src
+
+if [ $MSYSTEM = "MINGW32" ]; then
+  powershell.exe -command "Push-AppveyorArtifact \"share/Grisbi-32bit-$v-setup.exe\" -DeploymentName \"grisbi-compil\""
+else
+  powershell.exe -command "Push-AppveyorArtifact \"share/Grisbi-64bit-$v-setup.exe\" -DeploymentName \"grisbi-compil\""
+fi
+exit
