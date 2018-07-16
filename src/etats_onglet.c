d68c673cc40744eb35d8b471235a9791e340cb85
Pierre Biava <pierre.biava@orange.fr>
2018-07-16 21:07:55 +0200

    Modifications of the informations for the search report and blank report
    Changed default options of the serach report see bug 1858

--- src/etats_onglet.c
+++ src/etats_onglet.c
@@ -1048,34 +1048,19 @@ gboolean etats_onglet_ajoute_etat (void)
 							     1);
 
 
-/* 	    gsb_data_report_set_show_report_transactions (report_number,
- * 							   1);
- * 	    gsb_data_report_set_show_report_date (report_number,
- * 						   1);
- * 	    gsb_data_report_set_show_report_value_date (report_number,
- * 							 1);
- * 	    gsb_data_report_set_show_report_payee (report_number,
- * 						    1);
- * 	    gsb_data_report_set_show_report_category (report_number,
- * 						       1);
- * 	    gsb_data_report_set_split_credit_debit (report_number,
- * 						     1);
- * 	    gsb_data_report_set_transfer_choice (report_number,
- * 						  2);
- * 	    gsb_data_report_set_category_used (report_number,
- * 						1);
- * 	    gsb_data_report_set_category_show_category_amount (report_number,
- * 								1);
- * 	    gsb_data_report_set_category_show_sub_category (report_number,
- * 							     1);
- * 	    gsb_data_report_set_category_show_sub_category_amount (report_number,
- * 								    1);
- * 	    gsb_data_report_set_category_show_name (report_number,
- * 						     1);
- * 	    gsb_data_report_set_amount_comparison_only_report_non_null (report_number,
- * 									 1);
- */
-
+ 	    gsb_data_report_set_show_report_transactions (report_number, 1);
+ 	    gsb_data_report_set_show_report_date (report_number, 1);
+ 	    //~ gsb_data_report_set_show_report_value_date (report_number, 1);
+ 	    gsb_data_report_set_show_report_payee (report_number, 1);
+ 	    gsb_data_report_set_show_report_category (report_number, 1);
+ 	    gsb_data_report_set_split_credit_debit (report_number, 1);
+ 	    gsb_data_report_set_transfer_choice (report_number, 2);
+ 	    gsb_data_report_set_category_used (report_number, 1);
+ 	    gsb_data_report_set_category_show_category_amount (report_number, 1);
+ 	    gsb_data_report_set_category_show_sub_category (report_number, 1);
+ 	    gsb_data_report_set_category_show_sub_category_amount (report_number, 1);
+ 	    gsb_data_report_set_category_show_name (report_number, 1);
+ 	    gsb_data_report_set_amount_comparison_only_report_non_null (report_number, 1);
 
 	    /*   tout le reste est à NULL, ce qui est très bien */
 
@@ -1274,64 +1259,38 @@ gboolean etats_onglet_ajoute_etat (void)
 								GINT_TO_POINTER (6)));
 
 	    /*   les devises sont à 1 (euro) */
+	    gsb_data_report_set_currency_general (report_number, 1);
 
-	    gsb_data_report_set_currency_general (report_number,
-						   1);
-	    gsb_data_report_set_category_currency (report_number,
-						    1);
-	    gsb_data_report_set_budget_currency (report_number,
-						  1);
-	    gsb_data_report_set_payee_currency (report_number,
-						 1);
-	    gsb_data_report_set_amount_comparison_currency (report_number,
-							     1);
+		gsb_data_report_set_category_currency (report_number, 1);
+	    gsb_data_report_set_budget_currency (report_number, 1);
+	    gsb_data_report_set_payee_currency (report_number, 1);
+	    gsb_data_report_set_amount_comparison_currency (report_number, 1);
+ 	    gsb_data_report_set_show_report_transactions (report_number, 1);
+ 	    gsb_data_report_set_show_report_date (report_number, 1);
+	    //~ gsb_data_report_set_show_report_value_date (report_number, 1);
+	    gsb_data_report_set_show_report_payee (report_number, 1);
+ 	    gsb_data_report_set_show_report_category (report_number, 1);
+ 	    gsb_data_report_set_show_report_sub_category (report_number, 1);
+ 	    //~ gsb_data_report_set_show_report_method_of_payment (report_number, 1);
+ 	    //~ gsb_data_report_set_show_report_budget (report_number, 1);
+ 	    //~ gsb_data_report_set_show_report_sub_budget (report_number, 1);
+ 	    //~ gsb_data_report_set_show_report_method_of_payment_content (report_number, 1);
+ 	    gsb_data_report_set_show_report_note (report_number, 1);
+ 	    //~ gsb_data_report_set_show_report_voucher (report_number, 1);
+		//~ gsb_data_report_set_show_report_marked (report_number, 1);
+		//~ gsb_data_report_set_show_report_bank_references (report_number, 1);
+		//~ gsb_data_report_set_show_report_financial_year (report_number, 1);
 
+		gsb_data_report_set_report_can_click (report_number, 1);
+ 	    gsb_data_report_set_date_type (report_number, 4);
+	    //~ gsb_data_report_set_period_split (report_number, 1);
+ 	    //~ gsb_data_report_set_period_split_type (report_number, 3);
+ 	    //~ gsb_data_report_set_transfer_choice (report_number, 2);
 
-/* 	    gsb_data_report_set_show_report_transactions (report_number,
- * 							   1);
- * 	    gsb_data_report_set_show_report_date (report_number,
- * 						   1);
- * 	    gsb_data_report_set_show_report_value_date (report_number,
- * 							 1);
- * 	    gsb_data_report_set_show_report_payee (report_number,
- * 						    1);
- * 	    gsb_data_report_set_show_report_category (report_number,
- * 						       1);
- * 	    gsb_data_report_set_show_report_sub_category (report_number,
- * 							   1);
- * 	    gsb_data_report_set_show_report_method_of_payment (report_number,
- * 								1);
- * 	    gsb_data_report_set_show_report_budget (report_number,
- * 						     1);
- * 	    gsb_data_report_set_show_report_sub_budget (report_number,
- * 							 1);
- * 	    gsb_data_report_set_show_report_method_of_payment_content (report_number,
- * 									1);
- * 	    gsb_data_report_set_show_report_note (report_number,
- * 						   1);
- * 	    gsb_data_report_set_show_report_voucher (report_number,
- * 						      1);
- * 	    gsb_data_report_set_show_report_marked (report_number,
- * 						     1);
- * 	    gsb_data_report_set_show_report_bank_references (report_number,
- * 							      1);
- * 	    gsb_data_report_set_show_report_financial_year (report_number,
- * 							     1);
- */
+		/* coche l'affichage des titres de colonnes */
+		gsb_data_report_set_column_title_show (report_number, 1);
 
-/* 	    gsb_data_report_set_report_can_click (report_number,
- * 						   1);
- * 	    gsb_data_report_set_date_type (report_number,
- * 					    4);
- * 	    gsb_data_report_set_period_split (report_number,
- * 					       1);
- * 	    gsb_data_report_set_period_split_type (report_number,
- * 						    3);
- * 	    gsb_data_report_set_transfer_choice (report_number,
- * 						  2);
- */
-
-	    /*   tout le reste est à NULL, ce qui est très bien */
+		/*   tout le reste est à NULL, ce qui est très bien */
 
 	    break;
 
