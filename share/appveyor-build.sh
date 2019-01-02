06b5cb621f60b278e081ee236b978d4b6f072801
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-01-02 23:12:18 +0100

    AppVeyor: enable warnings
    The same warnings are also used for Travis-CI.
    See file travis-ci/travis-autogen.sh

--- share/appveyor-build.sh
+++ share/appveyor-build.sh
@@ -20,6 +20,40 @@ export PATH
 cd /c/projects/grisbi-src
 ./autogen.sh
 
+# Warnings enabled
+CFLAGS=""
+CFLAGS="-Wall"
+CFLAGS+=" -Wextra"
+
+#CFLAGS+=" -Wbad-function-cast"
+#CFLAGS+=" -Wcast-align"
+CFLAGS+=" -Wchar-subscripts"
+CFLAGS+=" -Wempty-body"
+CFLAGS+=" -Wformat"
+CFLAGS+=" -Wformat-security"
+CFLAGS+=" -Winit-self"
+CFLAGS+=" -Winline"
+CFLAGS+=" -Wmissing-declarations"
+CFLAGS+=" -Wmissing-include-dirs"
+CFLAGS+=" -Wmissing-prototypes"
+CFLAGS+=" -Wnested-externs"
+CFLAGS+=" -Wold-style-definition"
+CFLAGS+=" -Wpointer-arith"
+CFLAGS+=" -Wredundant-decls"
+#CFLAGS+=" -Wshadow"
+CFLAGS+=" -Wstrict-prototypes"
+CFLAGS+=" -Wswitch-enum"
+CFLAGS+=" -Wundef"
+CFLAGS+=" -Wuninitialized"
+CFLAGS+=" -Wunused"
+#CFLAGS+=" -Wwrite-strings"
+
+# warnings disabled on purpose
+CFLAGS+=" -Wno-unused-parameter"
+
+export CFLAGS
+echo "CFLAGS: $CFLAGS"
+
 ./configure --prefix /c/projects/grisbi-inst/ --with-ofx --with-goffice PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/inst/lib/pkgconfig
 
 v=$(grep PACKAGE_VERSION config.h | cut -f2 -d '"')
