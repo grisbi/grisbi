c00cf2c9ef8909692e412a14da81c33fc5b4e086
Ludovic Rousseau <ludovic.rousseau@free.fr>
2019-10-15 23:48:58 +0200

    Travis-CI: deprecated-declarations are not critical for werror
    Do not fail the build if "deprecated" warnings are emitted like in:
    /usr/include/libgsf-1/gsf/gsf-utils.h:303:9: warning: 'GParameter' is deprecated [-Wdeprecated-declarations]

--- travis-ci/travis-autogen.sh
+++ travis-ci/travis-autogen.sh
@@ -48,6 +48,9 @@ case "$CONF" in
 			# build with:
 			# /usr/local/Cellar/gdk-pixbuf/2.38.1_1/include/gdk-pixbuf-2.0/gdk-pixbuf/gdk-pixbuf-animation.h:122:85: warning: 'GTimeVal' is deprecated: Use 'GDateTime' instead [-Wdeprecated-declarations]
 			configure_args+="-Wno-deprecated-declarations"
+		else
+			# /usr/include/libgsf-1/gsf/gsf-utils.h:303:9: error: 'GParameter' is deprecated [-Werror=deprecated-declarations]
+			configure_args+="-Wno-deprecated-declarations"
 		fi
 
 		# disable goffice since it uses -pthread that generate warnings and then
