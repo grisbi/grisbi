32b904ce6fe3cfab19940e08a4626d63ea4be6cc
Pierre Biava <pierre@pierre.biava.name>
2020-04-24 11:21:18 +0200

    Adding two files to group all utils_widgets.

---
+++ src/utils_widgets.c
@@ -0,0 +1,60 @@
+/* *******************************************************************************/
+/*                                 GRISBI                                        */
+/*              Programme de gestion financière personnelle                      */
+/*                              license : GPLv2                                  */
+/*                                                                               */
+/*     Copyright (C)    2000-2008 Cédric Auger (cedric@grisbi.org)               */
+/*                      2003-2008 Benjamin Drieu (bdrieu@april.org)              */
+/*          2008-2020 Pierre Biava (grisbi@pierre.biava.name)                    */
+/*          http://www.grisbi.org                                                */
+/*                                                                               */
+/*     This program is free software; you can redistribute it and/or modify      */
+/*     it under the terms of the GNU General Public License as published by      */
+/*     the Free Software Foundation; either version 2 of the License, or         */
+/*     (at your option) any later version.                                       */
+/*                                                                               */
+/*     This program is distributed in the hope that it will be useful,           */
+/*     but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*     GNU General Public License for more details.                              */
+/*                                                                               */
+/*     You should have received a copy of the GNU General Public License         */
+/*     along with this program; if not, write to the Free Software               */
+/*     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/*                                                                               */
+/* *******************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "include.h"
+#include <glib/gi18n.h>
+
+/*START_INCLUDE*/
+#include "utils_widgets.h"
+#include "erreur.h"
+/*END_INCLUDE*/
+
+/*START_STATIC*/
+/*END_STATIC*/
+
+/*START_EXTERN*/
+/*END_EXTERN*/
+
+/******************************************************************************/
+/* Private functions                                                          */
+/******************************************************************************/
+/******************************************************************************/
+/* Public functions                                                           */
+/******************************************************************************/
+/**
+ *
+ *
+ * \param
+ *
+ * \return
+ **/
+/* Local Variables: */
+/* c-basic-offset: 4 */
+/* End: */
---
+++ src/utils_widgets.h
@@ -0,0 +1,12 @@
+#ifndef _UTILS_WIDGETS_H
+#define _UTILS_WIDGETS_H (1)
+
+#include <gtk/gtk.h>
+
+/* START_INCLUDE_H */
+/* END_INCLUDE_H */
+
+
+/* START_DECLARATION */
+/* END_DECLARATION */
+#endif
