a76b1b5ab6550716829abfe4bd7050af7fa5d103
Pierre Biava <pierre@pierre.biava.name>
2020-07-05 21:07:34 +0200

    Adding a script to create a package with the last commit hash

---
+++ share/create_git_distrib.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# sortie si erreur
+set -e
+
+echo "configuration de grisbi"
+version=$(git rev-parse --short HEAD)
+echo "Using git version: $version"
+
+FILE=configure.ac
+sed -i.old -E "s/GRISBI_GIT_HASH/$version/" $FILE
+
+./autogen.sh
+./configure --with-openssl --with-ofx --with-goffice --with-libxml2
+make clean
+
+echo "création du package de Grisbi"
+make dist
+
+# restore configure.ac
+echo restauration du fichier configure.ac
+mv $FILE.old $FILE
+
