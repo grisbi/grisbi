c97220b8810111adf3223722528aad297c425583
Ludovic Rousseau <ludovic.rousseau@free.fr>
2020-09-24 15:38:54 +0200

    macOS: add create-launcher.sh script
    This script is used to integrate Grisbi with its launcher.

---
+++ MacOS/create-launcher.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Script to add build the launcher and include Grisbi.app
+
+# On macOS Catalina and later access to Documents, Desktop & Downloads
+# folders is not allowed by default. On the first access a dialog is
+# displayed by the system.
+# Unfortunately the dialog is generated only when a Coca application
+# is running. For the Gtk+3 application we need to use a Cocoa launcher
+# that will "generate" the dialog and get the access rights.
+
+set -e
+
+xcodebuild -project Grisbi\ Launcher/Grisbi\ Launcher.xcodeproj clean
+xcodebuild -project Grisbi\ Launcher/Grisbi\ Launcher.xcodeproj
+
+mv dist/Grisbi.app dist/Grisbi.app.real
+
+cp -a Grisbi\ Launcher/build/Release/Grisbi.app dist/
+mv dist/Grisbi.app.real dist/Grisbi.app/Contents/Resources/Grisbi.app
