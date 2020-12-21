fe24a1b0136f2d43b8f2b8fccce4c53ca0edbbf2
Ludovic Rousseau <ludovic.rousseau@free.fr>
2020-12-21 16:27:32 +0100

    Add build github actions
    Build on GNU/Linux and macOS

---
+++ github-actions/build.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+env
+echo "CONF: $CONF"
+
+# Warnings enabled
+CFLAGS=""
+CFLAGS="-Wall"
+CFLAGS+=" -Wextra"
+
+#CFLAGS+=" -Wbad-function-cast"
+#CFLAGS+=" -Wcast-align"
+CFLAGS+=" -Wchar-subscripts"
+CFLAGS+=" -Wempty-body"
+CFLAGS+=" -Wformat"
+CFLAGS+=" -Wformat-security"
+CFLAGS+=" -Winit-self"
+CFLAGS+=" -Winline"
+CFLAGS+=" -Wmissing-declarations"
+CFLAGS+=" -Wmissing-include-dirs"
+CFLAGS+=" -Wmissing-prototypes"
+CFLAGS+=" -Wnested-externs"
+CFLAGS+=" -Wold-style-definition"
+CFLAGS+=" -Wpointer-arith"
+CFLAGS+=" -Wredundant-decls"
+#CFLAGS+=" -Wshadow"
+CFLAGS+=" -Wstrict-prototypes"
+CFLAGS+=" -Wswitch-enum"
+CFLAGS+=" -Wundef"
+CFLAGS+=" -Wuninitialized"
+CFLAGS+=" -Wunused"
+#CFLAGS+=" -Wwrite-strings"
+
+# warnings disabled on purpose
+CFLAGS+=" -Wno-unused-parameter"
+
+configure_args=""
+
+# special configurations
+case "$CONF" in
+	*werror*)
+		# fail on warning
+		configure_args+=" --enable-real-werror"
+
+		if [ "$RUNNER_OS" = "macOS" ]
+		then
+			# disable deprecated warnings since gdk-pixbuf fails to
+			# build with:
+			# /usr/local/Cellar/gdk-pixbuf/2.38.1_1/include/gdk-pixbuf-2.0/gdk-pixbuf/gdk-pixbuf-animation.h:122:85: warning: 'GTimeVal' is deprecated: Use 'GDateTime' instead [-Wdeprecated-declarations]
+			CFLAGS+=" -Wno-deprecated-declarations"
+		else
+			# /usr/include/libgsf-1/gsf/gsf-utils.h:303:9: error: 'GParameter' is deprecated [-Werror=deprecated-declarations]
+			CFLAGS+=" -Wno-deprecated-declarations"
+		fi
+
+		# disable goffice since it uses -pthread that generate warnings and then
+		# errors
+		configure_args+=" --without-goffice"
+	;;
+
+	*goffice*)
+		# enable goffice
+		configure_args+=" --with-goffice"
+	;;
+esac
+
+export CFLAGS
+echo "CFLAGS: $CFLAGS"
+
+if [ "$RUNNER_OS" = "macOS" ]
+then
+	# from brew
+	export PKG_CONFIG_PATH=/usr/local/opt/libxml2/lib/pkgconfig:/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgconfig
+	export PATH="$PATH:/usr/local/opt/gettext/bin"
+
+	mkdir m4
+	ln -sf /usr/local/opt/gettext/share/aclocal/nls.m4 m4
+fi
+
+# exit on error
+set -e
+
+echo "configure_args: $configure_args $@"
+./configure $configure_args "$@"
---
+++ github-actions/setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$RUNNER_OS" == "Linux" ]; then
+	echo "GNU/Linux"
+
+	sudo apt install \
+		autoconf \
+		automake \
+		build-essential \
+		intltool \
+		libgoffice-0.10-dev \
+		libgsf-1-dev \
+		libgtk-3-dev \
+		libofx-dev \
+		libssl-dev \
+		libtool-bin m4
+elif [ "$RUNNER_OS" == "Windows" ]; then
+	#choco install important_windows_software
+	echo "Windows"
+elif [ "$RUNNER_OS" == "macOS" ]; then
+	echo "macOS"
+
+	# use content of Brewfile file
+	brew bundle
+
+	# fix configure: error: XML::Parser perl module is required for intltool
+	perl -MCPAN -e 'install XML::Parser'
+
+	# from brew
+	export PKG_CONFIG_PATH=/usr/local/opt/libxml2/lib/pkgconfig:/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgconfig
+	export PATH="$PATH:/usr/local/opt/gettext/bin"
+
+	mkdir m4
+	ln -sf /usr/local/opt/gettext/share/aclocal/nls.m4 m4
+else
+	echo "$RUNNER_OS not supported"
+	exit 1
+fi
+
+
