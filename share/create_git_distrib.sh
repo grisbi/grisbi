6100ee098e5727fc1f8f83f7f4a34e617023caa0
Ludovic Rousseau <ludovic.rousseau@free.fr>
2021-01-05 21:46:02 +0100

    git snapshot: do not use goffice on macOS
    For yet unknown reasons the use of goffice is bogus on some versions of
    macOS.

--- share/create_git_distrib.sh
+++ share/create_git_distrib.sh
@@ -10,8 +10,24 @@ echo "Using git version: $version"
 FILE=configure.ac
 sed -i.old -E "s/GRISBI_GIT_HASH/$version/" $FILE
 
+OPTIONS=""
+OPTIONS+=" --with-openssl"
+OPTIONS+=" --with-ofx"
+OPTIONS+=" --with-libxml2"
+
+# plateform specific options
+case $(uname) in
+	Darwin)
+		OPTIONS+=" --without-goffice"
+		;;
+
+	Linux)
+		OPTIONS+=" --with-goffice"
+		;;
+esac
+
 ./autogen.sh
-./configure --with-openssl --with-ofx --with-goffice --with-libxml2 $@
+./configure $OPTIONS $@
 make clean
 
 echo "création du package de Grisbi"
