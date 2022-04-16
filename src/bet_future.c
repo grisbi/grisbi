e6fc2a9e754933c7b2b066066197cf493c00eeec
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-04-16 18:28:29 +0200

    Fix memory leak
    CID 1515650 (#1 of 1): Resource leak (RESOURCE_LEAK)
    14. leaked_storage: Variable scheduled going out of scope leaks the storage it points to.

--- src/bet_future.c
+++ src/bet_future.c
@@ -1917,6 +1917,7 @@ dialog_return:
                                  "not specified or the date is invalid."));
             dialogue_warning_hint (tmp_str, _("One field is not filled in"));
             g_free (tmp_str);
+			bet_data_future_struct_free(scheduled);
             goto dialog_return;
         }
         else
