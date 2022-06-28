efaad1077845c0036a2d1664e6374e7acb0f686e
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-06-28 18:54:04 +0200

    AppVeyor: install autoconf

--- share/appveyor-install.sh
+++ share/appveyor-install.sh
@@ -15,3 +15,4 @@ pacman -S --noconfirm mingw-$i_pkg_postfix-freetype
 pacman -S --noconfirm mingw-$i_pkg_postfix-libgsf 
 pacman -S --noconfirm mingw-$i_pkg_postfix-ntldd-git
 pacman -S --noconfirm mingw-$i_pkg_postfix-libxslt
+pacman -S --noconfirm autoconf
