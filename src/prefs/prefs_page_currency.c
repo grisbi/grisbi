a37e8a8540a559c0d30a7ae09b288038869cb448
Pierre <pierre@pierre.biava.name>
2022-07-27 21:08:26 +0200

    transfer prefs_page_currency.ui file in prefs directory

--- src/prefs/prefs_page_currency.c
+++ src/prefs/prefs_page_currency.c
@@ -776,7 +776,7 @@ static void prefs_page_currency_class_init (PrefsPageCurrencyClass *klass)
 	G_OBJECT_CLASS (klass)->dispose = prefs_page_currency_dispose;
 
 	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
-												 "/org/gtk/grisbi/ui/prefs_page_currency.ui");
+												 "/org/gtk/grisbi/prefs/prefs_page_currency.ui");
 
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageCurrency, vbox_currency);
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageCurrency, button_currency_add);
