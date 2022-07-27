4e5f49dd7884db1956aa8cdfd6555c1c28363551
Pierre <pierre@pierre.biava.name>
2022-07-27 21:13:19 +0200

    transfer prefs_page_currency_link.ui file in prefs directory

--- src/prefs/prefs_page_currency_link.c
+++ src/prefs/prefs_page_currency_link.c
@@ -477,7 +477,7 @@ static void prefs_page_currency_link_class_init (PrefsPageCurrencyLinkClass *kla
 	G_OBJECT_CLASS (klass)->dispose = prefs_page_currency_link_dispose;
 
 	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
-												 "/org/gtk/grisbi/ui/prefs_page_currency_link.ui");
+												 "/org/gtk/grisbi/prefs/prefs_page_currency_link.ui");
 
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageCurrencyLink, vbox_currency_link);
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageCurrencyLink, button_currency_link_add);
