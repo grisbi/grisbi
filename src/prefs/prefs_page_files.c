0ae69d2327ffbda6702ed7390eadb71f96de3bd3
Pierre <pierre@pierre.biava.name>
2022-07-28 13:07:22 +0200

    transfer prefs_page_files.ui file in prefs directory

--- src/prefs/prefs_page_files.c
+++ src/prefs/prefs_page_files.c
@@ -453,7 +453,7 @@ static void prefs_page_files_class_init (PrefsPageFilesClass *klass)
 	G_OBJECT_CLASS (klass)->dispose = prefs_page_files_dispose;
 
 	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
-												 "/org/gtk/grisbi/ui/prefs_page_files.ui");
+												 "/org/gtk/grisbi/prefs/prefs_page_files.ui");
 
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageFiles, vbox_files);
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageFiles, checkbutton_load_last_file);
