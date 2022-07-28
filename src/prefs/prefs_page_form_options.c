432831888062235fc51af32b2af56902375b38d2
Pierre <pierre@pierre.biava.name>
2022-07-28 13:19:16 +0200

    transfer prefs_page_form_options.ui file in prefs directory

--- src/prefs/prefs_page_form_options.c
+++ src/prefs/prefs_page_form_options.c
@@ -172,7 +172,7 @@ static void prefs_page_form_options_class_init (PrefsPageFormOptionsClass *klass
 	G_OBJECT_CLASS (klass)->dispose = prefs_page_form_options_dispose;
 
 	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
-												 "/org/gtk/grisbi/ui/prefs_page_form_options.ui");
+												 "/org/gtk/grisbi/prefs/prefs_page_form_options.ui");
 
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageFormOptions, vbox_form_options);
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass),
