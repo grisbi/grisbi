fb1a6889275e7e07c2fe80f78709142ff94beca2
Pierre <pierre@pierre.biava.name>
2022-07-28 13:24:58 +0200

    transfer prefs_page_fyear.ui file in prefs directory

--- src/prefs/prefs_page_fyear.c
+++ src/prefs/prefs_page_fyear.c
@@ -797,7 +797,7 @@ static void prefs_page_fyear_class_init (PrefsPageFyearClass *klass)
 	G_OBJECT_CLASS (klass)->dispose = prefs_page_fyear_dispose;
 
 	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
-												 "/org/gtk/grisbi/ui/prefs_page_fyear.ui");
+												 "/org/gtk/grisbi/prefs/prefs_page_fyear.ui");
 
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageFyear, vbox_fyear);
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageFyear, grid_fyear_details);
