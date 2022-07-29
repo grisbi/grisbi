0f58dbc8c6d7add7f6af8c10a7ed0138eeaf30e0
Pierre <pierre@pierre.biava.name>
2022-07-29 16:05:32 +0200

    transfer prefs_page_reconcile_sort.ui file in prefs directory

--- src/prefs/prefs_page_reconcile_sort.c
+++ src/prefs/prefs_page_reconcile_sort.c
@@ -798,7 +798,7 @@ static void prefs_page_reconcile_sort_class_init (PrefsPageReconcileSortClass *k
 	G_OBJECT_CLASS (klass)->dispose = prefs_page_reconcile_sort_dispose;
 
 	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
-												 "/org/gtk/grisbi/ui/prefs_page_reconcile_sort.ui");
+												 "/org/gtk/grisbi/prefs/prefs_page_reconcile_sort.ui");
 
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageReconcileSort, vbox_reconcile_sort);
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageReconcileSort, button_collapse_all);
