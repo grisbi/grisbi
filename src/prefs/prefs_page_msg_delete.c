9139aa41f7cb53df63f0d62d782b4c35be4bf19f
Pierre <pierre@pierre.biava.name>
2022-07-29 15:37:34 +0200

    transfer prefs_page_msg_delete.ui file in prefs directory

--- src/prefs/prefs_page_msg_delete.c
+++ src/prefs/prefs_page_msg_delete.c
@@ -282,7 +282,7 @@ static void prefs_page_msg_delete_class_init (PrefsPageMsgDeleteClass *klass)
 	G_OBJECT_CLASS (klass)->dispose = prefs_page_msg_delete_dispose;
 
 	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
-												 "/org/gtk/grisbi/ui/prefs_page_msg_delete.ui");
+												 "/org/gtk/grisbi/prefs/prefs_page_msg_delete.ui");
 
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageMsgDelete, vbox_msg_delete);
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageMsgDelete, sw_display_msg);
