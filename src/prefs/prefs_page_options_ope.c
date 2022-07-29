176fdab27352889e2785025e74e8e161b99c9e8f
Pierre <pierre@pierre.biava.name>
2022-07-29 15:45:51 +0200

    transfer prefs_page_options_ope file in prefs directory

--- src/prefs/prefs_page_options_ope.c
+++ src/prefs/prefs_page_options_ope.c
@@ -348,7 +348,7 @@ static void prefs_page_options_ope_class_init (PrefsPageOptionsOpeClass *klass)
 	G_OBJECT_CLASS (klass)->dispose = prefs_page_options_ope_dispose;
 
 	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
-												 "/org/gtk/grisbi/ui/prefs_page_options_ope.ui");
+												 "/org/gtk/grisbi/prefs/prefs_page_options_ope.ui");
 
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageOptionsOpe, vbox_options_ope);
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass),
