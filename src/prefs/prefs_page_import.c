e5d6573d4050096aebcdc3afc0dae20f3399a605
Pierre <pierre@pierre.biava.name>
2022-08-05 17:13:52 +0200

    fix the adress of  prefs_page_import.ui file

--- src/prefs/prefs_page_import.c
+++ src/prefs/prefs_page_import.c
@@ -131,7 +131,7 @@ static void prefs_page_import_class_init (PrefsPageImportClass *klass)
 	G_OBJECT_CLASS (klass)->dispose = prefs_page_import_dispose;
 
 	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass),
-												 "/org/gtk/grisbi/ui/prefs_page_import.ui");
+												 "/org/gtk/grisbi/prefs/prefs_page_import.ui");
 
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageImport, vbox_import);
 	gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), PrefsPageImport, notebook_import_pages);
