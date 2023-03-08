4c70009a71f9bdaf1316e800f650b8ca5c5e880a
Ludovic Rousseau <ludovic.rousseau@free.fr>
2023-03-08 14:40:06 +0100

    Fix compiler warning
    Reindent to fix:
    prefs_page_display_gui.c: In function ‘prefs_page_display_gui_active_scrolling_left_pane’:
    prefs_page_display_gui.c:94:5: error: this ‘else’ clause does not guard... [-Werror=misleading-indentation]
       94 |     else
          |     ^~~~
    prefs_page_display_gui.c:99:9: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the ‘else’
       99 |         return FALSE;
          |         ^~~~~~

--- src/prefs/prefs_page_display_gui.c
+++ src/prefs/prefs_page_display_gui.c
@@ -88,12 +88,12 @@ static gboolean prefs_page_display_gui_active_scrolling_left_pane (GtkWidget *to
 {
 	if (a_conf->active_scrolling_left_pane)
 		g_signal_handlers_unblock_by_func (gsb_gui_navigation_get_tree_view (),
-										   G_CALLBACK (gsb_gui_navigation_check_scroll),
-										   NULL);
+				G_CALLBACK (gsb_gui_navigation_check_scroll),
+				NULL);
 	else
 		g_signal_handlers_block_by_func (gsb_gui_navigation_get_tree_view (),
-										 G_CALLBACK (gsb_gui_navigation_check_scroll),
-										 NULL);
+				G_CALLBACK (gsb_gui_navigation_check_scroll),
+				NULL);
 
 	return FALSE;
 }
