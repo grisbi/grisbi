e7dc344fc3051de7491306a1828b36f3706d5947
Pierre <pierre@pierre.biava.name>
2023-07-12 15:13:08 +0200

    fixe deprecated property
    temporary : gtk_file_chooser is deprecated since gtk 4.10
    (grisbi:210742): GLib-GObject-WARNING **: 15:54:15.327: The property GtkButton:use-stock is deprecated and shouldn't be used anymore. It will be removed in a future version.

--- src/bet_finance_ui.c
+++ src/bet_finance_ui.c
@@ -779,6 +779,8 @@ static void bet_finance_ui_export_tab (GtkWidget *menu_item,
 									   GtkTreeView *tree_view)
 {
     GtkWidget *dialog;
+	GtkWidget *button_cancel;
+	GtkWidget *button_save;
     gint resultat;
     gchar *filename;
     gchar *tmp_last_directory;
@@ -786,16 +788,25 @@ static void bet_finance_ui_export_tab (GtkWidget *menu_item,
     dialog = gtk_file_chooser_dialog_new (_("Export the array"),
 										  GTK_WINDOW (grisbi_app_get_active_window (NULL)),
 										  GTK_FILE_CHOOSER_ACTION_SAVE,
-										  "gtk-cancel", GTK_RESPONSE_CANCEL,
-										  "gtk-save", GTK_RESPONSE_OK,
+										  GTK_BUTTONS_NONE,
 										  NULL);
+
+	button_cancel = gtk_button_new_with_label (_("Cancel"));
+	gtk_dialog_add_action_widget (GTK_DIALOG (dialog), button_cancel, GTK_RESPONSE_CANCEL);
+	gtk_widget_set_can_default (button_cancel, TRUE);
+
+	button_save = gtk_button_new_with_label (_("Save"));
+	gtk_dialog_add_action_widget (GTK_DIALOG (dialog), button_save, GTK_RESPONSE_OK);
+	gtk_widget_set_can_default (button_save, TRUE);
 
     gtk_file_chooser_set_current_name (GTK_FILE_CHOOSER (dialog),  _("credit.csv"));
     gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), gsb_file_get_last_path ());
     gtk_file_chooser_set_do_overwrite_confirmation (GTK_FILE_CHOOSER (dialog), TRUE);
     gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER_ON_PARENT);
 
-    resultat = gtk_dialog_run (GTK_DIALOG (dialog));
+	gtk_widget_show_all (dialog);
+
+	resultat = gtk_dialog_run (GTK_DIALOG (dialog));
 
     switch (resultat)
     {
