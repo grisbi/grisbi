f6aa1ff06f9355e89fe7969a5fd64cfbf48aece4
Ludovic Rousseau <ludovic.rousseau@free.fr>
2023-12-13 15:02:03 +0100

    Do not use intltool to build Grisbi

--- share/appveyor-install.sh
+++ share/appveyor-install.sh
@@ -17,5 +17,5 @@ pacman -S --noconfirm mingw-$i_pkg_postfix-ntldd-git
 pacman -S --noconfirm mingw-$i_pkg_postfix-libxslt
 pacman -S --noconfirm autoconf
 pacman -S --noconfirm automake
-pacman -S --noconfirm intltool
+pacman -S --noconfirm autopoint
 pacman -S --noconfirm libtool
