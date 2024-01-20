grisbi/grisbi

commit 28e0e803e0ab6005c46ff6c94f41160f6dd975c8

Use ~/Documents/ as default directory also on macOS

src/gsb_dirs.c
@@ -87,7 +87,6 @@ void gsb_dirs_init (char* gsb_bin_path)
 
     user_config_dir = g_build_filename (g_get_user_config_dir (), "grisbi", NULL);
     user_data_dir = g_build_filename (g_get_user_data_dir (), "grisbi", NULL);
-    user_default_dir = g_strdup (g_get_user_special_dir (G_USER_DIRECTORY_DOCUMENTS));
 }
 #else
 {
@@ -97,7 +96,6 @@ void gsb_dirs_init (char* gsb_bin_path)
                         "Library/Application Support/Grisbi/config", NULL);
         user_data_dir = g_build_filename (g_get_home_dir (),
                         "Library/Application Support/Grisbi/data", NULL);
-        user_default_dir = g_strdup (g_get_home_dir());
 
         #ifdef GTKOSXAPPLICATION
         {
@@ -136,9 +134,10 @@ void gsb_dirs_init (char* gsb_bin_path)
 
         user_config_dir = g_build_filename (g_get_user_config_dir (), "grisbi", NULL);
         user_data_dir = g_build_filename (g_get_user_data_dir (), "grisbi", NULL);
-        user_default_dir = g_strdup (g_get_user_special_dir (G_USER_DIRECTORY_DOCUMENTS));
     }
     #endif /* OS_OSX */
+
+	user_default_dir = g_strdup (g_get_user_special_dir (G_USER_DIRECTORY_DOCUMENTS));
 }
 #endif /* G_OS_WIN32 */
 

commit 9167c17bd42e1db30d03813a7d69b5b1a929556e

Correctly set user_default_dir on Windows
This bugs was introduced by me in
28e0e803e0ab6005c46ff6c94f41160f6dd975c8 included since Grisbi 3.0.0

This caused "permission denied" errors when saving the file (on Windows
only).

Fixes: https://www.grisbi.org/bugsreports/view.php?id=2283

src/gsb_dirs.c
@@ -137,9 +137,9 @@ void gsb_dirs_init (char* gsb_bin_path)
     }
     #endif /* OS_OSX */
 
-	user_default_dir = g_strdup (g_get_user_special_dir (G_USER_DIRECTORY_DOCUMENTS));
 }
 #endif /* G_OS_WIN32 */
+	user_default_dir = g_strdup (g_get_user_special_dir (G_USER_DIRECTORY_DOCUMENTS));
 
 	/* corrige un bug si un répertoire ui existe dans le compte de l'utilisateur */
 	if (g_strcmp0 (gsb_bin_path, "grisbi") == 0)