7b2ef17ffd138c1caf4d5c97b40983c9620c3a68
Pierre <pierre@pierre.biava.name>
2024-07-24 15:25:30 +0200

    Replace pacman with pacboy and fix errors

--- win32-msys2/prepare_Win32-ucrt.cmd
+++ win32-msys2/prepare_Win32-ucrt.cmd
@@ -1,26 +1,26 @@
 @echo off
 
-echo "Mise à jour de Msys2
+echo "Mise à jour de Msys2"
 pacman -Suuu
 
 echo "installation de autoconf automake make"
 pacman --noconfirm -S base-devel
 pacman --noconfirm -S autoconf
 pacman --noconfirm -S automake
-pacman --noconfirm -S mingw-w64-ucrt-x86_64-libtool
+pacboy --noconfirm -S libtool:p
 pacman --noconfirm -S unzip
 
 echo "installation de cairo gcc, gettext-tools, gtk3, libgsf librsvg, libxslt, ntldd pour UCRT system"
-pacman --noconfirm -S mingw-w64-ucrt-x86_64-cairo
-pacman --noconfirm -S mingw-w64-ucrt-x86_64-gcc
-pacman --noconfirm -S mingw-w64-ucrt-x86_64-gettext-tools
-pacman --noconfirm -S mingw-w64-ucrt-x86_64-gtk3
-pacman --noconfirm -S mingw-w64-ucrt-x86_64-libgsf
-pacman --noconfirm -S mingw-w64-ucrt-x86_64-librsvg
-pacman --noconfirm -S mingw-w64-ucrt-x86_64-libxslt
-pacman --noconfirm -S mingw-w64-ucrt-x86_64-ntldd
+pacboy --noconfirm -S cairo:p
+pacboy --noconfirm -S gcc:p
+pacboy --noconfirm -S gettext-tools:p
+pacboy --noconfirm -S gtk3:p
+pacboy --noconfirm -S libgsf:p
+pacboy --noconfirm -S librsvg:p
+pacboy --noconfirm -S libxslt:p
+pacboy --noconfirm -S ntldd:p
 
-echo "installation de git
+echo "installation de git"
 pacman --noconfirm -S git
 
 echo "creation repertoire dist"
