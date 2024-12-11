4d345d7ea01703d4b7510004bd728a8e95e9794e
Pierre <pierre@pierre.biava.name>
2024-12-11 09:02:56 +0100

    cosmetics changes

--- src/transaction_list_sort.c
+++ src/transaction_list_sort.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*     Copyright (C)    2000-2008 Cédric Auger (cedric@grisbi.org)            */
-/*          https://www.grisbi.org/                                            */
+/*          https://www.grisbi.org/                                           */
 /*                                                                            */
 /*  This program is free software; you can redistribute it and/or modify      */
 /*  it under the terms of the GNU General Public License as published by      */
@@ -49,15 +49,43 @@
 /*END_INCLUDE*/
 
 /*START_STATIC*/
-static gboolean transaction_list_sort_get_initial_sort ( void );
 /*END_STATIC*/
 
 /*START_EXTERN*/
 extern GtkTreeViewColumn *transactions_tree_view_columns[CUSTOM_MODEL_VISIBLE_COLUMNS];
 /*END_EXTERN*/
 
+/******************************************************************************/
+/* Private functions                                                          */
+/******************************************************************************/
+/**
+ * get if the sorting function is initial (by date or value_date) or not
+ *
+ * \param
+ *
+ * \return TRUE if the list is sorted by date or value_date, FALSE if not
+ * */
+static gboolean transaction_list_sort_get_initial_sort (void)
+{
+    gint account_number;
+    gint element_number;
 
+    account_number = gsb_gui_navigation_get_current_account ();
+    if (account_number == -1)
+        return FALSE;
 
+    element_number = gsb_data_account_get_element_sort (account_number,
+														gsb_data_account_get_sort_column (account_number));
+
+    if (element_number == ELEMENT_VALUE_DATE)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+/******************************************************************************/
+/* Public functions                                                           */
+/******************************************************************************/
 /**
  * Sort the list according to the current column and sort type
  * sort the filtered model because we don't need to sort not showed columns
@@ -72,7 +100,7 @@ extern GtkTreeViewColumn *transactions_tree_view_columns[CUSTOM_MODEL_VISIBLE_CO
  * \param
  *
  * \return
- * */
+ **/
 void transaction_list_sort (void)
 {
     GtkTreePath *path;
@@ -84,21 +112,21 @@ void transaction_list_sort (void)
 	w_run = (GrisbiWinRun *) grisbi_win_get_w_run ();
 
 	custom_list = transaction_model_get_model ();
-    g_return_if_fail ( custom_list != NULL );
+    g_return_if_fail (custom_list != NULL);
 
     /* resort */
-    if (custom_list -> user_sort_reconcile)
+    if (custom_list->user_sort_reconcile)
 	{
 #ifdef HAVE_G_SORT_ARRAY
-    g_sort_array
+		g_sort_array
 #else
-    g_qsort_with_data
+		g_qsort_with_data
 #endif
-                     (custom_list->visibles_rows,
-                        custom_list->num_visibles_rows,
-                        sizeof(CustomRecord*),
-                        (GCompareDataFunc) gsb_reconcile_list_sort_func,
-                        custom_list);
+						 (custom_list->visibles_rows,
+						  custom_list->num_visibles_rows,
+						  sizeof(CustomRecord*),
+						  (GCompareDataFunc) gsb_reconcile_list_sort_func,
+						  custom_list);
 	}
     else if (transaction_list_sort_get_initial_sort ()
 			 && w_run->equilibrage == FALSE
@@ -118,15 +146,15 @@ void transaction_list_sort (void)
 
 		/* sort of the list */
 #ifdef HAVE_G_SORT_ARRAY
-    g_sort_array
+		g_sort_array
 #else
-    g_qsort_with_data
+		g_qsort_with_data
 #endif
-		             (custom_list->visibles_rows,
-                        custom_list->num_visibles_rows,
-                        sizeof(CustomRecord*),
-                        (GCompareDataFunc) gsb_transactions_list_sort,
-                        custom_list);
+						 (custom_list->visibles_rows,
+						  custom_list->num_visibles_rows,
+						  sizeof(CustomRecord*),
+						  (GCompareDataFunc) gsb_transactions_list_sort,
+						  custom_list);
 	}
 
 	/* fixes bug 1875 */
@@ -134,7 +162,7 @@ void transaction_list_sort (void)
 		return;
 
 	/* let other objects know about the new order */
-    neworder = g_new0(gint, custom_list->num_visibles_rows);
+    neworder = g_new0 (gint, custom_list->num_visibles_rows);
 
     for (i = 0; i < custom_list->num_visibles_rows; ++i)
     {
@@ -144,13 +172,11 @@ void transaction_list_sort (void)
 
     path = gtk_tree_path_new();
 
-    gtk_tree_model_rows_reordered(GTK_TREE_MODEL(custom_list), path, NULL, neworder);
+    gtk_tree_model_rows_reordered (GTK_TREE_MODEL(custom_list), path, NULL, neworder);
 
     gtk_tree_path_free(path);
     g_free(neworder);
 }
-
-
 
 /**
  * change the sort column and/or the sort order
@@ -162,9 +188,9 @@ void transaction_list_sort (void)
  * \param new_sort_order	GTK_SORT_DESCENDING / GTK_SORT_ASCENDING
  *
  * \return
- * */
-void transaction_list_sort_set_column ( gint new_sort_col,
-                        GtkSortType new_sort_order )
+ **/
+void transaction_list_sort_set_column (gint new_sort_col,
+									   GtkSortType new_sort_order)
 {
     CustomList *custom_list;
 
@@ -172,30 +198,26 @@ void transaction_list_sort_set_column ( gint new_sort_col,
 
     devel_debug_int (new_sort_col);
 
-    g_return_if_fail ( custom_list != NULL );
+    g_return_if_fail (custom_list != NULL);
 
     if (new_sort_col >= CUSTOM_MODEL_VISIBLE_COLUMNS)
     {
-	g_warning (_("Asked to sort by column %d, which is bigger than the visible columns (%d)"),
-		   new_sort_col,
-		   CUSTOM_MODEL_VISIBLE_COLUMNS );
-	return;
+		g_warning (_("Asked to sort by column %d, which is bigger than the visible columns (%d)"),
+			   new_sort_col,
+			   CUSTOM_MODEL_VISIBLE_COLUMNS);
+
+		return;
     }
 
     /* update the indicator */
-    gtk_tree_view_column_set_sort_indicator (transactions_tree_view_columns[custom_list -> sort_col],
-					     FALSE);
-    gtk_tree_view_column_set_sort_indicator (transactions_tree_view_columns[new_sort_col],
-					     TRUE);
-    gtk_tree_view_column_set_sort_order (transactions_tree_view_columns[new_sort_col],
-					 new_sort_order );
+    gtk_tree_view_column_set_sort_indicator (transactions_tree_view_columns[custom_list->sort_col], FALSE);
+    gtk_tree_view_column_set_sort_indicator (transactions_tree_view_columns[new_sort_col], TRUE);
+    gtk_tree_view_column_set_sort_order (transactions_tree_view_columns[new_sort_col], new_sort_order);
 
     /* save the new values */
-    custom_list -> sort_col = new_sort_col;
-    custom_list -> sort_order = new_sort_order;
+    custom_list->sort_col = new_sort_col;
+    custom_list->sort_order = new_sort_order;
 }
-
-
 
 /**
  * change the sort column and/or the sort order
@@ -205,23 +227,21 @@ void transaction_list_sort_set_column ( gint new_sort_col,
  * \param sort_order	GTK_SORT_DESCENDING / GTK_SORT_ASCENDING
  *
  * \return
- * */
-void transaction_list_sort_get_column ( gint *sort_col,
-                        GtkSortType *sort_order )
+ **/
+void transaction_list_sort_get_column (gint *sort_col,
+									   GtkSortType *sort_order)
 {
     CustomList *custom_list;
 
     custom_list = transaction_model_get_model ();
 
-    g_return_if_fail ( custom_list != NULL );
+    g_return_if_fail (custom_list != NULL);
 
     if (sort_col)
-	*sort_col = custom_list -> sort_col;
+		*sort_col = custom_list->sort_col;
     if (sort_order)
-	*sort_order = custom_list -> sort_order;
+		*sort_order = custom_list->sort_order;
 }
-
-
 
 /**
  * set if the sorting function is normal (by columns) or for reconciliation
@@ -230,24 +250,20 @@ void transaction_list_sort_get_column ( gint *sort_col,
  *
  * \return
  * */
-void transaction_list_sort_set_reconcile_sort ( gboolean use_reconcile_sort )
+void transaction_list_sort_set_reconcile_sort (gboolean use_reconcile_sort)
 {
     CustomList *custom_list;
 
-    custom_list = transaction_model_get_model ();
-
     devel_debug_int (use_reconcile_sort);
+    custom_list = transaction_model_get_model ();
+    g_return_if_fail (custom_list != NULL);
 
-    g_return_if_fail ( custom_list != NULL );
-
-    custom_list -> user_sort_reconcile = use_reconcile_sort;
+    custom_list->user_sort_reconcile = use_reconcile_sort;
 
     /* update the indicator */
-    gtk_tree_view_column_set_sort_indicator (transactions_tree_view_columns[custom_list -> sort_col],
-					     !use_reconcile_sort );
+    gtk_tree_view_column_set_sort_indicator (transactions_tree_view_columns[custom_list->sort_col],
+											 !use_reconcile_sort);
 }
-
-
 
 /**
  * get if the sorting function is normal (by columns) or for reconciliation
@@ -255,40 +271,24 @@ void transaction_list_sort_set_reconcile_sort ( gboolean use_reconcile_sort )
  * \param
  *
  * \return TRUE if the list is sorted for reconciliation, FALSE if column sort
- * */
-gboolean transaction_list_sort_get_reconcile_sort ( void )
+ **/
+gboolean transaction_list_sort_get_reconcile_sort (void)
 {
     CustomList *custom_list;
 
     custom_list = transaction_model_get_model ();
+    g_return_val_if_fail (custom_list != NULL, FALSE);
 
-    g_return_val_if_fail ( custom_list != NULL, FALSE );
-
-    return custom_list -> user_sort_reconcile;
+    return custom_list->user_sort_reconcile;
 }
 
-
 /**
- * get if the sorting function is initial (by date or value_date) or not
+ *
  *
  * \param
  *
- * \return TRUE if the list is sorted by date or value_date, FALSE if not
- * */
-gboolean transaction_list_sort_get_initial_sort ( void )
-{
-    gint account_number;
-    gint element_number;
-
-    account_number = gsb_gui_navigation_get_current_account ();
-    if (account_number == -1)
-        return FALSE;
-
-    element_number = gsb_data_account_get_element_sort ( account_number,
-                        gsb_data_account_get_sort_column ( account_number ) );
-
-    if ( element_number == ELEMENT_VALUE_DATE )
-        return TRUE;
-    else
-        return FALSE;
-}
+ * \return
+ **/
+/* Local Variables: */
+/* c-basic-offset: 4 */
+/* End: */
