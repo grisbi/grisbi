0c68da4ea3142bebdd59ef5d6e151528faf7f8cb
Pierre <pierre@pierre.biava.name>
2025-09-14 11:46:11 +0200

    Added a wait cursor if the number of operations is greater than 10 000
    Added a wait cursor if the number of operations is greater than 10 000. 3000 for Windows

--- src/utils.c
+++ src/utils.c
@@ -902,18 +902,13 @@ GtkWidget *utils_menu_item_new_from_resource_label (const gchar *image_name,
 void utils_cursor_set_wait_cursor (GtkWidget *widget,
 								   gpointer  data)
 {
-	GdkCursor *cursor;
-	GdkDisplay *display;
 	GdkWindow *run_window;
 	GrisbiWin *win;
 
 	/* set progress cursor */
 	win = grisbi_app_get_active_window (NULL);
 	run_window = gtk_widget_get_window (GTK_WIDGET (win));
-	display = gdk_window_get_display (run_window);
-
-	cursor = gdk_cursor_new_from_name (display, "wait");
-	gdk_window_set_cursor (run_window, cursor);
+	utils_gdk_window_set_wait_cursor (run_window);
 
 	/* update ui */
 	g_main_context_iteration (NULL, FALSE);
@@ -922,6 +917,25 @@ void utils_cursor_set_wait_cursor (GtkWidget *widget,
 	gdk_window_set_cursor (run_window, NULL);
 }
 
+/**
+ *
+ *
+ * \param		GdkWindow run_window
+ *
+ * \return
+ **/
+void utils_gdk_window_set_wait_cursor (GdkWindow *run_window)
+{
+	GdkCursor *cursor;
+	GdkDisplay *display;
+
+	/* set progress cursor */
+	display = gdk_window_get_display (run_window);
+
+	cursor = gdk_cursor_new_from_name (display, "wait");
+	gdk_window_set_cursor (run_window, cursor);
+}
+ 
 /**
  *
  *
