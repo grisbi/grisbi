grisbi/grisbi

commit 005e00be2a9653aeb4ed81651e21c61f890c7e93

replaces direct access of structures by accessor functions except for gtk_table and tree_view widgets

src/gsb_calendar.c
@@ -103,25 +103,28 @@ gboolean gsb_calendar_update ( void )
 {
     time_t temps;
     GSList *tmp_list;
-    gint calendar_month;
-	gint calendar_year;
+    guint calendar_day;
+    guint calendar_month;
+    guint calendar_year;
 
-    gtk_calendar_clear_marks ( GTK_CALENDAR ( scheduled_calendar ));
+    gtk_calendar_clear_marks ( GTK_CALENDAR ( scheduled_calendar ) );
+    gtk_calendar_get_date ( GTK_CALENDAR ( scheduled_calendar ),
+                        &calendar_day,
+                        &calendar_month,
+                        &calendar_year );
 
     /* select the current day */
     time ( &temps );
 
-    if ( ( localtime ( &temps ) -> tm_mon == GTK_CALENDAR ( scheduled_calendar ) -> month )
-	 &&
-	 ( ( localtime ( &temps ) -> tm_year + 1900 ) == GTK_CALENDAR ( scheduled_calendar ) -> year ) )
-	gtk_calendar_select_day ( GTK_CALENDAR ( scheduled_calendar ),
-				  localtime ( &temps ) -> tm_mday );
+    if ( ( localtime ( &temps ) -> tm_mon == calendar_month )
+     &&
+     ( ( localtime ( &temps ) -> tm_year + 1900 ) == calendar_year ) )
+        gtk_calendar_select_day ( GTK_CALENDAR ( scheduled_calendar ), localtime ( &temps ) -> tm_mday );
     else
-	gtk_calendar_select_day ( GTK_CALENDAR ( scheduled_calendar ),
-				  FALSE );
+        gtk_calendar_select_day ( GTK_CALENDAR ( scheduled_calendar ), FALSE );
 
-    calendar_month = GTK_CALENDAR ( scheduled_calendar ) -> month + 1;
-	calendar_year = GTK_CALENDAR ( scheduled_calendar ) -> year + 25;
+    calendar_month = calendar_month + 1;
+    calendar_year = calendar_year + 25;
 
     /* check the scheduled transactions and bold them in the calendar */
     tmp_list = gsb_data_scheduled_get_scheduled_list ();
@@ -156,21 +159,26 @@ void click_sur_jour_calendrier_echeance ( GtkWidget *calendrier,
 					  gpointer null )
 {
     time_t temps;
+    guint calendar_day;
+    guint calendar_month;
+    guint calendar_year;
 
     time ( &temps );
+    gtk_calendar_get_date ( GTK_CALENDAR ( scheduled_calendar ),
+                        &calendar_day,
+                        &calendar_month,
+                        &calendar_year );
 
     g_signal_handlers_block_by_func ( G_OBJECT ( calendrier ),
 				       G_CALLBACK ( click_sur_jour_calendrier_echeance ),
 				       NULL );
 
-    if ( ( localtime ( &temps ) -> tm_mon == GTK_CALENDAR ( scheduled_calendar ) -> month )
-	 &&
-	 ( ( localtime ( &temps ) -> tm_year + 1900 ) == GTK_CALENDAR ( scheduled_calendar ) -> year ) )
-	gtk_calendar_select_day ( GTK_CALENDAR ( scheduled_calendar ),
-				  localtime ( &temps ) -> tm_mday );
+    if ( ( localtime ( &temps ) -> tm_mon == calendar_month )
+     &&
+     ( ( localtime ( &temps ) -> tm_year + 1900 ) == calendar_year ) )
+        gtk_calendar_select_day ( GTK_CALENDAR ( scheduled_calendar ), localtime ( &temps ) -> tm_mday );
     else
-	gtk_calendar_select_day ( GTK_CALENDAR ( scheduled_calendar ),
-				  FALSE );
+        gtk_calendar_select_day ( GTK_CALENDAR ( scheduled_calendar ), FALSE );
 
     g_signal_handlers_unblock_by_func ( G_OBJECT ( calendrier ),
 					 G_CALLBACK ( click_sur_jour_calendrier_echeance ),

src/accueil.c
@@ -286,7 +286,7 @@ static gboolean saisie_echeance_accueil ( GtkWidget *event_box,
     GtkWidget *button;
     gint result;
 
-    parent_save = form_transaction_part -> parent;
+    parent_save = gtk_widget_get_parent ( form_transaction_part );
 
     /* Create the dialog */
     dialog = gtk_dialog_new_with_buttons ( _("Enter a scheduled transaction"),
@@ -303,7 +303,7 @@ static gboolean saisie_echeance_accueil ( GtkWidget *event_box,
 
 	/* first we reparent the form in the dialog */
 	hbox = gtk_hbox_new ( FALSE, 0 );
-	gtk_box_pack_start ( GTK_BOX ( gtk_dialog_get_content_area ( GTK_DIALOG ( dialog ) ) ), hbox, TRUE, TRUE, 0 );
+	gtk_box_pack_start ( GTK_BOX ( dialog_get_content_area ( dialog ) ), hbox, TRUE, TRUE, 0 );
 	gtk_container_set_border_width ( GTK_CONTAINER(hbox), 12 );
 	gtk_widget_reparent ( form_transaction_part, hbox );
     gtk_widget_show_all ( hbox );

src/affichage_liste.c
@@ -944,14 +944,16 @@ gboolean gsb_transaction_list_config_realized ( GtkWidget *tree_view,
     for ( column = 0 ; column < CUSTOM_MODEL_VISIBLE_COLUMNS ; column++ )
     {
         gint width;
+        GtkAllocation allocation;
 
-        width = ( transaction_col_width [column] * ( tree_view -> allocation.width ) ) / 100;
+        gtk_widget_get_allocation ( tree_view, &allocation );
+        width = ( transaction_col_width [column] * ( allocation.width ) ) / 100;
         gtk_tree_view_column_set_fixed_width (
                         gtk_tree_view_get_column ( GTK_TREE_VIEW ( tree_view ), column ),
                         width );
     }
 
-    gdk_window_set_cursor ( tree_view -> window, gdk_cursor_new ( GDK_HAND2 ) );
+    gdk_window_set_cursor ( gtk_widget_get_window ( tree_view ), gdk_cursor_new ( GDK_HAND2 ) );
 
     return FALSE;
 }

src/bet_future.c
@@ -295,7 +295,7 @@ GtkWidget *bet_future_create_dialog ( gint account_number )
     gtk_dialog_set_default_response ( GTK_DIALOG ( dialog ), GTK_RESPONSE_OK );
 
 	vbox = gtk_vbox_new ( FALSE, 0 );
-	gtk_box_pack_start ( GTK_BOX ( GTK_DIALOG ( dialog )->vbox ), vbox, TRUE, TRUE, 0 );
+	gtk_box_pack_start ( GTK_BOX ( dialog_get_content_area ( dialog ) ), vbox, TRUE, TRUE, 0 );
 	gtk_container_set_border_width ( GTK_CONTAINER ( vbox ), 12 );
 
     /* next we fill the bet_form */
@@ -2273,7 +2273,7 @@ static GtkWidget *bet_transfert_create_dialog ( gint account_number )
     gtk_dialog_set_default_response ( GTK_DIALOG ( dialog ), GTK_RESPONSE_OK );
 
     main_vbox = gtk_vbox_new ( FALSE, 0 );
-    gtk_box_pack_start ( GTK_BOX ( GTK_DIALOG ( dialog )->vbox ), main_vbox, TRUE, TRUE, 0 );
+    gtk_box_pack_start ( GTK_BOX ( dialog_get_content_area ( dialog ) ), main_vbox, TRUE, TRUE, 0 );
     gtk_container_set_border_width ( GTK_CONTAINER ( main_vbox ), 12 );
 
     /* list of accounts */

src/categories_onglet.c
@@ -735,7 +735,7 @@ gboolean edit_category ( GtkTreeView *tree_view )
 
     /* Ugly dance to avoid side effects on dialog's vbox. */
     hbox = gtk_hbox_new ( FALSE, 0 );
-    gtk_box_pack_start ( GTK_BOX ( gtk_dialog_get_content_area ( GTK_DIALOG ( dialog ) ) ), hbox, FALSE, FALSE, 0 );
+    gtk_box_pack_start ( GTK_BOX ( dialog_get_content_area ( dialog ) ), hbox, FALSE, FALSE, 0 );
     paddingbox = new_paddingbox_with_title ( hbox, TRUE, title );
     gtk_container_set_border_width ( GTK_CONTAINER(hbox), 6 );
     gtk_container_set_border_width ( GTK_CONTAINER(paddingbox), 6 );

src/dialog.c
@@ -761,6 +761,30 @@ const gchar *dialogue_hint_with_entry ( gchar *text, gchar *hint, gchar *entry_d
 
     return string;
 }
+/**
+ * return the content_area of dialog
+ *
+ * \param dialog
+ *
+ * \return the content_area
+ */
+GtkWidget *dialog_get_content_area ( GtkWidget *dialog )
+{
+    return gtk_dialog_get_content_area ( GTK_DIALOG ( dialog ) );
+}
+
+
+/**
+ * return the action_area of dialog
+ *
+ * \param dialog
+ *
+ * \return the action_area
+ */
+GtkWidget *dialog_get_action_area ( GtkWidget *dialog )
+{
+    return gtk_dialog_get_action_area ( GTK_DIALOG ( dialog ) );
+}
 /* Local Variables: */
 /* c-basic-offset: 4 */
 /* End: */

src/dialog.h
@@ -25,6 +25,8 @@ G_MODULE_EXPORT void dialogue_error ( gchar *text );
 void dialogue_error_brain_damage ();
 G_MODULE_EXPORT void dialogue_error_hint ( const gchar *text, gchar *hint );
 void dialogue_error_memory ();
+GtkWidget *dialog_get_action_area ( GtkWidget *dialog );
+GtkWidget *dialog_get_content_area ( GtkWidget *dialog );
 void dialogue_hint ( gchar *text, gchar *hint );
 const gchar *dialogue_hint_with_entry ( gchar *text, gchar *hint, gchar *entry_description );
 void dialogue_special ( GtkMessageType param, gchar *text );

src/erreur.c
@@ -165,7 +165,7 @@ void traitement_sigsegv ( gint signal_nb )
 
     gtk_expander_set_use_markup ( GTK_EXPANDER ( expander ), TRUE );
     gtk_container_add ( GTK_CONTAINER ( expander ), print_backtrace() );
-    gtk_box_pack_start ( GTK_BOX ( gtk_dialog_get_content_area ( GTK_DIALOG ( dialog ) ) ), expander, FALSE, FALSE, 6 );
+    gtk_box_pack_start ( GTK_BOX ( dialog_get_content_area ( dialog ) ), expander, FALSE, FALSE, 6 );
 
     gtk_widget_show_all ( dialog );
 #endif

commit f5539a64612e40f3bb0122974b509f32160e216c

Scheduler calendar: correctly display the current date in blue
The code mixed day and year. It was broken since
005e00be2a9653aeb4ed81651e21c61f890c7e93 in 2013.

src/gsb_calendar.c
@@ -105,9 +105,9 @@ gboolean gsb_calendar_update ( void )
 
     gtk_calendar_clear_marks ( GTK_CALENDAR ( scheduled_calendar ) );
     gtk_calendar_get_date ( GTK_CALENDAR ( scheduled_calendar ),
-                        &calendar_day,
+                        &calendar_year,
                         &calendar_month,
-                        &calendar_year );
+                        &calendar_day );
 
     /* select the current day */
     time ( &temps );
@@ -161,9 +161,9 @@ void click_sur_jour_calendrier_echeance ( GtkWidget *calendrier,
 
     time ( &temps );
     gtk_calendar_get_date ( GTK_CALENDAR ( scheduled_calendar ),
-                        &calendar_day,
+                        &calendar_year,
                         &calendar_month,
-                        &calendar_year );
+                        &calendar_day );
 
     g_signal_handlers_block_by_func ( G_OBJECT ( calendrier ),
 				       G_CALLBACK ( click_sur_jour_calendrier_echeance ),